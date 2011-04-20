5aee5903ac06e62c67c09015a595558ed42981fd
Travis Kriplean <travis@cs.washington.edu>
2011-04-19 18:53:26 -0700

    much of the positions/new completed
    Former-commit-id: dd06e7130223d919ec8ea190a217405f0578e0aa [formerly dd06e7130223d919ec8ea190a217405f0578e0aa [formerly 3a4b7af8d58a10b7b3e409cdaa5cc3c1a032ac54]]
    Former-commit-id: f5e19a07a4829319bd231414bc85ac34cb58e964
    Former-commit-id: f76382eb2c58d6deb753d941b6a5d85b7a80b156

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,2 +1,32 @@
 class PositionsController < ApplicationController
+  def new 
+    @option = Option.find(params[:option_id])
+    @position = Position.new( :stance => 0.0)
+    
+    # TODO: clean this out on log in
+    #key = "init-#{@initiative.id}".intern
+    #unless session.has_key?( key )
+    #  session[key] = {}
+    #  PointList.new( :initiative => @initiative, :user => current_user, :position => 1, :new => true, :session => session )
+    #  PointList.new( :initiative => @initiative, :user => current_user, :position => 0, :new => true, :session => session )                
+    #end
+    #@pro_points = PointList.new(session[key][:pro_list]).next_page!( POINT_LIST_SIZE, session )
+    #@con_points = PointList.new(session[key][:con_list]).next_page!( POINT_LIST_SIZE, session )    
+    @pro_points = Point.where(:is_pro => true, :option_id => @option.id)
+    @con_points = Point.where(:is_pro => false, :option_id => @option.id)
+
+    @user = current_user
+  end
+  
+  def create
+    
+  end
+  
+  def update
+    
+  end
+  
+  def show
+    
+  end
 end

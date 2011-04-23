b681a870aa5c6176bb705104ee6f63538e84c85d
Travis Kriplean <travis@cs.washington.edu>
2011-04-22 18:15:12 -0700

    inclusion deletions
    Former-commit-id: 1530866e5998ba8cb35e6e9973321a326e66833a [formerly 1530866e5998ba8cb35e6e9973321a326e66833a [formerly 3cb8e89d66fda8fea29bf2842620295cfa69ca52]]
    Former-commit-id: 2bd66982268cc628467c3ecf09cf9afc2f179b11
    Former-commit-id: 32732fb4106357e598a4ba060d9e865fe203eeb7

--- app/controllers/inclusions_controller.rb
+++ app/controllers/inclusions_controller.rb
@@ -11,27 +11,44 @@ class InclusionsController < ApplicationController
       :user_id => @user.id
     })
     
-    @inclusion = Inclusion.create!( params[:inclusion] )
-    #@point.inclusions -= 1
+    @inclusion = current_user.inclusions.where( :point_id => @point.id ).first
+    if ( !@inclusion )
     
-    new_point = @option.points        
-    if ( @point.is_pro )
-      new_point = new_point.pros
+      @inclusion = Inclusion.create!( params[:inclusion] )
+      #@point.inclusions -= 1
+      
+      new_point = @option.points        
+      if ( @point.is_pro )
+        new_point = new_point.pros
+      else
+        new_point = new_point.cons
+      end
+      
+      next_point = new_point.not_included_by(current_user).first
+      new_point = next_point ? render_to_string( :partial => "points/show_in_margin", :locals => { :point => next_point, :user => @user }) : nil
+            
+      #TODO: also filter by LEAST LISTED point
+  
+      #TODO: return pagination    
+      pagination = nil
+      
+      approved_point = render_to_string :partial => "points/show_on_board_self", :locals => { :static => false, :point => @point, :user => @user }    
+      response = { :new_point => new_point, :pagination => pagination, :approved_point => approved_point } 
     else
-      new_point = new_point.cons
+      response = { :success => false }       
     end
     
-    new_point = render_to_string :partial => "points/show_in_margin", :locals => { :point => new_point.not_included_by(current_user).first, :user => @user }
-    #TODO: also filter by LEAST LISTED point
-
-    #TODO: return pagination    
-    pagination = nil
+    render :json => response.to_json
+  end
+  
+  def destroy
+    @option = Option.find(params[:option_id])
+    @point = Point.find(params[:point_id])
+    @user = current_user
     
-    approved_point = render_to_string :partial => "points/show_on_board_self", :locals => { :static => false, :point => @point, :user => @user }    
-    response = { :new_point => new_point, :pagination => pagination, :approved_point => approved_point }        
-        
-    respond_with(@option, @point, @inclusion) do |format|
-      format.js { render :json => response.to_json }
-    end    
+    @inc = current_user.inclusions.where(:point_id => @point.id).first
+    @inc.destroy
+    
+    render :partial => "points/show_in_margin", :locals => { :static => false, :point => @point, :user => @user }    
   end
 end

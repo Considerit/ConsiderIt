1ca7d5bb43090dec2bfa8c3c2a9447a41aeeb3ab
Travis Kriplean <travis@cs.washington.edu>
2011-04-28 20:22:30 -0700

    moving from custom env file to private vars in env vars
    Former-commit-id: ec03919710f69725f200db45b1ede91f8921371c [formerly c6cb318b2bd7c6f33c7128dc0be259ced27827a3] [formerly 66bea12e8edfc996a66bec66f5e6dcd65afb114f [formerly 66bea12e8edfc996a66bec66f5e6dcd65afb114f [formerly 9845feed4cc4e3e586c980aa8fbfd676c6ed5493]]]
    Former-commit-id: 83cd6578024e03b96456109f999cd34180cb18d0 [formerly 5feb1bce648aa31e437c76641702ee9e1823f7df]
    Former-commit-id: f62b433a1ac73545b949b3ce858dc28ac75d7c0b
    Former-commit-id: d1c0a2d4ab673202ce4e4809cd9b98f333f27b1e

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-require File.join(File.dirname(__FILE__), 'environment_custom')
 
 # Initialize the rails application
 ConsiderIt::Application.initialize!

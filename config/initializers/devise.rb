df8192d99c31a7e79f873f2ade725e1e64313c2c
Travis Kriplean <travis@cs.washington.edu>
2011-04-28 20:30:36 -0700

    minor error...
    Former-commit-id: 51d04e42a2bebaddcb893b2e088e6e5616363996 [formerly 841d31a8f221827c7dbd59ca148c042e3d9a5363] [formerly bc9c4d55c8bf0bf907738c3b5c0ec50660113e78 [formerly bc9c4d55c8bf0bf907738c3b5c0ec50660113e78 [formerly ab2a0e1ebb974561eb1b1e7a5a6f67e8891b1f0d]]]
    Former-commit-id: fbab065e81a0b1cd87228387400279164cd72446 [formerly 0ed7247d790c475860d5ded87f749786c3f75d90]
    Former-commit-id: 2e9c5e61498ecf3b3827b294c616ac29c9505c06
    Former-commit-id: 62c32a3cadf863999684905eacb8dd3f099f7f92

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -171,7 +171,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, ENV['FB_ID'], ENV[FB_SECRET], {:scope => 'email'}
+  config.omniauth :facebook, ENV['FB_ID'], ENV['FB_SECRET'], {:scope => 'email'}
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or

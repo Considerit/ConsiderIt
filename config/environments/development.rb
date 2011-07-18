1cb8ee3adcb0d15367726022136c2e6758b5afa2
Travis Kriplean <travis@cs.washington.edu>
2011-07-18 13:57:48 -0700

    turning on serving static assets in production
    Former-commit-id: bc5135ac3783d63ddf78acdd2a9bed2953496497 [formerly ef773783c0ab21dcbc058ac7992e0472684b40ad] [formerly 2eaec61cf5cbbb2cc5b4043003c5eeae4c342bdb [formerly 2eaec61cf5cbbb2cc5b4043003c5eeae4c342bdb [formerly d9964b32926c6936f061e2589780339663cb5c94]]]
    Former-commit-id: d0504622182509814b5c05f9e70691749fb44fb1 [formerly fc9ee1ddcf3c1c745b474379e9a5efef1bcb6b9a]
    Former-commit-id: e864dbdbfc54fba9a4e181d31504d8b5704a4b0a
    Former-commit-id: f91c5e7327e9cbbdc10519f277a9dffcb1aad09f

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,9 +24,7 @@ ConsiderIt::Application.configure do
   config.action_dispatch.best_standards_support = :builtin
   
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  
-  config.action_controller.perform_caching = false
-  
+    
 end
 
 GOOGLE_ANALYTICS = ''

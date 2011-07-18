ada7a7e6d5ae28bccedcb0ab8918a23d7774a3f8
Travis Kriplean <travis@cs.washington.edu>
2011-07-18 14:23:49 -0700

    devise mail addr

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = "admin@considerit.net"
+  config.mailer_sender = "info@livingvotersguide.org"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"

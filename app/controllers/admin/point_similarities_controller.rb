210b4c3d9e78b558a1d634acafe3db8fa72dd62d
Travis Kriplean <travis@cs.washington.edu>
2011-07-24 23:43:37 -0700

    adding functionality around point similarity interface, including network viz
    Former-commit-id: 347fbb9aca948b29a61502088ebb51118ca8227a [formerly fb426868f7ca2ed6bb6fd595b7c700d9b6b94983] [formerly 3ba226516d5bf97b8597324d22d994da11807e29 [formerly 3ba226516d5bf97b8597324d22d994da11807e29 [formerly 0bf26c0923673bbc54c52e183025a5aa8da5bc15]]]
    Former-commit-id: 8897d6bb3bb94c33d56212bc3888d036a9344615 [formerly 2208538e091d16cd845de3a24e49f62adbbfef01]
    Former-commit-id: 5283a939a60f282e5ee00b87b8e2dca670bbf2ce
    Former-commit-id: e5237585994bc18c95728d30ad333ab3d5a0b2bd

--- app/controllers/admin/point_similarities_controller.rb
+++ app/controllers/admin/point_similarities_controller.rb
@@ -1,23 +1,82 @@
 class Admin::PointSimilaritiesController < ApplicationController
   
+  def index
+    redirect_to root_path unless current_user.admin?
+    @user = current_user
+    @option = Option.find(params[:option_id])
+
+    pairs = {};
+    @option.point_similarities.each do |ps|
+      pp pairs
+      if pairs.has_key?(ps.p1)
+        if pairs[ps.p1].has_key?(ps.p2)
+          pairs[ps.p1][ps.p2].push(ps)
+        else
+          pairs[ps.p1][ps.p2] = [ps]
+        end
+      else
+        pairs[ps.p1] = {ps.p2 => [ps]}
+      end
+
+      if pairs.has_key?(ps.p2)
+        if pairs[ps.p2].has_key?(ps.p1)
+          pairs[ps.p2][ps.p1].push(ps)
+        else
+          pairs[ps.p2][ps.p1] = [ps]
+        end
+
+
+      else
+        pairs[ps.p2] = {ps.p1 => [ps]}
+      end
+
+    end
+
+    @graph_def = []
+
+    pairs.each do |pnt, compared_points|
+      my_def = {
+        "data" => {  
+          "$color" => pnt.is_pro ? "#988"  : "#899",   
+          "$type" => pnt.is_pro ? "circle"  : "triangle"  
+        },   
+        "id" => "point-#{pnt.id}",   
+        "name" => "point-#{pnt.id}",
+        "adjacencies"  => [] 
+      }
+      compared_points.each do |pnt2, comparisons|
+        my_def["adjacencies"].push({
+         "nodeTo" =>  "point-#{pnt2.id}",
+         "nodeFrom" => "point-#{pnt.id}",
+         "data" => { "weight" => comparisons.inject(0){ |sum, el| sum + el.value }.to_f / comparisons.size }
+        })
+      end
+      @graph_def.push(my_def)
+    end
+    @graph_def = @graph_def.to_json
+
+  end
+
   def new
-    #redirect_to root_path unless current_user.admin?
+    redirect_to root_path unless current_user.admin?
 
     @option = Option.find(params[:option_id])
     @user = current_user
 
-    @total_compared = current_user.point_similarities.count
+    @total_compared = current_user.point_similarities.where(:option_id=>@option.id).count
     @num_points = @option.points.count
     @total_possible_comparisons = (@num_points * (@num_points-1))/2
+    @current = @total_compared + 1
 
-    if current_user.point_similarities.count >= @total_possible_comparisons
+    if @total_compared >= @total_possible_comparisons
       first = current_user.point_similarities.where(:option_id => @option.id).first
       redirect_to edit_option_point_similarity_path(@option, first)
     end
 
     pairs = {}
+
     
-    current_user.point_similarities.each do |ps|
+    current_user.point_similarities.where(:option_id=>@option.id).each do |ps|
 
       if pairs.has_key?(ps.p1_id)
         pairs[ps.p1_id][ps.p2_id] = 1
@@ -28,38 +87,47 @@ class Admin::PointSimilaritiesController < ApplicationController
       if pairs.has_key?(ps.p2_id)
         pairs[ps.p2_id][ps.p1_id] = 1
       else
-        pairs[ps.p2_id] = {ps.p1.id => 1}
+        pairs[ps.p2_id] = {ps.p1_id => 1}
       end
 
     end
 
     @comparison = @option.point_similarities.build
     @init_val = 3
-    
-    @p1 = @p2 = nil
-    while ( !@p1 || !@p2) || (pairs.has_key?(@p1.id) && pairs[@p1.id].has_key?(@p2.id))
-      (@p1, @p2) = @option.points.sample(2)
+
+    last_comparison = current_user.point_similarities.where(:option_id=>@option.id).last
+    if ( last_comparison && pairs[last_comparison.p1_id].length < @num_points - 1 && @total_compared % 10 != 9 )
+      @p1 = last_comparison.p1
+      while ( !@p2) || (pairs.has_key?(@p1.id) && pairs[@p1.id].has_key?(@p2.id))
+        @p2 = @option.points.sample()
+      end
+    else(!last_comparison || @total_compared % 10 == 9) 
+      @p1 = @p2 = nil
+      while ( !@p1 || !@p2) || (pairs.has_key?(@p1.id) && pairs[@p1.id].has_key?(@p2.id))
+        (@p1, @p2) = @option.points.sample(2)
+      end
     end
     
   end
   
   def edit    
-    #redirect_to root_path unless current_user.admin?
+    redirect_to root_path unless current_user.admin?
     
     @option = Option.find(params[:option_id])
     @user = current_user
-    @total_compared = current_user.point_similarities.count
+    @total_compared = current_user.point_similarities.where(:option_id=>@option.id).count
     @num_points = @option.points.count
     @comparison = PointSimilarity.find(params[:id])
     @init_val = @comparison.value
     @total_possible_comparisons = (@num_points * (@num_points-1))/2
+    @current = current_user.point_similarities.where(:option_id=>@option.id).where('id <' + params[:id]).count + 1
 
     @p1 = @comparison.p1
     @p2 = @comparison.p2
   end
   
   def create
-    # redirect_to root_path unless current_user.admin?
+    redirect_to root_path unless current_user.admin?
     @option = Option.find(params[:option_id])
     @user = current_user
     
@@ -72,16 +140,14 @@ class Admin::PointSimilaritiesController < ApplicationController
     @comparison = PointSimilarity.create!(params[:point_similarity])
 
     if params[:commit] == 'Next >>'
-      pp 'new!!!'
       redirect_to new_option_point_similarity_path(@option)
     else
-      pp 'old!!!'
       redirect_to edit_option_point_similarity_path(@option, last)
     end
   end
 
   def update
-    # redirect_to root_path unless current_user.admin?
+    redirect_to root_path unless current_user.admin?
     
     @option = Option.find(params[:option_id])
     @user = current_user

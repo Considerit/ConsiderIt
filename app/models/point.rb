64bee903ed02527b5993dc5ce7eef0fc1e647c87
Travis Kriplean <travis@cs.washington.edu>
2011-09-12 12:03:57 -0700

    fixing point comments
    Former-commit-id: 41943438ee4923a739e822e444ccd8abc13a3990 [formerly 19f5431a6ef8ed8baef51d789c0427c3f5af3c54] [formerly 5f6790e3609b7be6f655a29ea1a382e8baec6916 [formerly 5f6790e3609b7be6f655a29ea1a382e8baec6916 [formerly 5309bd94abcca0064577b2c345650ae9313cd2d3]]]
    Former-commit-id: 813fd85dcb72cb5b873e0561997ed1850e9aaa33 [formerly c8c3a4cf0d06a4ea60a9d68932adfb1947d966bb]
    Former-commit-id: 820abc1e72bf7d07d89339c71af1e7e0fc31c5cb
    Former-commit-id: 1c0520fccd64dd85ed6618126c13d1ff6ecd40b1

--- app/models/point.rb
+++ app/models/point.rb
@@ -5,7 +5,7 @@ class Point < ActiveRecord::Base
   has_many :inclusions, :dependent => :destroy
   has_many :point_listings, :dependent => :destroy
   has_many :point_links, :dependent => :destroy
-  has_one :comment
+  has_many :comments
 
   accepts_nested_attributes_for :point_links, :reject_if => lambda { |pl| pl[:url].blank? }, :allow_destroy => true
 

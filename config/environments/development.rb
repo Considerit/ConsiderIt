04be685ecfa1e81aba683125efa5e28d44cd30c1
Travis Kriplean <travis@cs.washington.edu>
2011-09-27 16:28:22 -0700

    big commit with position point carousel and homepage design
    Former-commit-id: 5c2824f2bc0150d46a0348d16883f517e1d4cd97 [formerly f3b39b07bde5d92575a1b396d1a3b6fe69b65f5d] [formerly 45774cfd67b5a45e9a3fb33b913faaad7fc72b26 [formerly 45774cfd67b5a45e9a3fb33b913faaad7fc72b26 [formerly b3a9767f699bb8212984f8ff9cb31e34d948802e]]]
    Former-commit-id: 89c987baf470310733cef89c8698897905335d58 [formerly ee2d3556a58bef41d77b49767344c4e3d1d56319]
    Former-commit-id: 011b9c96f935cf5212be6e01b0534fdde30a0b65
    Former-commit-id: 98b5a1729092305c7912e51e166863a22435d86f

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,7 @@ ConsiderIt::Application.configure do
     :enable_starttls_auto => true
   }
 
+  Sass::Plugin.options[:debug_info] = true
 
   Paperclip.options[:command_path] = "/opt/local/bin/"
 end

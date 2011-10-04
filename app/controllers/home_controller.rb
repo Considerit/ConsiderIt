3e2c2e8b0e9b97095630e28f774330b3dbe90a46
Travis Kriplean <travis@cs.washington.edu>
2011-10-03 22:00:00 -0700

    working on pledge
    Former-commit-id: dcf2de5374fb5fbf746618043e08708e3db27486 [formerly 75fcc80259becf5c7eca39b9c811a989b116f3e8] [formerly 46579f066d9d2d6043710c1782746a40f86bd09d [formerly 46579f066d9d2d6043710c1782746a40f86bd09d [formerly 8aaf2daf64df3e7c82323d7ed764144e866a165d]]]
    Former-commit-id: 51dae5dc2d0d4a9d5ad65e86f14ce242d095499a [formerly 4357d7d28b15c69569838c6bdd8bdcd8dff7ca25]
    Former-commit-id: a45514596b069c592762b87104fb88fe4576fefe
    Former-commit-id: 4d82026e8b70ddb7de0bf43806a4efab73c9f1fd

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -32,8 +32,9 @@ class HomeController < ApplicationController
       flash[:notice] = "Welcome to the LVG community!"
     end
 
+    response = {:html => render_to_string :partial => "users/sessions/confirmation_sent"}
     respond_to do |format|
-      format.js {render :partial => "users/sessions/confirmation_sent"}
+      format.js {render :json => response.to_json
     end
         
   end

ae48f15a0a63ad67f9b3c71fa969394439e813fe
Travis Kriplean <travis@cs.washington.edu>
2011-10-03 22:24:21 -0700

    fixing url host for production
    Former-commit-id: 3b8c758b03f41f2dfe4e710ca239f2f377b084d7 [formerly e6e07a60fe6de1957d62eed12395eb5ba454c87e] [formerly 6a8205cf5bc99eb9eabc734ee56df1be166c79ad [formerly 6a8205cf5bc99eb9eabc734ee56df1be166c79ad [formerly d5accf1bb244414ed00b66f11a242d382db83a55]]]
    Former-commit-id: aabde55e04d1d211a6964e7fe362b394db9a19d4 [formerly ebcd1f6ad251bd3712f7ff0a8a3b662a1c7fc180]
    Former-commit-id: fdbd2246869cfa27b141403d9e193e030b324f82
    Former-commit-id: 79bcc9e7a1ba850daafa1551b6c2b95a327001ca

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ ConsiderIt::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'livingvotersguide.org' }
   config.action_mailer.perform_deliveries = true 
   #config.action_mailer.delivery_method = :sendmail  
 

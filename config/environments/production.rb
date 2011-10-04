e4da5b68e633bf43e2498493a423df8713b42139
Engage User <engage@trondheim.cs.washington.edu>
2011-10-04 00:08:01 -0700

    fixing email host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,8 @@ ConsiderIt::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'livingvotersguide.org' }
+  config.action_mailer.default_url_options = { :host => 'livingvotersguide.org',
+  :address => 'localhost' }
   config.action_mailer.perform_deliveries = true 
   #config.action_mailer.delivery_method = :sendmail  
 

a50ae2e9c9a0eeec0623261086496f5ce78d5940
Travis Kriplean <travis@cs.washington.edu>
2011-10-06 00:30:19 -0700

    email preferences
    Former-commit-id: 2c2cb716a6d487750d33a64ad8451110236d3806 [formerly 6b852a8befb786f6c50f27b9f17a3072ae9f0b9e] [formerly f8db8e7962d4afa0fb6a07fb72dee7dacc63b89f [formerly f8db8e7962d4afa0fb6a07fb72dee7dacc63b89f [formerly 374f78a571c2cdb6b4f64475fa641ea0e1b0df2d]]]
    Former-commit-id: 3b38f99658689bb53bdfe80e7f08cbf378cae9fe [formerly 91d91dcdde153859610a515c424f13a42c80ef28]
    Former-commit-id: 09a2dc05d8cd761fc20281486e3941c791c7e734
    Former-commit-id: d3bc2d53d2f6c218b82a3a67358852dd8d565c40

--- db/migrate/20111006045648_add_messaging_preferences_to_user.rb
+++ db/migrate/20111006045648_add_messaging_preferences_to_user.rb
@@ -1,7 +1,20 @@
 class AddMessagingPreferencesToUser < ActiveRecord::Migration
   def self.up
+    add_column :users, :notification_commenter, :boolean, :default => true
+    add_column :users, :notification_author, :boolean, :default => true
+    add_column :positions, :notification_includer, :boolean
+    add_column :positions, :notification_option_subscriber, :boolean
+    add_column :position_versions, :notification_includer, :boolean
+    add_column :position_versions, :notification_option_subscriber, :boolean
+
   end
 
   def self.down
+    remove_column :users, :notification_commenter
+    remove_column :users, :notification_author
+    remove_column :positions, :notification_includer
+    remove_column :positions, :notification_option_subscriber
+    remove_column :position_versions, :notification_includer
+    remove_column :position_versions, :notification_option_subscriber
   end
 end

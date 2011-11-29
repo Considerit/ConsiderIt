4ed975bc1012298e6f2d41ee0ea817ca5a84d811
Travis Kriplean <travis@cs.washington.edu>
2011-11-28 16:30:04 -0800

    Updating wording on emails
    Former-commit-id: 0db89a0da6701c606cc311c557256b90e598cdc0 [formerly 2b9087a4b1470bfdf71aea9b4098b594e017c66e] [formerly 3e9bcdc9230f62835846c54298d5cafd5b7e4ede [formerly 3e9bcdc9230f62835846c54298d5cafd5b7e4ede [formerly d4eecac586914d8344aca73ffb0013365599b28b]]]
    Former-commit-id: a603a987679b0c5f190f9f98038a44551fd54395 [formerly c89791e3a67baa0319e6037d35ab0419422e7586]
    Former-commit-id: b640fdceb8502a6071baf4dd5f3ebd264de0a7f0
    Former-commit-id: cd1a30ee54c9b354278b997137d6a08b08fed4ac

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ActionMailer::Base
-  default :from => "info@livingvotersguide.org"
+  default :from => APP_CONFIG['default_from_email'] 
 
   def option_subscription(user, pnt)
     @user = user
     @point = pnt
     email_with_name = "#{@user.name} <#{@user.email}>"
-    mail(:to => email_with_name, :subject => "[Living Voters Guide] new #{@point.is_pro ? 'pro' : 'con'} point for #{@point.option.category} #{@point.option.designator}")
+    mail(:to => email_with_name, :subject => "[#{APP_CONFIG['email_head']}] new #{@point.is_pro ? 'pro' : 'con'} point for #{@point.option.category} #{@point.option.designator}")
   end
 
   def someone_discussed_your_point(user, pnt, comment)
@@ -13,7 +13,7 @@ class UserMailer < ActionMailer::Base
     @point = pnt
     @comment = comment
     email_with_name = "#{@user.name} <#{@user.email}>"
-    mail(:to => email_with_name, :subject => "[Living Voters Guide] new comment on a #{@point.is_pro ? 'pro' : 'con'} point you wrote")
+    mail(:to => email_with_name, :subject => "[#{APP_CONFIG['email_head']}] new comment on a #{@point.is_pro ? 'pro' : 'con'} point you wrote")
   end
 
   def someone_commented_on_thread(user, pnt, comment)
@@ -21,7 +21,7 @@ class UserMailer < ActionMailer::Base
     @point = pnt
     @comment = comment
     email_with_name = "#{@user.name} <#{@user.email}>"
-    mail(:to => email_with_name, :subject => "[Living Voters Guide] new comment on a #{@point.is_pro ? 'pro' : 'con'} point you wrote")
+    mail(:to => email_with_name, :subject => "[#{APP_CONFIG['email_head']}] new comment on a #{@point.is_pro ? 'pro' : 'con'} point you wrote")
   end
 
   def someone_commented_on_an_included_point(user, pnt, comment)
@@ -29,7 +29,7 @@ class UserMailer < ActionMailer::Base
     @point = pnt
     @comment = comment
     email_with_name = "#{@user.name} <#{@user.email}>"
-    mail(:to => email_with_name, :subject => "[Living Voters Guide] new comment on a #{@point.is_pro ? 'pro' : 'con'} point you wrote")
+    mail(:to => email_with_name, :subject => "[#{APP_CONFIG['email_head']}] new comment on a #{@point.is_pro ? 'pro' : 'con'} point you wrote")
   end  
 
 end

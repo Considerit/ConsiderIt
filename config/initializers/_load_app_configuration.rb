2904e46aeddc45db2e423c1723f703515ec3c7bd
Travis Kriplean <travis@cs.washington.edu>
2012-03-14 12:27:13 -0700

    working on local config solution
    Former-commit-id: 7d7e006475aa406c13296d4cfb2c31bb62aae376 [formerly 7b9102f1321af3eedb6c8e0151e730ac5827009a] [formerly eed2225b7e9b425ebfb1aef06eabe6bfbd3cdfc4 [formerly eed2225b7e9b425ebfb1aef06eabe6bfbd3cdfc4 [formerly 4aea4977523f0c8a54708d709d9f9474bc45e817]]]
    Former-commit-id: eaeeaf06625da85420335e2cbaf41cf7d425bf48 [formerly 90dcc9e56e3ccf9c0510d950be55880992608b2f]
    Former-commit-id: dd5b33d8e8b6b15de5cd0e38619e694da35173c6
    Former-commit-id: 06bf357f6536614775513443b44efc42d153b549

---
+++ config/initializers/_load_app_configuration.rb
@@ -0,0 +1,65 @@
+module Yamler
+  class Template
+    def optional_yaml(path)
+      path = File.extname(path) == '' ? "#{path}.yml" : path
+      require_yaml(path) if File.exists?(path)
+    end
+  end
+end
+
+require 'yamler'
+
+class Configuration
+  def self.load_yaml(path, opts = {})
+    yml = Yamler.load(path)
+
+    config = build_inheritance_chain yml, opts[:hash].to_s, opts[:inherit].to_s unless opts[:hash].nil? or opts[:inherit].nil?
+    apply_overrides yml, config, opts[:override_with].to_s unless opts[:override_with].nil?
+
+    config
+  end
+
+  def self.build_inheritance_chain(everything, selector, inheritance_attr)
+    selection = everything[selector]
+
+    if not selection[inheritance_attr].nil?
+      puts "Settings for '#{selector}' inherit '#{selection[inheritance_attr]}' via attribute '#{inheritance_attr}'"
+
+      inherited = build_inheritance_chain everything, selection[inheritance_attr], inheritance_attr
+      selection = inherited.recursive_merge(selection)
+      selection.delete(inheritance_attr)
+    end
+
+    selection
+  end
+
+  def self.apply_overrides(everything, config, selector)
+    overrides = everything[selector] 
+
+    return if overrides.nil?
+
+    puts "Applying configuration overrides from #{selector}"
+    config.recursive_merge(overrides)
+  end
+end
+
+class Hash
+  def recursive_merge(h)
+    self.merge!(h) do |key, _old, _new|
+      if _old.class == Hash
+        _old.recursive_merge(_new)
+      else
+        _new
+      end
+    end
+  end
+end
+
+config = Configuration.load_yaml("config/local_environment.yml")
+
+APP_CONFIG = config.merge(
+  Configuration.load_yaml("config/applications/#{config.application_name}.yml", :hash => Rails.env, :inherit => :default_to)
+
+
+# Save the configuration in a global, accessible from the application.
+LocalConfig = OPTIONS.merge _local_config

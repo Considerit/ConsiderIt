59d61d9a79c12dba127066d36e4113902b4a6f8d
Travis Kriplean <travis@cs.washington.edu>
2012-03-17 11:37:34 -0700

    removing paranoid versioned

--- config/initializers/versioned.rb
+++ config/initializers/versioned.rb
@@ -9,27 +9,29 @@ module ActiveRecord
         # not in the non-versioned table.
         self.class_eval do
           def destroy()
-            transaction {
-              # call the acts_as_paranoid delete function
-              self.class.delete_all(:id => self.id)
+            with_transaction_returning_status do
+              run_callbacks :destroy do
+                # call the acts_as_paranoid delete function
+                self.class.delete_all(:id => self.id)
 
-              # get the 'deleted' object
-              tmp = self.class.unscoped.find(id)
+                # get the 'deleted' object
+                tmp = self.class.unscoped.find(id)
 
-              # run it through the equivalent of acts_as_versioned's
-              # save_version(). We used to call that function but it is a
-              # noop when @saving_version is not set. That only gets done in
-              # a protected function set_new_version(). Easier to just
-              # replicate the meat of the save_version() function here.
-              rev = tmp.class.versioned_class.new
-              clone_versioned_model(tmp, rev)
-              rev.send("#{tmp.class.version_column}=", send(tmp.class.version_column))
-              rev.send("#{tmp.class.versioned_foreign_key}=", id)
-              rev.save
+                # run it through the equivalent of acts_as_versioned's
+                # save_version(). We used to call that function but it is a
+                # noop when @saving_version is not set. That only gets done in
+                # a protected function set_new_version(). Easier to just
+                # replicate the meat of the save_version() function here.
+                rev = tmp.class.versioned_class.new
+                clone_versioned_model(tmp, rev)
+                rev.send("#{tmp.class.version_column}=", send(tmp.class.version_column))
+                rev.send("#{tmp.class.versioned_foreign_key}=", id)
+                rev.save
 
-              # and finally really destroy the original
-              self.class.delete_all!(:id => self.id)
-            }
+                # and finally really destroy the original
+                self.class.delete_all!(:id => self.id)
+              end
+            end
           end
         end
 

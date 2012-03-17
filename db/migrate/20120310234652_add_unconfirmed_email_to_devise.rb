5152016570e346891e30d1f574a8e6197dae0dcb
Travis Kriplean <travis@cs.washington.edu>
2012-03-17 11:47:58 -0700

    temporary fix for migration issue
    Former-commit-id: e53aed88c2cd7ede4c7357d0460a55a6f60efe9c [formerly b3c83e0710b7968735f8ea5c9d25df7d77e09147] [formerly ff5e934b49d9e47f1b0f901a16757f65850122f6 [formerly ff5e934b49d9e47f1b0f901a16757f65850122f6 [formerly f812d8e1da922bd018efe6cc0cfe3e7ba415f386]]]
    Former-commit-id: e4dab48c61d4d672c51037137aaf15db672776cb [formerly 8afa5041087b93186d969cf53f10ad96f4ccd14e]
    Former-commit-id: 725fea0bcecf82255545d69f941a98872a3a3740
    Former-commit-id: 9041df831e0fbed3ecd648a461a78859982fb2d2

--- db/migrate/20120310234652_add_unconfirmed_email_to_devise.rb
+++ db/migrate/20120310234652_add_unconfirmed_email_to_devise.rb
@@ -2,6 +2,7 @@ class AddUnconfirmedEmailToDevise < ActiveRecord::Migration
   def change
     begin
       add_column :users, :unconfirmed_email, :string
+      add_column :users, :reset_password_sent_at, :datetime
     rescue
     end
   end

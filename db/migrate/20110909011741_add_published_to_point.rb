f6acf825912b474d661c7ddc234d2b03367bf807
Travis Kriplean <travis@cs.washington.edu>
2012-03-17 11:37:34 -0700

    removing paranoid versioned
    Former-commit-id: f6c8a3db0b8590179a274af233d827d60341f429 [formerly a421496fb587a7875e41f0d867d254beca05e37f] [formerly c7b9777c34155417b31ae1e3640e22a306598749 [formerly c7b9777c34155417b31ae1e3640e22a306598749 [formerly 51045301b5bbe00cc3b270ae847ecc33e804ca53]]]
    Former-commit-id: d98ac6a34c3d2e0a57f587329ff849340205177e [formerly 40debae8af09500fdbc08dffa77708a317740f18]
    Former-commit-id: 8f9e2a5e4ce18de2f6f2fafbb2e12f3cdce69b89
    Former-commit-id: cada61a3ab291db5521ea1a9fcf212c0ed2836f4

--- db/migrate/20110909011741_add_published_to_point.rb
+++ db/migrate/20110909011741_add_published_to_point.rb
@@ -1,11 +1,9 @@
 class AddPublishedToPoint < ActiveRecord::Migration
   def self.up
     add_column :points, :published, :boolean, :default => true
-    add_column :point_versions, :published, :boolean, :default => true
   end
 
   def self.down
     remove_column :points, :published
-    remove_column :point_versions, :published
   end
 end

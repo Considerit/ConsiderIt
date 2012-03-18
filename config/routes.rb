231e6cb6f877fd44abfef440e911c271812d6a46
Travis Kriplean <travis@cs.washington.edu>
2012-03-17 22:03:05 -0700

    subURI configuration tinkeringle bars
    Former-commit-id: 6a0d5c3ea04e8c822107341c55a24a7013f5496a [formerly 3a690b987084438ff9b9369f3f44bc6681c8b3ce] [formerly 38539ae46e3471762a16e0a21231605a249f0761 [formerly 38539ae46e3471762a16e0a21231605a249f0761 [formerly 10b8fe50e7087c4125882fd00471e0f3681881c7]]]
    Former-commit-id: 3d030287a377ffe9970db63b9414f67dc04b6616 [formerly cd94f24e601afd6a77c4b32753a7cfff090edc04]
    Former-commit-id: aadd6005a56e021bb7fd29eee320a0c2be238d16
    Former-commit-id: 57b0a95400be7d1fba536da2cc092acf3419f44f

--- config/routes.rb
+++ config/routes.rb
@@ -1,51 +1,48 @@
 ConsiderIt::Application.routes.draw do
   
-  #scope ENV['RAILS_RELATIVE_URL_ROOT'] || '/' do
-  scope '/' do
-    resources :point_links
+  resources :point_links
 
-    root :to => "home#index"
-    
-    resources :proposals, :only => [:show, :index] do
-      resources :positions, :only => [:new, :edit, :create, :update, :show, :destroy]
-      resources :points, :only => [:index, :create, :update, :destroy, :show] do 
-        resources :inclusions, :only => [:create] 
-      end
-      resources :point_similarities, :module => :admin
-      resources :comments, :only => [:index, :create]
+  root :to => "home#index"
+  
+  resources :proposals, :only => [:show, :index] do
+    resources :positions, :only => [:new, :edit, :create, :update, :show, :destroy]
+    resources :points, :only => [:index, :create, :update, :destroy, :show] do 
+      resources :inclusions, :only => [:create] 
     end
+    resources :point_similarities, :module => :admin
+    resources :comments, :only => [:index, :create]
+  end
 
-    devise_for :users, :controllers => { 
-      :omniauth_callbacks => "users/omniauth_callbacks", 
-      :sessions => "users/sessions", 
-      :registrations => "users/registrations",
-      :passwords => "users/passwords",
-      :confirmations => "users/confirmations"
-    } do 
+  devise_for :users, :controllers => { 
+    :omniauth_callbacks => "users/omniauth_callbacks", 
+    :sessions => "users/sessions", 
+    :registrations => "users/registrations",
+    :passwords => "users/passwords",
+    :confirmations => "users/confirmations"
+  } do 
 
-      match "users/check_login_info" => "users/registrations#check_login_info", :via => :post
+    match "users/check_login_info" => "users/registrations#check_login_info", :via => :post
 
-    end
-    #, :path => ENV.fetch('RAILS_RELATIVE_URL_ROOT', '')
+  end
+  #, :path => ENV.fetch('RAILS_RELATIVE_URL_ROOT', '')
 
-    themes_for_rails # themes_for_rails gem routes 
+  themes_for_rails # themes_for_rails gem routes 
 
-    #match "/theme" => "theme#set", :via => :post
-    match "/home/domain" => "home#set_domain", :via => :post
-    match '/home/:page' => "home#show", :via => :get, :constraints => { :page => /terms-of-use|considerit|media|videos|help/ } 
+  #match "/theme" => "theme#set", :via => :post
+  match "/home/domain" => "home#set_domain", :via => :post
+  match '/home/:page' => "home#show", :via => :get, :constraints => { :page => /terms-of-use|considerit|media|videos|help/ } 
 
-    match '/home/study/:category' => "home#study", :via => :post  
-    match '/admin/dashboard' => "admin/dashboard#index", :via => :get, :module => :admin
+  match '/home/study/:category' => "home#study", :via => :post  
+  match '/admin/dashboard' => "admin/dashboard#index", :via => :get, :module => :admin
 
-    namespace :reflect do
-      match "/data" => "reflect_bullet#index", :via => :get
-      match "/bullet_new" => 'reflect_bullet#create', :via => :post
-      match "/bullet_update" => 'reflect_bullet#update', :via => :post
-      match "/bullet_delete" => 'reflect_bullet#destroy', :via => :post
-      match "/response_new" => 'reflect_response#create', :via => :post
-      match "/response_update" => 'reflect_response#update', :via => :post
-      match "/response_delete" => 'reflect_response#destroy', :via => :post              
-    end
+  namespace :reflect do
+    match "/data" => "reflect_bullet#index", :via => :get
+    match "/bullet_new" => 'reflect_bullet#create', :via => :post
+    match "/bullet_update" => 'reflect_bullet#update', :via => :post
+    match "/bullet_delete" => 'reflect_bullet#destroy', :via => :post
+    match "/response_new" => 'reflect_response#create', :via => :post
+    match "/response_update" => 'reflect_response#update', :via => :post
+    match "/response_delete" => 'reflect_response#destroy', :via => :post              
   end
 
 end

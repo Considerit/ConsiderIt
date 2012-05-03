69d475b3b85596ed96cd6f1622b31d5d7e9358f7
Travis Kriplean <travis@cs.washington.edu>
2012-05-02 17:36:50 -0700

    moving all settings to Account in database
    Former-commit-id: e3bcf021ae4b55dfb1e1bd9a10a85e8a2408fa1f [formerly a59fe69186fab91af6ff07013e1d3e6ff5eef52c] [formerly 2a6f0c45c2ceb5fbf66fe8b5fce82f42e05a0b28 [formerly 2a6f0c45c2ceb5fbf66fe8b5fce82f42e05a0b28 [formerly edb902325d159ad493b3234f9dec885feab8c47f]]]
    Former-commit-id: f893f46fb163b2ed2e96f3455a88623c07546ad4 [formerly 74fe6f6e67cbfec13a5f6fb26ddbe7a684414988]
    Former-commit-id: 63621839b10d344c37ac1ecc0f4cbf644031ccdc
    Former-commit-id: a47912b24edfe14b16638b76f2d96398fc52892c

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,18 +1,18 @@
 class UserMailer < ActionMailer::Base
-  default :from => APP_CONFIG[:default_from_email] 
+  default :from => current_tenant.app_notification_email
 
   def proposal_subscription(user, pnt)
     @user = user
     @point = pnt
     email_with_name = "#{@user.name} <#{@user.email}>"
-    mail(:to => email_with_name, :subject => "[#{APP_CONFIG[:email_head]}] new #{@point.is_pro ? 'pro' : 'con'} point for #{@point.proposal.category} #{@point.proposal.designator}")
+    mail(:to => email_with_name, :subject => "[current_tenant.app_title] new #{@point.is_pro ? 'pro' : 'con'} point for #{@point.proposal.category} #{@point.proposal.designator}")
   end
 
   def position_subscription(user, position)
     @user = user
     @position = position
     email_with_name = "#{@user.name} <#{@user.email}>"
-    mail(:to => email_with_name, :subject => "[#{APP_CONFIG[:email_head]}] new review #{@point.proposal.category} for #{@position.proposal.category} #{@position.proposal.designator}")
+    mail(:to => email_with_name, :subject => "[current_tenant.app_title] new review #{@point.proposal.category} for #{@position.proposal.category} #{@position.proposal.designator}")
   end  
 
   def someone_discussed_your_point(user, pnt, comment)
@@ -20,7 +20,7 @@ class UserMailer < ActionMailer::Base
     @point = pnt
     @comment = comment
     email_with_name = "#{@user.name} <#{@user.email}>"
-    mail(:to => email_with_name, :subject => "[#{APP_CONFIG[:email_head]}] new comment on a #{@point.is_pro ? 'pro' : 'con'} point you wrote")
+    mail(:to => email_with_name, :subject => "[current_tenant.app_title] new comment on a #{@point.is_pro ? 'pro' : 'con'} point you wrote")
   end
 
   def someone_discussed_your_position(user, position, comment)
@@ -28,7 +28,7 @@ class UserMailer < ActionMailer::Base
     @position = position
     @comment = comment
     email_with_name = "#{@user.name} <#{@user.email}>"
-    mail(:to => email_with_name, :subject => "[#{APP_CONFIG[:email_head]}] new comment on your review")
+    mail(:to => email_with_name, :subject => "[current_tenant.app_title] new comment on your review")
   end  
 
   def someone_commented_on_thread(user, obj, comment)
@@ -38,10 +38,10 @@ class UserMailer < ActionMailer::Base
 
     if obj.commentable_type == 'Point'
       @point = obj
-      mail(:to => email_with_name, :subject => "[#{APP_CONFIG[:email_head]}] #{@comment.user.name} also commented on #{@point.user.name}'s #{@point.is_pro ? 'pro' : 'con'} point")
+      mail(:to => email_with_name, :subject => "[current_tenant.app_title] #{@comment.user.name} also commented on #{@point.user.name}'s #{@point.is_pro ? 'pro' : 'con'} point")
     elsif obj.commentable_type == 'Position'
       @position = obj
-      mail(:to => email_with_name, :subject => "[#{APP_CONFIG[:email_head]}] #{@comment.user.name} also commented on #{@position.user.name}'s review")
+      mail(:to => email_with_name, :subject => "[current_tenant.app_title] #{@comment.user.name} also commented on #{@position.user.name}'s review")
     end
   end
 
@@ -50,7 +50,7 @@ class UserMailer < ActionMailer::Base
     @point = pnt
     @comment = comment
     email_with_name = "#{@user.name} <#{@user.email}>"
-    mail(:to => email_with_name, :subject => "[#{APP_CONFIG[:email_head]}] new comment on a #{@point.is_pro ? 'pro' : 'con'} point you wrote")
+    mail(:to => email_with_name, :subject => "[current_tenant.app_title] new comment on a #{@point.is_pro ? 'pro' : 'con'} point you wrote")
   end
 
   def someone_reflected_your_point(user, bullet, comment)
@@ -59,7 +59,7 @@ class UserMailer < ActionMailer::Base
     @comment = comment
     email_with_name = "#{@user.name} <#{@user.email}>"
         
-    mail(:to => email_with_name, :subject => "[#{APP_CONFIG[:email_head]}] #{@bullet.user.name} summarized your comment")
+    mail(:to => email_with_name, :subject => "[current_tenant.app_title] #{@bullet.user.name} summarized your comment")
   end
 
   def your_reflection_was_responded_to(user, response, bullet, comment)
@@ -68,7 +68,7 @@ class UserMailer < ActionMailer::Base
     @comment = comment
     @response = response
     email_with_name = "#{@user.name} <#{@user.email}>"
-    mail(:to => email_with_name, :subject => "[#{APP_CONFIG[:email_head]}] #{@comment.user.name} responded to your summary")
+    mail(:to => email_with_name, :subject => "[current_tenant.app_title] #{@comment.user.name} responded to your summary")
   end
 
 end
--- config/initializers/_0_load_app_configuration.rb
+++ config/initializers/_0_load_app_configuration.rb
@@ -60,8 +60,8 @@ def recursive_symbolize_keys! hash
   hash.values.select{|v| v.is_a? Hash}.each{|h| recursive_symbolize_keys!(h)}
 end
 
-config = Configuration.load_yaml "config/local_environment.yml", :hash => Rails.env, :inherit => :default_to
-APP_CONFIG = config.merge(
-  Configuration.load_yaml("config/applications/#{config['application_name']}.yml", :hash => Rails.env, :inherit => :default_to))
+APP_CONFIG = Configuration.load_yaml "config/local_environment.yml", :hash => Rails.env, :inherit => :default_to
+# APP_CONFIG = config.merge(
+  # Configuration.load_yaml("config/applications/#{config['application_name']}.yml", :hash => Rails.env, :inherit => :default_to))
 
 recursive_symbolize_keys! APP_CONFIG

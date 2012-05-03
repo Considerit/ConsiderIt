152b679fe674d25f15bc0ef36d986c592e156684
Travis Kriplean <travis@cs.washington.edu>
2012-05-03 12:59:35 -0700

    renamed badly name Account col, and added a requires pledge field
    Former-commit-id: 190b51f762ecf043e4fee6c1e264624509863d8f [formerly 10068f851ffea8048bf9fa14615044525f1cba24] [formerly 50acc699833f7e6d0bd7fecf630a3933b2cf39ba [formerly 50acc699833f7e6d0bd7fecf630a3933b2cf39ba [formerly 3eb4adb124b6072af98f51fcb0cef4260b65a38d]]]
    Former-commit-id: 68e4b9c74b4f03ea68196814f3d571487b903694 [formerly cc53f238f337e8a62587b3a164843446f6feed3b]
    Former-commit-id: 540a4b699c74a9b0efb63da9a24ed293ae50a83b
    Former-commit-id: a74f993444116898ef55dbb043248e0d7cea7763

--- db/migrate/20120503185151_rename_account_field.rb
+++ db/migrate/20120503185151_rename_account_field.rb
@@ -1,7 +1,6 @@
 class RenameAccountField < ActiveRecord::Migration
-  def up
-  end
-
-  def down
+  def change
+    rename_column :accounts, :socmedia_facebook_id, :socmedia_facebook_client
+    add_column :accounts, :requires_civility_pledge_on_registration, :boolean, :default => false
   end
 end

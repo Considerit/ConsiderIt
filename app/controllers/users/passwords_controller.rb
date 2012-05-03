76c86603e5cbbf155c7ddc7d16e9f85a7effd2ea
Travis Kriplean <travis@cs.washington.edu>
2012-05-03 16:52:30 -0700

    messing with user devise mails

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,7 +1,18 @@
 
 
 class Users::PasswordsController < Devise::PasswordsController
-  
+
+  # POST /resource/password
+  def create
+    self.resource = resource_class.send_reset_password_instructions(params[resource_name], current_tenant.contact_email)
+
+    if successfully_sent?(resource)
+      respond_with({}, :location => after_sending_reset_password_instructions_path_for(resource_name))
+    else
+      respond_with(resource)
+    end
+  end
+
   protected
     # The path used after sending reset password instructions
     def after_sending_reset_password_instructions_path_for(resource_name)

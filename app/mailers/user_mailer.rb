a744ca0e598a0916721faf635b26ab7b3ad8554d
Travis Kriplean <travis@cs.washington.edu>
2012-05-11 02:15:33 -0500

    message queuing system for offline sending of email
    Former-commit-id: 96d3300879e100597d8372978d5c78ef53a40c90 [formerly a0d00e8e74e36bc6760b4a135eeeb19dd9deffa9] [formerly e406aac5e7814b482f2b6551e61212f738751e03 [formerly e406aac5e7814b482f2b6551e61212f738751e03 [formerly 762fedbae31c7fb04aa0d3327261ab63f713a7f3]]]
    Former-commit-id: 0bd866bf33b773f31996be6b720cbf1bc5fec2f4 [formerly c28d6e30dfdac8e09446e38eb0975625040ae94d]
    Former-commit-id: f81093c50b556f26f33a4d65a0925b27f725c825
    Former-commit-id: 96b01c5fda49736f01657fb62a47a13b1e80382e

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,74 +1,74 @@
 class UserMailer < ActionMailer::Base
   include Devise::Mailers::Helpers
 
-  def proposal_subscription(user, pnt, from)
+  def proposal_subscription(user, pnt, from, app_title)
     @user = user
     @point = pnt
     email_with_name = "#{@user.name} <#{@user.email}>"
-    mail(:from => from, :to => email_with_name, :subject => "[#{current_tenant.app_title}] new #{@point.is_pro ? 'pro' : 'con'} point for \"#{@point.proposal.title}\"")
+    mail(:from => from, :to => email_with_name, :subject => "[#{app_title}] new #{@point.is_pro ? 'pro' : 'con'} point for \"#{@point.proposal.title}\"")
   end
 
-  def position_subscription(user, position, from)
+  def position_subscription(user, position, from, app_title)
     @user = user
     @position = position
     email_with_name = "#{@user.name} <#{@user.email}>"
-    mail(:from => from, :to => email_with_name, :subject => "[#{current_tenant.app_title}] new review #{@point.proposal.category} for \"#{@point.proposal.title}\"")
+    mail(:from => from, :to => email_with_name, :subject => "[#{app_title}] new review #{@point.proposal.category} for \"#{@point.proposal.title}\"")
   end  
 
-  def someone_discussed_your_point(user, pnt, comment, from)
+  def someone_discussed_your_point(user, pnt, comment, from, app_title)
     @user = user
     @point = pnt
     @comment = comment
     email_with_name = "#{@user.name} <#{@user.email}>"
-    mail(:from => from, :to => email_with_name, :subject => "[#{current_tenant.app_title}] new comment on a #{@point.is_pro ? 'pro' : 'con'} point you wrote")
+    mail(:from => from, :to => email_with_name, :subject => "[#{app_title}] new comment on a #{@point.is_pro ? 'pro' : 'con'} point you wrote")
   end
 
-  def someone_discussed_your_position(user, position, comment, from)
+  def someone_discussed_your_position(user, position, comment, from, app_title)
     @user = user
     @position = position
     @comment = comment
     email_with_name = "#{@user.name} <#{@user.email}>"
-    mail(:from => from, :to => email_with_name, :subject => "[#{current_tenant.app_title}] new comment on your review")
+    mail(:from => from, :to => email_with_name, :subject => "[#{app_title}] new comment on your review")
   end  
 
-  def someone_commented_on_thread(user, obj, comment, from)
+  def someone_commented_on_thread(user, obj, comment, from, app_title)
     @user = user
     @comment = comment
     email_with_name = "#{@user.name} <#{@user.email}>"
 
     if obj.commentable_type == 'Point'
       @point = obj
-      mail(:from => from, :to => email_with_name, :subject => "[#{current_tenant.app_title}] #{@comment.user.name} also commented on #{@point.user.name}'s #{@point.is_pro ? 'pro' : 'con'} point")
+      mail(:from => from, :to => email_with_name, :subject => "[#{app_title}] #{@comment.user.name} also commented on #{@point.user.name}'s #{@point.is_pro ? 'pro' : 'con'} point")
     elsif obj.commentable_type == 'Position'
       @position = obj
-      mail(:from => from, :to => email_with_name, :subject => "[#{current_tenant.app_title}] #{@comment.user.name} also commented on #{@position.user.name}'s review")
+      mail(:from => from, :to => email_with_name, :subject => "[#{app_title}] #{@comment.user.name} also commented on #{@position.user.name}'s review")
     end
   end
 
-  def someone_commented_on_an_included_point(user, pnt, comment, from)
+  def someone_commented_on_an_included_point(user, pnt, comment, from, app_title)
     @user = user
     @point = pnt
     @comment = comment
     email_with_name = "#{@user.name} <#{@user.email}>"
-    mail(:from => from, :to => email_with_name, :subject => "[#{current_tenant.app_title}] new comment on a #{@point.is_pro ? 'pro' : 'con'} point you wrote")
+    mail(:from => from, :to => email_with_name, :subject => "[#{app_title}] new comment on a #{@point.is_pro ? 'pro' : 'con'} point you wrote")
   end
 
-  def someone_reflected_your_point(user, bullet, comment, from)
+  def someone_reflected_your_point(user, bullet, comment, from, app_title)
     @user = user
     @bullet = bullet
     @comment = comment
     email_with_name = "#{@user.name} <#{@user.email}>"
         
-    mail(:from => from, :to => email_with_name, :subject => "[#{current_tenant.app_title}] #{@bullet.user.name} summarized your comment")
+    mail(:from => from, :to => email_with_name, :subject => "[#{app_title}] #{@bullet.user.name} summarized your comment")
   end
 
-  def your_reflection_was_responded_to(user, response, bullet, comment, from)
+  def your_reflection_was_responded_to(user, response, bullet, comment, from, app_title)
     @user = user
     @bullet = bullet
     @comment = comment
     @response = response
     email_with_name = "#{@user.name} <#{@user.email}>"
-    mail(:from => from, :to => email_with_name, :subject => "[#{current_tenant.app_title}] #{@comment.user.name} responded to your summary")
+    mail(:from => from, :to => email_with_name, :subject => "[#{app_title}] #{@comment.user.name} responded to your summary")
   end
 
   ######### DEVISE MAILERS

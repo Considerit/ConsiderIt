196a3fcdcc706ed306799a23850b856902e0e5bd
Travis Kriplean <travis@cs.washington.edu>
2012-06-06 16:07:03 -0700

    tagging system for proposals

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120603165745) do
+ActiveRecord::Schema.define(:version => 20120606205648) do
 
   create_table "accounts", :force => true do |t|
     t.string   "identifier"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(:version => 20120603165745) do
     t.boolean  "requires_civility_pledge_on_registration", :default => false
     t.integer  "followable_last_notification_milestone"
     t.datetime "followable_last_notification"
+    t.string   "default_hashtags"
   end
 
   create_table "active_admin_comments", :force => true do |t|
@@ -501,6 +502,25 @@ ActiveRecord::Schema.define(:version => 20120603165745) do
     t.boolean  "bval"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "account_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context",       :limit => 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", :force => true do |t|
+    t.string  "name"
+    t.integer "account_id"
   end
 
   create_table "users", :force => true do |t|

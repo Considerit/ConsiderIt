4c275b31de86a3fd798e4a316f1f8b8005f69522
Travis Kriplean <travis@cs.washington.edu>
2012-06-13 19:45:07 -0700

    input cleanup, trashing blogademia theme
    Former-commit-id: 7723ad53776d8db99a2c6368dbce66cff6a2a52d [formerly 3b20e90bc17ad403cef94d52a4066adaaefffea9] [formerly 662924fcc07e1264a258a2785b9b9a7b3b0ca1b3 [formerly 662924fcc07e1264a258a2785b9b9a7b3b0ca1b3 [formerly 619610afe8a2800a3bf5e12ffc2ce2fc313f154f]]]
    Former-commit-id: a1558c165f134c50c06284eb2367c03216efee51 [formerly 32db162027d5735a0e314280de01320749840855]
    Former-commit-id: d21cfaf5ca0ddf27122c8e4826b988579c037214
    Former-commit-id: d54f9f12e0921cc43c2576e342f89710ca035018

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,6 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require jquery.noblecount.min.js
-//= require jquery.qtip-1.0.0-rc3.js
 //= require jquery.example.min.js
 //= require jquery.jcarousel.min.js
 //= require jquery.autoresize.js

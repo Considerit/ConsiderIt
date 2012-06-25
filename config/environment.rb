7feff83167c50fbeb33c721aafe60f32cb84d677
Travis Kriplean <travis@cs.washington.edu>
2012-06-24 18:25:13 -0700

    fixing vexing position overwriting bug
    Former-commit-id: ffc870f86518a15d086e4df7643b48065640e178 [formerly b1ce78e004804479a6cb9df5f8f4828578f13748] [formerly 8bd185de0a0f62da2057ee6436e8c1b36300c1f8 [formerly 8bd185de0a0f62da2057ee6436e8c1b36300c1f8 [formerly 61d2beb42fea907b2bcf700a1a01fc9b9bb9c75e]]]
    Former-commit-id: 6ac27808db8a1c561a14370483f0aac20a7961c7 [formerly b02ffb55e69ba9e8224f390f548e5bd58d853b67]
    Former-commit-id: f5ed7057e2f50a8ae98c3b6d6a718fbe47ceaa1e
    Former-commit-id: 9fbdd90862d2d1bf4c4259dec3155b516627fc7a

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,8 @@ require File.expand_path('../application', __FILE__)
 ConsiderIt::Application.initialize! do |config|
   config.serve_static_assets = true    
 end
+
+if "irb" == $0
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
+  ActiveSupport::Cache::Store.logger = Logger.new(STDOUT)
+end

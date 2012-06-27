f7c7404fe71bdddb98f978ee0fdfeaacfc56bcc3
Travis Kriplean <travis@cs.washington.edu>
2012-06-27 13:48:29 -0700

    better default email
    Former-commit-id: acaaaf1b3712836d493c2956c259e5f39bb4d2f8 [formerly db2ff6848d9962571d097dd4b1849377da1b9ea9] [formerly 5c2d64993767d33f79bbfa0cb11586ec4a8becc7 [formerly 5c2d64993767d33f79bbfa0cb11586ec4a8becc7 [formerly f67b528515ddf5145c3976864533f880d37b72da]]]
    Former-commit-id: eaeaf5acadeba458f63add67343491fc43191f15 [formerly 91cc55fcbe6e617f14e5f9ae4d2561addfb5fa1f]
    Former-commit-id: 0db047e93c3bc288e698396f7eb62f3a24bd42ff
    Former-commit-id: 970a493f864ba66b1dc3a87b34b497cfe6d9203e

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ ConsiderIt::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :address => 'localhost' }
   config.action_mailer.perform_deliveries = true 
-  #config.action_mailer.delivery_method = :sendmail  
+  #config.action_mailer.delivery_method = :mailhopper  
 
   # Enable threaded mode
   # config.threadsafe!

621fdaa7cf946ba3e4dbdbe6ec1e66064dbbbf88
Travis Kriplean <travis@cs.washington.edu>
2012-06-27 11:32:06 -0700

    whenever is now tracking delayed job

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,20 +3,31 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
 env :MAILTO, '""' 
+env :PATH, ENV['PATH']
+
+set :output, ":path/log/cron_log.log"
+
+job_type :envcommand, 'cd :path && RAILS_ENV=:environment :task'
+
+# on some systems, need to source rvm before running standard rake command
+#job_type :rake, "source ~/.rvm/scripts/rvm; cd :path && RAILS_ENV=:environment bundle exec rake :task --silent :output"
 
 every 5.minutes do
-  rake "cache:points"
-  rake "cache:proposals"
+  rake "compute_metrics"
 end
 
 # backup database
-every 1.week do
+every :week do
   command "backup perform --trigger my_backup"
+end
+
+every :reboot do
+  envcommand 'script/delayed_job restart'
+end
+
+every :day, :at => '1:30 am' do
+  envcommand 'script/delayed_job restart'
 end
 
 # Learn more: http://github.com/javan/whenever

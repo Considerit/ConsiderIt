81cf3406f797c99895e6078949f3a6e5bfd44082
Travis Kriplean <travis@cs.washington.edu>
2012-06-28 10:53:40 -0700

    (semi)efficient point score calculation
    Former-commit-id: 4a3df5ae439c7b390704210e9289169ed7b56810 [formerly 0755724222316657a57316827ae986b59e5d69e0] [formerly 22f41f00fbf6dad0106e109f820a510063f1f78b [formerly 22f41f00fbf6dad0106e109f820a510063f1f78b [formerly 0bae6abad05f39a987e47902e36f7c5a860e918d]]]
    Former-commit-id: e2601cd56f64a3df3fc67f5c9581e6e0c58b2723 [formerly 8d497c7c207233b86b875bfbd579511b26d84002]
    Former-commit-id: 94b15599f6ddb9832c90a3c832b4170eeb164edc
    Former-commit-id: 952c357790b26f88779dc6b046d1d09c98a42c0a

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,5 @@
+require 'mail'
+
 class UserMailer < ActionMailer::Base
   ActionMailer::Base.delivery_method = :mailhopper  
   layout 'email'
@@ -12,50 +14,32 @@ class UserMailer < ActionMailer::Base
     email_with_name = "#{@user.username} <#{@user.email}>"
 
     subject = "please confirm your email"
-    
-    mail(:from => options[:from], :to => email_with_name, :subject => "[#{options[:app_title]}] #{subject}")
+    from = format_email(options[:from], options[:app_title])
+    mail(:from => from, :to => email_with_name, :subject => "[#{options[:app_title]}] #{subject}")
 
   end
 
-  def reset_password_instructions(record)
-    from_email = record.account.contact_email
-    devise_mail(record, :reset_password_instructions, from_email)
+  def reset_password_instructions(user)
+    @user = user
+    @host = options[:host]
+    @options = options
+    email_with_name = "#{@user.username} <#{@user.email}>"
+    subject = "password reset instructions"
+    from = format_email(options[:from], options[:app_title])
+    mail(:from => from, :to => email_with_name, :subject => "[#{options[:app_title]}] #{subject}")
   end
 
-end
+  private
 
-#taken from https://github.com/plataformatec/devise/blob/master/lib/devise/mailers/helpers.rb
-module Devise
-  module Mailers
-    module Helpers
-      protected
-
-      # Configure default email options
-      def devise_mail(record, action, from_email = "no-reply@#{APP_CONFIG[:host]}")
-        @scope_name = Devise::Mapping.find_scope!(record)
-        @resource   = instance_variable_set("@#{devise_mapping.name}", record)
-        mail headers_for(action, from_email)
+    def format_email(addr, name = nil)
+      address = Mail::Address.new addr # ex: "john@example.com"
+      if name
+        address.display_name = name # ex: "John Doe"
       end
+      # Set the From or Reply-To header to the following:
+      address.format # returns "John Doe <john@example.com>"
 
-      def headers_for(action, from_email)
-
-        headers = {
-          :subject       => translate(devise_mapping, action),
-          :from          => from_email,
-          :to            => resource.email,
-          :template_path => template_paths
-        }
-
-        if resource.respond_to?(:headers_for)
-          headers.merge!(resource.headers_for(action))
-        end
-
-        unless headers.key?(:reply_to)
-          headers[:reply_to] = headers[:from]
-        end
-
-        headers
-      end
     end
-  end
+
 end
+

117fd621f07a8bd0896c000b50fa0399337dfd61
Travis Kriplean <travis@cs.washington.edu>
2012-07-02 11:06:12 -0700

    updating gems
    Former-commit-id: aec17b136e31395b97dfe6fbfe309878b446389e [formerly 00d2a2e44283c923b62cbf795c2522d79a60cd57] [formerly c1ce63225877a98a44de8aa2052def3fc199f417 [formerly c1ce63225877a98a44de8aa2052def3fc199f417 [formerly 8abcb03f94e99f3c4791888c892574a693822981]]]
    Former-commit-id: 67566be3aeba394d2240c88a07a10415fe92606a [formerly 515f90890fc761df8a8592c174f6a9fcd6d2170b]
    Former-commit-id: f24e53b2db5082445d09cf22135dac673eef11a5
    Former-commit-id: 8cf9d8461ad8a73e16070ee98b2442854951e232

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,11 +10,12 @@ ConsiderIt::Application.configure do
   config.whiny_nils = true
 
   # Do not compress assets
-  config.assets.compress = true #false
+  config.assets.compress = false
    
   # Expands the lines which load the assets
-  config.assets.debug = false #true
-  config.assets.compile = true
+  config.assets.debug = true
+  
+  #config.assets.compile = true
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true

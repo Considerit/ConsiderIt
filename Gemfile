98d975b0312880f238e0e02cf89c8adca52df5e7
Travis Kriplean <travis@cs.washington.edu>
2012-07-03 11:38:05 -0700

    CDN
    Former-commit-id: e3d46ac9a33281ff21a5e1d9a3d4a86644712bf3 [formerly f8b1a4d9b1841fbc570ea619e89a08320f567310] [formerly aae6ca01475b514dec5179f8222f5be8202e70a3 [formerly aae6ca01475b514dec5179f8222f5be8202e70a3 [formerly c582429e2f3eab3e86638821bd68353f215b796a]]]
    Former-commit-id: 4c8d038fcdaeb043bbf3e76b985045b186b92dbb [formerly ce4ff0e0f5858be0105381faa352eeb8a3d04e47]
    Former-commit-id: 44c1321d776c815c91bda84006c870a52c528556
    Former-commit-id: af687ddb6e0db3e2eed41c04b8cf243e009d719e

--- Gemfile
+++ Gemfile
@@ -97,4 +97,5 @@ group :assets do
   gem 'sass-rails',   "~> 3.2.3"
   gem 'coffee-rails', "~> 3.2.1"
   gem 'uglifier'
+  gem "asset_sync"
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ ConsiderIt::Application.configure do
   config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  # config.action_controller.asset_host = "//#{APP_CONFIG[:aws][:fog_directory]}.s3.amazonaws.com"
 
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = true

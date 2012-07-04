7dbd29b36ecee09059a412a98350eded2a36032c
Travis Kriplean <travis@cs.washington.edu>
2012-07-03 17:06:47 -0700

    CDNpt2
    Former-commit-id: 788e219ec4f899319f755d5702c413e1e21e9cca [formerly 3b3f24f7eb3c1a24015c742b07c50d4d5ff0b205] [formerly c505fc81ab48ac61ec57c80ff56edae58b49eeac [formerly c505fc81ab48ac61ec57c80ff56edae58b49eeac [formerly a0514e61aadfd19c532fa4d94c9691dc95496697]]]
    Former-commit-id: dcd5212721dcecba4a360b677b0a8645e6d838fb [formerly 37d4236cc7306f88f751feb742b8510a1e8b558e]
    Former-commit-id: 8dfcf96c5f37de471c83dc6a33c7c4d3ad9199ba
    Former-commit-id: 660fd13116017ded7f4c6575917b3dc4bdc91129

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require 'pp'
-
+require './config/local_environment'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line

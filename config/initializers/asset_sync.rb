b50962f7fb273ccf2b97cd3f8cb43236100fd9f9
Travis Kriplean <travis@cs.washington.edu>
2012-07-03 17:08:48 -0700

    CDNpt2
    Former-commit-id: fb61e601bdd5bb65746602a73ba48f58a7a4d43a [formerly 1632381c869a9285c7b852e9610599694ed21797] [formerly 2cd3ef96c18079d2e3b16eb7e75d3bf1600b112a [formerly 2cd3ef96c18079d2e3b16eb7e75d3bf1600b112a [formerly 260ffcb4368483f04af0c26ac859e3995fae7984]]]
    Former-commit-id: 07d8c0ccac427d01308b5b3a93672db7acfb0537 [formerly b4e5eff625667092cbec78bd53ff959093e4ee68]
    Former-commit-id: 18b7136b4c0f32f397f16459e2abcd97889e049e
    Former-commit-id: 21bf881d277b7aef8eaa3e45cbf1db654232657d

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -6,7 +6,7 @@ if defined?(AssetSync)
     config.fog_directory = APP_CONFIG.has_key?(:aws) ? APP_CONFIG[:aws][:fog_directory] : ''
     
     # Increase upload performance by configuring your region
-    config.fog_region = 'us-west-2'
+    #config.fog_region = 'us-west-2'
     #
     # Don't delete files from the store
     # config.existing_remote_files = "keep"

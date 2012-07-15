9f56fe5c5457936f2c3f29e4cd68629ee6044866
Travis Kriplean <travis@cs.washington.edu>
2012-07-15 13:40:57 -0700

    first pass on homepage for directrep
    Former-commit-id: b0a8e718c7f123c9b8ae68a856d0895a57612baf [formerly d47b904a31e9bfb3d08521eb72ef99e587957689] [formerly 330e67f5c80bd544844a4a4dab73655568ecb772 [formerly 330e67f5c80bd544844a4a4dab73655568ecb772 [formerly 7acd444ef64450450d28b501e0a683eadd150e58]]]
    Former-commit-id: af48652bf7e275d3dd2163e7efd4c1a0338d082c [formerly b37c1b8f9064968c19b3da4b306b3d37c7afae15]
    Former-commit-id: a9e100bc4a4119f7aca2c8de564267b0bf282057
    Former-commit-id: 616b41cbb745cc05d4d440e67f25298ae0a34c84

--- app/assets/themes/directrep/javascripts/considerit.theme.js
+++ app/assets/themes/directrep/javascripts/considerit.theme.js
@@ -1,8 +1,11 @@
-$j('.homepage #proposals .proposals').infiniteCarousel({
-  speed: 1500,
-  vertical: false,
-  total_items: 8,
-  items_per_page: 8,
-  loading_from_ajax: false,
-  dim: 720
-});    
+
+$j(document).ready(function(){
+  $j('.homepage #proposals .proposals').infiniteCarousel({
+    speed: 1500,
+    vertical: false,
+    total_items: 3,
+    items_per_page: 3,
+    loading_from_ajax: false,
+    dim: 700
+  });    
+});

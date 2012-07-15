7acd444ef64450450d28b501e0a683eadd150e58
Travis Kriplean <travis@cs.washington.edu>
2012-07-15 13:40:57 -0700

    first pass on homepage for directrep

--- app/assets/themes/lvg/javascripts/application.js
+++ app/assets/themes/lvg/javascripts/application.js
@@ -3,20 +3,3 @@
 *= require lvg/javascripts/considerit.theme
 */
 
-$j('#intro .proposals.horizontal').infiniteCarousel({
-  speed: 1500,
-  vertical: false,
-  total_items: 8,
-  items_per_page: 8,
-  loading_from_ajax: false,
-  dim: 900
-});
-
-$j('.proposal_prompt .proposals.horizontal').infiniteCarousel({
-  speed: 1500,
-  vertical: false,
-  total_items: 8,
-  items_per_page: 8,
-  loading_from_ajax: false,
-  dim: 720
-});    
---
+++ app/assets/themes/lvg/javascripts/considerit.theme.js
@@ -0,0 +1,21 @@
+$j(document).ready(function(){
+
+  $j('#intro .proposals.horizontal').infiniteCarousel({
+    speed: 1500,
+    vertical: false,
+    total_items: 8,
+    items_per_page: 8,
+    loading_from_ajax: false,
+    dim: 900
+  });
+
+  $j('.proposal_prompt .proposals.horizontal').infiniteCarousel({
+    speed: 1500,
+    vertical: false,
+    total_items: 8,
+    items_per_page: 8,
+    loading_from_ajax: false,
+    dim: 720
+  });    
+
+});

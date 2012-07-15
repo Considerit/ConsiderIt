8d87676c802ee4e0e039ba632b90cb518bf39446
Travis Kriplean <travis@cs.washington.edu>
2012-07-15 12:20:31 -0700

    theme javascript enabled
    Former-commit-id: 8b577182d62b57c1f419b705a91bf0ca8ca32306 [formerly aaf222a1b7d1b52eaf6b8ebe4cb628bbe91fab1c] [formerly f3a933cc7757da41fca9d78c3af5981fafcc0e7a [formerly f3a933cc7757da41fca9d78c3af5981fafcc0e7a [formerly b2374d5117aa7ccd71e56cdb53c9568bae54897d]]]
    Former-commit-id: feb007d8627f1e9405c5b78f856fb11da5dc0456 [formerly 66c19cd99bc2cdb709c44783be39834d11839635]
    Former-commit-id: c686eb54e3b71112661d66f75507c252db1169ae
    Former-commit-id: 934a33ff6df44a3cff5d7b34bf8377f3e7cda756

---
+++ app/assets/themes/lvg/javascripts/application.js
@@ -0,0 +1,22 @@
+/*
+*= require application
+*= require lvg/javascripts/considerit.theme
+*/
+
+$j('#intro .proposals.horizontal').infiniteCarousel({
+  speed: 1500,
+  vertical: false,
+  total_items: 8,
+  items_per_page: 8,
+  loading_from_ajax: false,
+  dim: 900
+});
+
+$j('.proposal_prompt .proposals.horizontal').infiniteCarousel({
+  speed: 1500,
+  vertical: false,
+  total_items: 8,
+  items_per_page: 8,
+  loading_from_ajax: false,
+  dim: 720
+});    

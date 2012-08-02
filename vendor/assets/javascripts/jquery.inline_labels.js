70678383241ee1a6235caac4ec592415ca8652d7
Travis Kriplean <travis@cs.washington.edu>
2012-08-01 22:20:19 -0700

    fixing hover problems for points in list; disabling hiding drop targets
    Former-commit-id: 6efffcd3e5519ccf99df4754a4fdf2a6bebca502 [formerly 5bdae0b42a1c47b9e7808e4caa72f8cb5e45c0c8] [formerly 08481f7707ca714b4eef0f931f55037786dbfb28 [formerly 08481f7707ca714b4eef0f931f55037786dbfb28 [formerly 2f439fd466358c2d24e5978619d85c228de5523b]]]
    Former-commit-id: 760b592f4f5c1e4cfc3f0b7b9bb14f25bce8f2fc [formerly cd595cc246a5163745a7679d7b58e3724b5551c2]
    Former-commit-id: 6a190579e510b0bcb0f69633512682e668a4b0c5
    Former-commit-id: 6a456b4d31834c87092cfb7e5fb84c3deedfb277

--- vendor/assets/javascripts/jquery.inline_labels.js
+++ vendor/assets/javascripts/jquery.inline_labels.js
@@ -49,6 +49,10 @@
           leftOffset = el[0].tagName == "TEXTAREA" ? 0 : 2,
           hidden_parent = el.is(":visible") ? null : el.parents(':hidden:last');
 
+      if ( hidden_parent ) {
+        console.log(hidden_parent.attr('class'))
+        console.log(el);
+      }
       if( el.data('inlined') ) return;
       el.data('inlined', true);
 
@@ -74,6 +78,7 @@
 
       if( hidden_parent ) {
         hidden_parent.hide();
+        hidden_parent.css('display', '');
       }
 
 

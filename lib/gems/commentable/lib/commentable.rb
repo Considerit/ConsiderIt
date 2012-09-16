7884160f71211a4f9d012f0db3a60dfd0a0be554
Travis Kriplean <travis@cs.washington.edu>
2012-09-15 20:30:20 -0700

    moved commentable and trackable functionality into separate gems
    Former-commit-id: 5aeb8e504f316b0534bdcfb28b4bcab2980008c0 [formerly 4b64ceb6a549386890d069afddca72f118a6d93f] [formerly 15c6c4ef199d3eaf8b7333f299a5422c91828999 [formerly 15c6c4ef199d3eaf8b7333f299a5422c91828999 [formerly 24c1573282d096098a6650b3454e4df28c672edf]]]
    Former-commit-id: 29e4586e1542e7e2a1355f4835340fd68ad7ea00 [formerly 5f121f601dc50071518382b62421c7081a9c28f1]
    Former-commit-id: c0e1e80893700c9e82a098b826797ae151fb6989
    Former-commit-id: d0dcfada91878f8ef7a66c9b7f6c9b45f101747c

---
+++ lib/gems/commentable/lib/commentable.rb
@@ -0,0 +1,31 @@
+#*********************************************
+# For the ConsiderIt project.
+# Copyright (C) 2010 - 2012 by Travis Kriplean.
+# Licensed under the AGPL for non-commercial use.
+# See https://github.com/tkriplean/ConsiderIt/ for details.
+#*********************************************
+
+module Commentable
+  class Engine < ::Rails::Engine #:nodoc:
+  end
+
+  class CommentableRailtie < ::Rails::Railtie
+    #initializer 'init' do |app|
+    #end
+  end
+
+  module Commentable
+    def is_commentable
+      has_many :comments, :as=>:commentable, :dependent=>:destroy
+      include InstanceMethods
+    end
+    module InstanceMethods
+      def commentable?
+        true
+      end
+    end
+  end
+
+end
+
+ActiveRecord::Base.extend Commentable::Commentable
---
+++ lib/gems/trackable/lib/trackable/version.rb
@@ -0,0 +1,3 @@
+module Trackable
+  VERSION = "0.0.1"
+end

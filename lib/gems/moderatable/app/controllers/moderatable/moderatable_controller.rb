a661f8ed984ae07b2dfd4ba64b3d3e0b1aeac6f9
Travis Kriplean <travis@cs.washington.edu>
2012-09-27 00:18:32 -0700

    various fixes for moderation; exception notifier
    Former-commit-id: f0392e0fb10a124934f0b0230d9eb97f82b65041 [formerly b6ad048f649356e0557e3348ca09e4276f418653] [formerly 465b43111be7f7cc8a0e69da7d669dde101438bb [formerly 465b43111be7f7cc8a0e69da7d669dde101438bb [formerly d0e6d5b8a54f49f211598336a466e7965d4ff380]]]
    Former-commit-id: 72b86df5adb2a88d8823abe46f664781a0364a55 [formerly 4d8455135cddea2e5b0a086be0877eee7f67e2f9]
    Former-commit-id: c8fdbb15885c70af04a4d6f097068278149454f2
    Former-commit-id: 9a221b276a7168aa825963810f9b146935e78fb7

--- lib/gems/moderatable/app/controllers/moderatable/moderatable_controller.rb
+++ lib/gems/moderatable/app/controllers/moderatable/moderatable_controller.rb
@@ -1,16 +1,40 @@
 class Moderatable::ModeratableController < ApplicationController
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, :notice => 'Please login first to access the moderation panel'
+    return
+  end
 
   # list all the objects to be moderated; allow seeing the existing moderations
   def index
+    
     authorize! :index, Moderatable::Moderation
+
     @classes_to_moderate = Moderatable::Moderation.classes_to_moderate
 
     @existing_moderations = {}
     @objs_to_moderate = {}
     @classes_to_moderate.each do |mc|
-      @objs_to_moderate[mc.name] = mc.moderatable_objects.call
+      
       @existing_moderations[mc.name] = {}
-      Moderatable::Moderation.where(:moderatable_type => mc.name).includes(:user).each do |mod|
+      if mc == Commentable::Comment
+        comments = []
+        mc.moderatable_objects.call.each do |comment|
+          if comment.commentable_type != 'Point' || comment.root_object.proposal.active 
+            comments.push(comment)
+          end
+        end
+        @objs_to_moderate[mc.name] = comments
+        objs = @objs_to_moderate[mc.name].map{|x| x.id}.compact
+        records = Moderatable::Moderation.where(:moderatable_type => mc.name)
+        if objs.length > 0
+          records = records.where("moderatable_id in (#{objs.join(',')})")
+        end
+        records = records.includes(:user)
+      else
+        @objs_to_moderate[mc.name] = mc.moderatable_objects.call
+        records = Moderatable::Moderation.where(:moderatable_type => mc.name).includes(:user)
+      end
+      records.each do |mod|
         @existing_moderations[mc.name][mod.moderatable_id] = mod unless @existing_moderations[mc.name].has_key?(mod.moderatable_id) && @existing_moderations[mc.name][mod.moderatable_id].user_id == current_user.id
       end
     end

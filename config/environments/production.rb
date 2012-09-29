6f6a3259d3dc37ed9d009a645dceeb30950458a9
Travis Kriplean <travis@cs.washington.edu>
2012-09-29 11:59:40 -0700

    adding new css/js to precompile list
    Former-commit-id: fc718b361c067d33cd3fc1bafed5b91a7278422c [formerly e1a5c6156fe237ff169b2d003c7ad2c59b59e70e] [formerly 987c43fc929ae83be81d44818b2cc17b7cf23461 [formerly 987c43fc929ae83be81d44818b2cc17b7cf23461 [formerly 9659dc39689736ad4cc020330b0a7202f1c0c506]]]
    Former-commit-id: b29bcf9d0dff811cdda754529f7ef93a61f8f896 [formerly 12f53f0c49c3daa67e5666b253dbc4e2db92d340]
    Former-commit-id: e833b60543855c91965d8a78cb451cd6f4e53161
    Former-commit-id: f30220b2845ec1f0dac8548062aabf42da533934

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ ConsiderIt::Application.configure do
   # config.assets.manifest = YOUR_PATH
    
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += ['ie.css', 'email.css', 'admin.css', 'active_admin.css']
+  config.assets.precompile += ['ie.css', 'email.css', 'admin.css', 'jquery.flot.css', 'jquery.flot.js', 'jquery.flot.selection.js']
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile"

6e938e46cd70540b566e2824e19cf8c9ed8d9327
Travis Kriplean <travis@cs.washington.edu>
2012-10-03 22:57:59 -0700

    updating lvg homepage & eliminating browser check that was triggering Avast trojan warnings
    Former-commit-id: dd0a391b8ad35eb3d2b8c50f366b9bd03f082b21 [formerly 6ce4945545f7572bfb002e0903b91dbac99fa965] [formerly 8a1a58949b95d61f0f5bf4b8e674d8f0b79ecc57 [formerly 8a1a58949b95d61f0f5bf4b8e674d8f0b79ecc57 [formerly 0944ab242714596761d371ab38d8bfcd3fa8b216]]]
    Former-commit-id: b074d37858c8e27349b7536019bcbd6ef03a2d44 [formerly 880074c16d56732f4a6cf02edd15b519fb3dfc36]
    Former-commit-id: 3281961e4e01e4d4a5ba1621686fae83574d656b
    Former-commit-id: 814e6052cbcb9cae7d9f69f5f7120e622fda39d6

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -9,7 +9,7 @@ if defined?(AssetSync)
     #config.fog_region = 'us-west-2'
     #
     # Don't delete files from the store
-    # config.existing_remote_files = "keep"
+    config.existing_remote_files = "ignore"
     #
     # Automatically replace files with their equivalent gzip compressed version
     config.gzip_compression = true

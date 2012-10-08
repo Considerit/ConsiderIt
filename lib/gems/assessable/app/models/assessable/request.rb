650de790ca896a6201d11e7f2fd3ac5aa300dfb9
Travis Kriplean <travis@cs.washington.edu>
2012-10-07 21:31:29 -0700

    the bulk of fact-checking framework
    Former-commit-id: 4ca27bc69c84e16b3a778e7bdf078d1dbb35bb81 [formerly 63d7d489a1400d876bfb94a3f0232abf21c86c0e] [formerly f473a6698799c0ab6e86eee4397e5bc267b15a54 [formerly f473a6698799c0ab6e86eee4397e5bc267b15a54 [formerly f2ef8e9081bb5b9095e62edbbcce9a05b778c509]]]
    Former-commit-id: 860a82726836ac85c279ccb4debaf7e33e99bf61 [formerly 319adb1d0cb5afcaf20503f24bd6c4056ee9f726]
    Former-commit-id: 808d0b8eea54c32b629066df1563e8ea996992ce
    Former-commit-id: f9e4c1cf01cfb0d61c19801501643760442b0684

--- lib/gems/assessable/app/models/assessable/request.rb
+++ lib/gems/assessable/app/models/assessable/request.rb
@@ -5,45 +5,24 @@
 # See https://github.com/tkriplean/ConsiderIt/ for details.
 #*********************************************
 
-class Assessable::Claim < ActiveRecord::Base
+class Assessable::Request < ActiveRecord::Base
 
+  belongs_to :assessable, :polymorphic=>true
+  belongs_to :user
   belongs_to :assessment, :class_name => 'Assessable::Assessment'
-  
+
   acts_as_tenant(:account)
 
   def self.build_from(obj, user_id, status)
     c = self.new
     c.assessable_id = obj.id 
     c.assessable_type = obj.class.name 
+    c.user_id = user_id
     c
   end
 
   def root_object
     assessable_type.constantize.find(assessable_id)
-  end
-
-  def self.format_verdict(verdict)
-    if verdict == 2
-      'Verified'
-    elsif verdict == 1
-      'Unverifiable'
-    elsif verdict == 0
-      'Questionable'
-    elsif verdict == -1
-      'No checkable claims'
-    else
-      '-'
-    end
-  end
-
-  def self.translate(verdict)
-    if verdict == 'verified'
-      2
-    elsif verdict == 'unverifiable'
-      1
-    else
-      0
-    end
   end
 
 end

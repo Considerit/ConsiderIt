ef3b631ddc114c7d32bd191c5955612e9cbf1f72
Travis Kriplean <travis@cs.washington.edu>
2012-10-09 12:05:41 -0700

    fixing flash messages

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -4,9 +4,8 @@ class Users::PasswordsController < Devise::PasswordsController
   def create
     user = User.find_by_email(params[:user][:email])
     UserMailer.reset_password_instructions(user, mail_options).deliver!
-    
+    set_flash_message(:notice, :send_instructions)
     respond_with({}, :location => root_path)
-    
   end
 
 end

ddb3669878550f43ede59996b03589e432223023
Travis Kriplean <travis@cs.washington.edu>
2012-10-10 12:09:00 -0700

    removing relation between domain and user; fixing a few misc problems

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -56,9 +56,7 @@ class UserMailer < ActionMailer::Base
     def generate_reset_password_token!(user)
       user.reset_password_token = User.reset_password_token
       user.reset_password_sent_at = Time.now.utc
-      pp user
-      pp user.save(:validate => false)
-      pp user
+      user.save
     end
 end
 

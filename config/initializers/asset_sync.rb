7424b3dbd7117af15a8721cddfb410506054deca
Travis Kriplean <travis@cs.washington.edu>
2012-10-23 03:00:03 -0700

    themes with asset digests
    Former-commit-id: b146277e496992b310a17edf7e3326a574f22a2e [formerly 724b63619803017c35bdd8867db413b0db5687a7] [formerly a3f366fe37909833008d696ed51e8652e806a419 [formerly a3f366fe37909833008d696ed51e8652e806a419 [formerly 1715efdbaedd39f75988040659aa25d5b5b34efc]]]
    Former-commit-id: 0d59227687adb927f69dff284db6c8c3d084ff6b [formerly 02d0fbcaf0e755bdee32f5ce0c8e8cec6bec8f7c]
    Former-commit-id: 174a8e2a7cc152a3565aa268a8b3ebb66488e4ef
    Former-commit-id: f3802c50100dc82b900059b4446cdb0ef87fd8bd

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -9,7 +9,7 @@ if defined?(AssetSync)
     #config.fog_region = 'us-west-2'
     #
     # Don't delete files from the store
-    config.existing_remote_files = "ignore"
+    config.existing_remote_files = "keep"
     #
     # Automatically replace files with their equivalent gzip compressed version
     config.gzip_compression = true

d1131d8be169b345664f78a55f40d6d4eb8e443e
Travis Kriplean <travis@cs.washington.edu>
2012-10-24 15:05:43 -0700

    fixes for chalkboardit theme
    Former-commit-id: 41a71e71407b6f5a8b87be6c81ef1dacb5384abe [formerly 2bd8741690c96955b44e17c73f6f51bf79f4ce96] [formerly 3d26ebfa46958b124759b1fbeecbe80343205924 [formerly 3d26ebfa46958b124759b1fbeecbe80343205924 [formerly fbe01c49eae11fa1e5aa3cb150206cafa0df1bf9]]]
    Former-commit-id: b1a203156f8a4e1757778daf3b4d767644cb9555 [formerly 9325ebc17c322ab051c7aa850423699d536f790a]
    Former-commit-id: 369a75b30417298544190cb2043684728bedd32e
    Former-commit-id: fe6ef5253f2453f96d8969040c3bd098178de47b

--- app/assets/themes/chalkboardit/javascripts/considerit.theme.js
+++ app/assets/themes/chalkboardit/javascripts/considerit.theme.js
@@ -1,10 +1,10 @@
 $j(document).ready(function(){
-  $j('.proposals').infiniteCarousel({
-    speed: 1500,
-    vertical: false,
-    total_items: 3,
-    items_per_page: 3,
-    loading_from_ajax: false,
-    dim: 700
-  });    
+  // $j('.proposals').infiniteCarousel({
+  //   speed: 1500,
+  //   vertical: false,
+  //   total_items: 3,
+  //   items_per_page: 3,
+  //   loading_from_ajax: false,
+  //   dim: 700
+  // });    
 });

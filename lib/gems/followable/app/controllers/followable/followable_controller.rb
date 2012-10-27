cc56a4e9b2e1f97a01e9efe96e969ca607264e09
Travis Kriplean <travis@cs.washington.edu>
2012-10-26 18:13:19 -0700

    ability to unsubscribe to all email notifications with one click
    Former-commit-id: 3e98cc046fe00e39b824349813444f21244dceed [formerly 74945367b12b9ab64f68fb6becba754c95cd0c15] [formerly e4794e5c313fa7edd3f8fe922b5a484c51ff135e [formerly e4794e5c313fa7edd3f8fe922b5a484c51ff135e [formerly 9d54123984eea149fe5f25f98143d13808d6623d]]]
    Former-commit-id: b24e1e36cba94db86712e1a98c04099114c43e49 [formerly 8f7ef09e1535e50cf8bb37a963c99509e5c167e2]
    Former-commit-id: 7848ac28c1746110c686d5774af861b3e28cb5c6
    Former-commit-id: 4d04c9b4757e107baa9db0d018478f9da8c2d05f

--- lib/gems/followable/app/controllers/followable/followable_controller.rb
+++ lib/gems/followable/app/controllers/followable/followable_controller.rb
@@ -8,26 +8,37 @@ class Followable::FollowableController < ApplicationController
   end
 
   def unfollow
+    # following an unsubscribe token link in an email
+    @followable_id = params[:i]
+    @followable_type = params[:m]
+    @user = User.find(params[:u])
+    @obj_to_follow = @followable_type.constantize.find(@followable_id)
+    if params[:t] == ApplicationController.token_for_action(params[:u], @obj_to_follow, 'unfollow')
+      render 'followable/unfollow'
+    else 
+      redirect_to root_path, :notice => "Cannot unsubscribe because that email link is expired."
+    end
+    
+  end 
 
-    if params.has_key? :t
-      # following an unsubscribe token link
-      followable_id = params[:i]
-      followable_type = params[:m]
-      user = User.find(params[:u])
-      obj_to_follow = followable_type.constantize.find(followable_id)
-      if params[:t] == ApplicationController.token_for_action(params[:u], obj_to_follow, 'unfollow')
-        obj_to_follow.follow!(user, :follow => false, :explicit => true)
-      end
-      #TODO: get the model's path to redirect to
-      redirect_to root_path, :notice => "You have unsubscribed to the #{followable_type.downcase}"
+  def unfollow_create
+    user = User.find(params[:follows][:user_id])
+
+    if params[:follows].has_key?(:unsubscribe_all) && params[:follows][:unsubscribe_all] == 'true'
+      user.unsubscribe!
+      redirect_to root_path, :notice => "You have unsubscribed to all notifications."
     else
       followable_type = params[:follows][:followable_type]
       followable_id = params[:follows][:followable_id]
       obj_to_follow = followable_type.constantize.find(followable_id)
-      obj_to_follow.follow!(current_user, :follow => false, :explicit => true)
-      render :json => {:success => true}.to_json
+      obj_to_follow.follow!(user, :follow => false, :explicit => true)
+      if request.xhr?
+        render :json => {:success => true}.to_json
+      else
+        redirect_to root_path, :notice => "You have unsubscribed to the #{followable_type.downcase}."
+      end
     end
-  end 
 
+  end
 
 end

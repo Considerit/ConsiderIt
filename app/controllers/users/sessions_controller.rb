96bff7bf02d662a7e170603667dbb051a343434a
Travis Kriplean <travis@cs.washington.edu>
2012-11-20 21:16:34 -0800

    removing what I believe is unnecessary store location
    Former-commit-id: 6a723374dbb313146a0299909e9ef7fe2c651054 [formerly b38e6f86ca43b7c7d0aa1d3709c5ffc25ccb175a] [formerly d188d0cf9844fc30b26f129118346cbbe2d62927 [formerly d188d0cf9844fc30b26f129118346cbbe2d62927 [formerly 722f80316159ad32ad771b51ca81d996982b85b4]]]
    Former-commit-id: fb271c65229ed6b0f55241fe1bdedefb8839daf5 [formerly f236df8a62064074c82b1049fd8c6af7d08e5b80]
    Former-commit-id: 84ea7d03dbaf620866d7a9a114ff51f3122cb8f3
    Former-commit-id: 7fa4f2dcaa0b2b3d21404bdf7af1013b689802fa

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -10,7 +10,7 @@ class Users::SessionsController < Devise::SessionsController
 	protect_from_forgery :except => :create
 
   def new     
-    store_location request.referer
+    #store_location request.referer unless params[:redirect_already_set] == 'true'   
     if ( params[:third_party] )
     	case params[:provider]
 	    	when 'twitter'

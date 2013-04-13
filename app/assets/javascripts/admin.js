d3cd1461bc5100b72f7bcda9ad718f16b4ea7f95
Travis Kriplean <tkriplean@gmail.com>
2013-04-13 00:10:05 -0700

    progress on user, admin dashboard; most of moderation finished
    Former-commit-id: a02d56907f985f95e4bdaeefb67d18abfd69815e [formerly d0131b8fe2874262be6b6a1d48f0ecf911259ad0] [formerly da0b345053e54f37106abd5add78783fd9b5564c [formerly da0b345053e54f37106abd5add78783fd9b5564c [formerly 01eddcc62da0a74c468d78d5c8321a0a9027042f]]]
    Former-commit-id: 21e2b43a04f3197b3ece541c4157a4bbf7ca8035 [formerly 6f43d88a734c67dea793ad72272b3a194e5bb051]
    Former-commit-id: c9616b01ff3b19388d7f7effeddff6e769062e23
    Former-commit-id: fe74e0ca34829a0ee45357530fdce5422f9cd01b

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,20 +1,7 @@
 // ...
-//= require jquery
-//= require jquery_ujs
-//= require underscore
-
-//= require jquery.remotipart
-//= require jquery.autoresize
-//= require jquery.h5validate
-//= require jquery.touch-punch
-//= require jquery.inline_labels
 //= require jquery.fixedheadertable.min
-//= require jquery.fileinput
-//= require sevenup.min
-//= require sevenup_black.min
 //= require jquery.flot
 //= require jquery.flot.selection
-
 //= require javascripts/moderatable
 
 $(document)
--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,7 +1,18 @@
-class AccountsController < ApplicationController
+class AccountsController < Dashboard::DashboardController
   #include ActsAsFollowable::ControllerMethods
+  respond_to :json
+
+  def show
+    authorize! :show, Account
+
+    rendered_admin_template = params["admin_template_needed"] == 'true' ? self.admin_template() : nil
+
+    render :json => {:account => current_tenant, :admin_template => rendered_admin_template}
+  end
 
   def update
+    authorize! :update, Account
+
     if !current_user.is_admin?
       redirect_to root_path, :notice => 'Insufficient permissions.'
       return
@@ -9,7 +20,7 @@ class AccountsController < ApplicationController
 
     # TODO: explicitly grab params
     current_tenant.update_attributes(params[:account])
-    redirect_to request.referrer
+    render :json => current_tenant
 
   end
 

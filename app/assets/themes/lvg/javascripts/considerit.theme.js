6f52e29ad0fe4db04f4303ac634eb71832e55b4f
Travis Kriplean <tkriplean@gmail.com>
2013-07-07 14:00:25 -0700

    moving toward more standard method of theming background; more work on LVG theme
    Former-commit-id: 15eab81c48aade43fd6775ddc7bfae270f4685c5 [formerly a492641f1bf6c7b3b2e05f9e970769789c920d1b] [formerly edfc55995e169dd63d80e506544bf19e0b4dc4c7 [formerly edfc55995e169dd63d80e506544bf19e0b4dc4c7 [formerly 63a25320f4c24523eb7ab041ca974528006cb15c]]]
    Former-commit-id: 78aa7104a6fc7197d3469acfd0691150306fdf4c [formerly 5b1858dc97a1871efe01475e25462cc0e8d6cef0]
    Former-commit-id: be344aab8172c5b7d249391503b44a664eb1e4e7
    Former-commit-id: c714d9481ca82e33c368768d46a8e644619fdae4

--- app/assets/themes/lvg/javascripts/considerit.theme.js
+++ app/assets/themes/lvg/javascripts/considerit.theme.js
@@ -1,13 +1,30 @@
-// $(document).ready(function(){
-  
-//   per_request();
-//   $(document).ajaxComplete(function(e, xhr, settings) {
-//     per_request();
-//   });
+$(document).ready(function(){
 
-//   function per_request(){
-//     $('#points_on_board_pro .pro_header img').attr('src', '/assets/policyninja/images/pro_header.png');
-//     $('#points_on_board_con .con_header img').attr('src', '/assets/policyninja/images/con_header.png');
+  $(document).on('mouseenter mouseleave', '#t-intro-people .avatar', function(e){
+    if(e.type == 'mouseenter') {
+      var to_hover = [];
+      var idx = $(this).index();
+      var area = 3;
 
-//   }
-// });
+      var row = parseInt($(this).parent().attr('row'));
+
+      var first = Math.max(row-area,0);
+      var rows = $('#t-intro-people').children();//.slice(first, row + area + 1);
+
+      for (var i = 0; i < rows.length; i+=1) {
+        first = Math.max(idx-area,0);
+        var children = $(rows[i]).children().slice(first, idx+area+1);
+        for ( var j = 0; j < children.length; j+=1) {
+          to_hover.push( children[j] );
+        }
+      }
+
+      $(to_hover).addClass('hovered');
+
+    } else {
+      $(this).parent().parent().find('.avatar.hovered').removeClass('hovered');
+    }
+
+  });
+
+});

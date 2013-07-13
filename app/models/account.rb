23a865cd1cd72eeb929f4d726b797c0193ed6084
Travis Kriplean <tkriplean@gmail.com>
2013-07-13 11:45:47 -0700

    setting default value for header and details text on home header
    Former-commit-id: 65ee2c5a6b77f1a83368fb9aa749a2520378001d [formerly fb6dcb95a9976d750d14352c5bb5eef7261eef8e] [formerly 94650102a1117f24a2f7d31f995fad82c74b00a3 [formerly 94650102a1117f24a2f7d31f995fad82c74b00a3 [formerly c98f0c253677a5baaeed194b95b6feb0edd07411]]]
    Former-commit-id: df83a49da0f49ab82d0ae4ca831119fa3c0506ac [formerly dd958cc84000d22a1196b31ab1f0f07da35a3061]
    Former-commit-id: 4a6d782e6ba77113224962e40039bcb25e4c6980
    Former-commit-id: 2a8b3ada85623cad6ebcbcff4f905d124d10d3af

--- app/models/account.rb
+++ app/models/account.rb
@@ -17,6 +17,8 @@ class Account < ActiveRecord::Base
 
   is_followable
 
+  before_create :set_default
+
   def host_without_subdomain
     host_with_port.split('.')[-2, 2].join('.')
   end
@@ -24,5 +26,10 @@ class Account < ActiveRecord::Base
 
   def self.all_themes
     Dir['app/assets/themes/*/'].map { |a| File.basename(a) }
+  end
+
+  def set_default
+    header_text ||= 'The main callout to participants'
+    header_details_text ||= 'This is where you\'ll add more details about why this forum exists, and whom you want to participate.'
   end
 end

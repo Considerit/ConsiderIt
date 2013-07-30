672b193d72376be84bc705e17b02f59e85f73674
Travis Kriplean <tkriplean@gmail.com>
2013-07-29 19:24:06 -0700

    removing Reflect
    Former-commit-id: 652f32dbcc29ae36b18a5f427fb6b61c85d523c9 [formerly c383d64811294a7f05c83eb109720a179aa80cde] [formerly 62bdde1c8dac1cf6342f54c21a38ed53757dd224 [formerly 62bdde1c8dac1cf6342f54c21a38ed53757dd224 [formerly 4992614cf819af0dd7d9b4b7a79fe3b33544bc05]]]
    Former-commit-id: fe4d855dd5fc6e7045f82691447e0f646fd1e59d [formerly 36fb76a8e0e49467769ae02ef276f32d3f8af602]
    Former-commit-id: aa46f3c56a8ce87cb2d9f9c5ce27fe10fd93c24e
    Former-commit-id: 3a0b11d979211124bdf99fe977f8c25fc39bb4db

--- lib/gems/reflect/app/models/reflect/reflect_response.rb
+++
@@ -1,5 +0,0 @@
-class Reflect::ReflectResponse < ActiveRecord::Base
-  belongs_to :bullet, :class_name => 'Reflect::ReflectBullet', :foreign_key => 'bullet_id'
-  has_many :revisions, :class_name => 'Reflect::ReflectResponseRevision', :foreign_key => 'response_id', :dependent => :destroy
-  acts_as_tenant(:account)
-end

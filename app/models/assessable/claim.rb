5a98b8d5d5143a29fe229f7fa17eb5a3973e274f
Travis Kriplean <tkriplean@gmail.com>
2013-09-11 11:02:14 -0700

    further implementation of new assessment backend interface
    Former-commit-id: 66068df73c6a6c44a0fa48af9b725ee790ee39a7 [formerly 5023a026808b06cc0cd80dab45f4a60a2f3a2cd8] [formerly def8c70a09b0c30ff67aeb67d41b231fa592ecb8 [formerly def8c70a09b0c30ff67aeb67d41b231fa592ecb8 [formerly 20547235a7861d937ffdfb86297b4e261f2264aa]]]
    Former-commit-id: aa4c6e5950ba1ba76ace974205ac1abc3f518ccb [formerly 9bc2e734e9e61d6eb068a1cc6d1d7ce842fd1039]
    Former-commit-id: 60cc9768ac89d424061caabbc898c5a3deb066b9
    Former-commit-id: 818d518f416d59ff0ea8afeb6aa853ca70b1ee2c

--- app/models/assessable/claim.rb
+++ app/models/assessable/claim.rb
@@ -3,7 +3,10 @@ class Assessable::Claim < ActiveRecord::Base
   belongs_to :assessment, :class_name => 'Assessable::Assessment'
   acts_as_tenant :account
 
-  scope :public_fields, select( [:id, :verdict, :claim_restatement, :result, :assessment_id])
+  belongs_to :creator_user, :foreign_key => 'creator', :class_name => 'User'
+  belongs_to :approver_user, :foreign_key => 'approver', :class_name => 'User'
+
+  scope :public_fields, select( [:id, :verdict, :claim_restatement, :result, :assessment_id, :approver, :creator])
 
   #TODO: sanitize before_validation
   #self.text = Sanitize.clean(self.text, Sanitize::Config::RELAXED)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130819215938) do
+ActiveRecord::Schema.define(:version => 20130910233120) do
 
   create_table "accounts", :force => true do |t|
     t.string   "identifier"
@@ -110,6 +110,8 @@ ActiveRecord::Schema.define(:version => 20130819215938) do
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
     t.text     "notes"
+    t.integer  "creator"
+    t.integer  "approver"
   end
 
   create_table "comments", :force => true do |t|

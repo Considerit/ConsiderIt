8c07d45a51f93ba8c38afa031894c02c024d6d28
Travis Kriplean <tkriplean@gmail.com>
2013-09-16 08:16:24 -0700

    refactoring
    Former-commit-id: c288544f736fe5b4bf31c902f2bb8e5180fdc3f2 [formerly 14db6e3f5aa7a2dce5d6644b1da4e5c4c95cbce4] [formerly 437c11c3d09bcc5be88a0952f90218e9f0bb8922 [formerly 437c11c3d09bcc5be88a0952f90218e9f0bb8922 [formerly 0b59f2cf18429485466c49ff214b2f7c83212b6d]]]
    Former-commit-id: 75637a40071bd9e659425d121cff5a14cd7b4bf9 [formerly 1a922aeb610400cd89a3c41d8006635eb389eea3]
    Former-commit-id: 02098e2daa8ef72d2fb4f485a687bf6cc2ff5ae2
    Former-commit-id: 584b2660d8d4e8376051aa708ca89f348907c244

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,15 +15,12 @@
 //= require_tree ./backbone/config
 //= require backbone/app
 
+//= require_tree ./backbone/entities
 //= require_tree ./backbone/controllers
 //= require_tree ./backbone/views
-//= require_tree ./backbone/entities
 //= require_tree ./backbone/components
 //= require_tree ./backbone/apps
 
-
 // TO BE REFACTORED: 
-
-//= require main
 
 //= require javascripts/acts_as_followable

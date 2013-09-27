3b6f625ec0aafe406fcca2e45727d7ee96aa1910
Travis Kriplean <tkriplean@gmail.com>
2013-09-26 23:27:15 -0700

    make sure to send position.published from the server
    Former-commit-id: a6220924ee9069e00021655cb4a851a101c19e90 [formerly a34bc2896b9ff91b32bbaf91d6cd983227928a13] [formerly 893a13e3c56986fcd61a5f9f3b9987393ba6a926 [formerly 893a13e3c56986fcd61a5f9f3b9987393ba6a926 [formerly e4225034da1a48add78d9173b1ce0da38eabea05]]]
    Former-commit-id: 9b680c1d59f3391f788a4d5d57fee7e7418e83ac [formerly 56fd93987bf735cc06835614aae82bf73a9de82c]
    Former-commit-id: 14ad85ecefbe36b9376beee3eda587bdf10a1d5a
    Former-commit-id: e30696a43ed390ae76ff71bdd9ca83654cfcd216

--- app/models/position.rb
+++ app/models/position.rb
@@ -14,7 +14,7 @@ class Position < ActiveRecord::Base
   acts_as_tenant(:account)
 
   scope :published, where( :published => true )
-  scope :public_fields, select( [:long_id, :created_at, :updated_at, :id, :point_inclusions, :proposal_id, :stance, :stance_bucket, :user_id, :explanation])
+  scope :public_fields, select( [:long_id, :created_at, :updated_at, :id, :point_inclusions, :proposal_id, :stance, :stance_bucket, :user_id, :explanation, :published])
 
   before_save do 
     self.explanation = Sanitize.clean(self.explanation, Sanitize::Config::RELAXED)

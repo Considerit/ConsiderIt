f6f9533449f2dd2a38445c439c30ff87eb1c55fc
Travis Kriplean <tkriplean@gmail.com>
2013-10-15 14:26:58 -0700

    updating sitemap generator
    Former-commit-id: 1155719eb5d4c8ee643ec734ef8b80cf754ea7c4 [formerly 6108afae3532388eaa944c870faa07ecff6d619d] [formerly 588d25da5199958ba0c5f94a787db6aef2ee78b9 [formerly 588d25da5199958ba0c5f94a787db6aef2ee78b9 [formerly 0ee9ba01785ef8b10579eea0d67bcd9c03fb0d68]]]
    Former-commit-id: 17f23ce59c09de4ecc8e89acdbc9e4e4f1661d44 [formerly 4e5a854495a5fd2d421c08c32ed5c63822d67b84]
    Former-commit-id: b99cf0c8e3979c113de8defba1e53349c5bc3de2
    Former-commit-id: 805bff7f512a3c7cdf0121c68787bc32fb1e0044

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -26,15 +26,21 @@ Account.all.each do |accnt|
     #
     accnt.proposals.active.open_to_public.each do |prop|
       begin
-        add new_position_proposal_path(prop.long_id), {:priority => 0.7, :changefreq => 'weekly'}
+        add new_position_proposal_path(prop.long_id), {:priority => 1.0, :changefreq => 'daily'}
         #add proposal_path(prop.long_id), {:priority => 0.7, :changefreq => 'weekly'}
       rescue
         pp "#{prop.long_id} failed"
       end
     end
 
-    # add '/home/media', {:priority => 0.2, :changefreq => 'daily'}
-    # add '/home/copromoters', {:priority => 0.2, :changefreq => 'daily'}
+    accnt.proposals.inactive.open_to_public.each do |prop|
+      begin
+        add new_position_proposal_path(prop.long_id), {:priority => 0.2, :changefreq => 'monthly'}
+        #add proposal_path(prop.long_id), {:priority => 0.7, :changefreq => 'weekly'}
+      rescue
+        pp "#{prop.long_id} failed"
+      end
+    end
 
   end
   #SitemapGenerator::Sitemap.ping_search_engines

4023038cdc2d1ccb9b3d486de8ce76e7d97c30fc
Travis Kriplean <tkriplean@gmail.com>
2013-12-16 13:14:29 -0800

    move to memcache session store
    Former-commit-id: f173f85d69a894d2991a28cbb7cc0dca7731f734 [formerly a49609496eed7d6e88b0aa3e421d1badb5aa272b] [formerly 9a5f3436d7768044c350b70fe1e0a772a8fc5f17 [formerly 9a5f3436d7768044c350b70fe1e0a772a8fc5f17 [formerly 87d8f3bdbd962e28f1aa7d3d0d5aa536b4db5008]]]
    Former-commit-id: ab0cbbb24b6fcf91526bfae5ca6e5813774f1428 [formerly 93e2f5fecd589b58765345d16ffe03c1eed76c82]
    Former-commit-id: 969b15884a5d16069926bf5511b8313005f878d5
    Former-commit-id: c274e7f35f4f988ddc406c0ed2b89b42e9bead1d

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-ConsiderIt::Application.config.session_store :cookie_store, :key => '_ConsiderIt_session'
+#ConsiderIt::Application.config.session_store :cookie_store, :key => '_ConsiderIt_session'
+#ConsiderIt::Application.config.session_store :mem_cache_store, 'localhost', '127.0.0.1:11211', {:namespace => 'considerit'}
+
+ConsiderIt::Application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 1.day
+
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information

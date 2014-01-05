c34a58bb649004c0ffee23295d18acf9a4a0eaec
Travis Kriplean <tkriplean@gmail.com>
2014-01-04 16:08:11 -0800

    fixing security issue with secret_token
    Former-commit-id: 16300c674062d6b6e0057bfb259cb48173959d9c [formerly 7032ae05cf5622cec61d328a1185a26900f42ccb] [formerly 527209b296ccec41aa7e00b9d117eb7a6b754fef [formerly 71ec84daf56362f869671d1b79cca72cbfff3b0b]] [formerly 4cb846ae78fb474c8839ddf1dc1c0d724ccaa181 [formerly 15d5522df6afb856645faf71965449715366ab10] [formerly 15ab17f21669b2d997fedead5ed69549cce7571b [formerly 15ab17f21669b2d997fedead5ed69549cce7571b [formerly ee43c2b734b24a7d38821dbba621d3d0416f7638]]]]
    Former-commit-id: fa3666d16921c519cadb9adc6b8c293312fffc7b [formerly cc08319d2f8f0522a6120a8a039b7569824f03b4] [formerly 4a2705c4374bbece80dd9ec2ed1f32569e70507c [formerly c83dad1d0f7bbad62cb39c3b2275c95d80236f55]]
    Former-commit-id: 4306b677c5ece57a4733529db84e1ac9bb78e768 [formerly d5d393311c12b708d3b84bcebd450271ee5d2fd5]
    Former-commit-id: 48abc5ad674f02355d567b69472a090fe813ffec
    Former-commit-id: 7e46e359a85d12e6932561949d5cee515b72f52e

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ConsiderIt::Application.config.secret_token = '67c7d18d8977b7f6a06fa73b1f89976e2f32adca50283ec15a5acc9af2def68bd2b8912769309fba9dcbed88ba6f10ade32d2ee6dcee01842064f5d4c0d98c4b'
+
+ConsiderIt::Application.config.secret_token = APP_CONFIG[:secret_token]

c9875a1cd1c44b209102524a390cad9eeb0da0f7
Travis Kriplean <tkriplean@gmail.com>
2014-01-09 22:05:08 -0800

    local testing pipeline up and working; loading data and running tests
    Former-commit-id: 3717e1032b4b647ccfc22c2ef582457239506196 [formerly ff0d0019216d8b1a89d28f414d79f4c75665edb5] [formerly e2378247cc79ccc1c63eaae5514061d614e3b013 [formerly 6b068fb8d49c4ea0b5027e9f3b04bbc553cac5d7]] [formerly 7c900ed49b4258779b06ea9f428721c803bb6d99 [formerly 01e422cfbec1b54ac47fc05143f722c2e5cfb65b] [formerly ce0e0676d5f00e3f67ac81e547dae34e1f800b9d [formerly ce0e0676d5f00e3f67ac81e547dae34e1f800b9d [formerly 6d655ba464fdfbeee0fdfcc1bc32acbf207f4aea]]]]
    Former-commit-id: 426242abdc07384953c99ca41c648263aae81123 [formerly cdc9efbc63c515ac753a9f491ce528f9523d75e3] [formerly 2bb3adbec5992fd90d40b3f462223135aec2def3 [formerly df2515290e9b58780f38faceb24a3e1a43cbad3e]]
    Former-commit-id: d5597102c8b7656dd842e4f90f990e509d8f2007 [formerly 2efaf2244c7e6b8bee9896b55909463c4e829c2b]
    Former-commit-id: 05e7a22bafeacdb074ea64d1eb2298c08d239fd3
    Former-commit-id: b9c5a1ca4f52580d4f54d949387647e28ccc88ab

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -6,3 +6,4 @@
 # no regular words or you'll be exposed to dictionary attacks.
 
 ConsiderIt::Application.config.secret_token = APP_CONFIG[:secret_token]
+ConsiderIt::Application.config.secret_key_base = APP_CONFIG[:secret_key_base]

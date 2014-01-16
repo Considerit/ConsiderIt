aa116ae58323635626ce427c695feac5b8bcba58
Travis Kriplean <tkriplean@gmail.com>
2014-01-16 12:25:43 -0800

    removing old schema migrations
    Former-commit-id: cb1be2a8b0af3963491e89e6edd6230873fb768f [formerly de6d58f793a8ac497905c8f1de19c8dab29ebd2a] [formerly 21259055debaa13be63beeb930e545e32686de73 [formerly c0f69afa8ed86a0bd9964c17eba0ff658de00762]] [formerly 8e54ec5d2b61f23298f5a036ad963a7565c5cffd [formerly 297da8b36f47b3a6b4d8dec82a0a30f05ee95475] [formerly 4eb1568578be5bb378e6c521c5a0be3027fed581 [formerly 4eb1568578be5bb378e6c521c5a0be3027fed581 [formerly 5061c797f285a42f42a7825e51a4b5073abbc028]]]]
    Former-commit-id: 6d9d7879a76bda312f6ca00a84eaf624f2b552b9 [formerly fedd46b6ac8fe0af369e424fb231d20593390ba0] [formerly dc6e0c08a0c9e8b312e98a45dda6a2a7e37920a2 [formerly 6ca5c72ca9ac893424feca3305dc083fae3f35f8]]
    Former-commit-id: 8104c468b17ed1f5def95a811008b799f24289a8 [formerly 2733fe65d1383e081885d20fb2de10b7b3a19878]
    Former-commit-id: a4479744ef7d1e4201a5995a48dd4219a7934ab1
    Former-commit-id: 9497ce1553c6f7e3fc10ef0b1eeb381dd095380b

--- db/migrate/20130702063847_add_num_views_to_proposal.rb
+++
@@ -1,8 +0,0 @@
-class AddNumViewsToProposal < ActiveRecord::Migration
-  def change
-    begin
-      add_column :proposals, :num_views, :integer, :default => 0
-    rescue
-    end
-  end
-end
--- db/migrate/20130819215938_add_targettable_to_proposal.rb
+++
@@ -1,5 +0,0 @@
-class AddTargettableToProposal < ActiveRecord::Migration
-  def change
-    add_column :proposals, :targettable, :boolean, :default => false
-  end
-end

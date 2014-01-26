b2b3692d815216353343b92c2469e2bd1f0f4df6
Travis Kriplean <tkriplean@gmail.com>
2014-01-25 21:41:18 -1000

    major directory restructuring, creating @server, @client and assets in the top level
    Former-commit-id: 7bc281163a4929fe420423792a019e79c9c6a39f [formerly 5c654da6e574aa53f5f56b99e0840e651b88b04e] [formerly 01e9bae243864d0e3b193c9d6741789b1133c211 [formerly bbaf16abb1db75b756fcf35194d1723e28057c91]] [formerly 1be2f5cd1eda565cf8f0b3949113cf08a2852c3e [formerly 034098fd9970f6a7733ef19e3c3a4791e80863b7] [formerly 0550764e41100509f0c594832d5823da23fdb60f [formerly 0550764e41100509f0c594832d5823da23fdb60f [formerly cf7d8be357c10b94e4d92ddbf6ae804fdd07bb99]]]]
    Former-commit-id: 5a696015b72c83a12a6ec8ac019277002fdfc1d4 [formerly 01688316248f01f3196ca5f7c790388b578ebadd] [formerly e6d8b5adde21d2ef047b9e3e8a67b8266c97c106 [formerly 17c45ebee54d282fdbb7c2e69f97e1f06b18d242]]
    Former-commit-id: de58bd20fc6329ac62b6c8e9dd2f0a19185754ae [formerly d5a520672797546ff45e8ed0064be790ce3dbb70]
    Former-commit-id: f114617459e478bacd65c8f94c32e047e2076a15
    Former-commit-id: e1a002ff1c1e35db5dbcb95046814c09e5e851a3

---
+++ assets/javascripts/application.js
@@ -0,0 +1,22 @@
+// ...
+//= require jquery
+//= require jquery_ujs
+//= require jquery.remotipart
+
+//= require lib/underscore
+//= require lib/backbone
+//= require lib/marionette
+
+//= require js-routes
+
+//= require_tree ./vendor
+
+
+
+//= require_tree ../../@client/config
+//= require app_entry
+//= require_tree ../../@client/entities
+//= require_tree ../../@client/controllers
+//= require_tree ../../@client/views
+//= require_tree ../../@client/components
+//= require_tree ../../@client/apps

d64a5f33151486644bf17014087606f2349fd5f1
Travis Kriplean <tkriplean@gmail.com>
2014-01-25 22:12:46 -1000

    underscore templates now under @client/templates
    Former-commit-id: c0b07407c1d7d9b2f7b7258be87b02ada8a132b3 [formerly 73bdbf9a231a4ef9cdb61c3c396459b642649d1c] [formerly 80b5ecaac76b88596ef9f39256f5b1b3ee4ed418 [formerly 16d8d15616f40a9543057ce61064ec218311e571]] [formerly ebff918dec4c43cdd0f8dfddd0c8a358c78dee78 [formerly 2ac99eab059af22cb13b476072b68c7bdf674ad4] [formerly 426962eb5aceeecf74f87d1f774600becb303292 [formerly 426962eb5aceeecf74f87d1f774600becb303292 [formerly 703edc22d54e36450b40f7c81fea564ef964290f]]]]
    Former-commit-id: 13ca58bd3a1fcc490469ceafa433e6191dc2fe47 [formerly 9fd80d6507b183eb3cc0d678a9c25eb54c4beee7] [formerly f3d9c6d8df3143806699847c2ee80e570ec91d54 [formerly 17bc0f69415f931ef394c4c32085937474172cb3]]
    Former-commit-id: facfe1ffebfda0d57113fba36515ee2068cc39dd [formerly 10389454002e1142fc602ed322309bd0854a60e1]
    Former-commit-id: 72af669c1315f9dc5a7ed6e450b089e9fb2eafb8
    Former-commit-id: 0deefab691425e6df24f202578e88610b1704601

--- config/initializers/_1_themes_for_rails.rb
+++ config/initializers/_1_themes_for_rails.rb
@@ -1,12 +1,12 @@
 ThemesForRails.config do |config|
   # themes_dir is used to allow ThemesForRails to list available themes. It is not used to resolve any paths or routes.
-  config.themes_dir = ":root/app/assets/themes"
+  config.themes_dir = ":root/assets/themes"
 
   # assets_dir is the path to your theme assets.
-  config.assets_dir = ":root/app/assets/themes/:name"
+  config.assets_dir = ":root/assets/themes/:name"
 
   # views_dir is the path to your theme views
-  config.views_dir =  ":root/app/views/themes/:name"
+  config.views_dir =  ":root/@client/templates/themes/:name"
 
   # themes_routes_dir is the asset pipeline route base. 
   # Because of the way the asset pipeline resolves paths, you do

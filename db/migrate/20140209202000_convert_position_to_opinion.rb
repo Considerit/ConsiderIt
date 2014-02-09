a3d41a9865392ce68afc3f30b3422280f814e94a
Travis Kriplean <tkriplean@gmail.com>
2014-02-09 12:00:31 -1000

    fixing migration
    Former-commit-id: 63a32af542c4936d089b363d99d7cbc6967b4d9f [formerly f219087ff9df73ca757507c490118b290bcafeae] [formerly f04e5a2924080ddca5e5dac3ef5570d381264754 [formerly 0e1d292483930b50d6adf1610d0918912c35ce5c]] [formerly 948250ca643f99f3a12c6feec9bbd3977e725ab7 [formerly 9c73f505bf564e21109c55aae985af0b6b381913] [formerly 6cb90e1149c38e6a2ce60de72ee59fd229c71e56 [formerly 6cb90e1149c38e6a2ce60de72ee59fd229c71e56 [formerly ab9c4cb9e3905ac7ab089a8da34ec27c218826d0]]]]
    Former-commit-id: ee86348e4eb2f6244baaadac1369d7e2d156246e [formerly b7082406b80a66ba4b65d9bf3ea7cb0d5367b62e] [formerly 2cfdd05c7e93a921077348d66a6c1774867039a6 [formerly 0f5e6331b8b6577d7f8385b06265fad54e9511b8]]
    Former-commit-id: f897b52fea19559ff3a9af8666c9a5ccdfdc00a9 [formerly 79bce9d9542f3f987e76303a90802be6900ad8f3]
    Former-commit-id: 4260590510f69db98960608e725e2a3aa5a59b3e
    Former-commit-id: f50af744b4a4c01f3e891b913b14de25ace8dd9c

--- db/migrate/20140209202000_convert_position_to_opinion.rb
+++ db/migrate/20140209202000_convert_position_to_opinion.rb
@@ -4,7 +4,6 @@ class ConvertPositionToOpinion < ActiveRecord::Migration
     rename_column :inclusions, :position_id, :opinion_id
     rename_column :point_listings, :position_id, :opinion_id
     rename_column :points, :position_id, :opinion_id
-    rename_column :inclusions, :position_id, :opinion_id
     rename_column :users, :metric_positions, :metric_opinions
   end
 

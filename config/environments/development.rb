90fa0ec180cf1359f50ef2782d1f15d300a0828a
Travis Kriplean <tkriplean@gmail.com>
2014-02-16 09:23:05 -1000

    better tracking of counts of remaining moderation items

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,8 +37,8 @@ ConsiderIt::Application.configure do
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => "gmail.com",
-    :user_name            => "#{ENV['UNAMER']}@gmail.com",
-    :password             => "#{ENV['YASSER']}",
+    :user_name            => "",
+    :password             => "",
     :authentication       => "plain",
     :enable_starttls_auto => true
   }

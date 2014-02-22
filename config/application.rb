47d4bfacfbfc64177c5d89ed59a66250953071da
Travis Kriplean <tkriplean@gmail.com>
2014-02-21 17:50:35 -1000

    refactoring test framework files, adding a library for common casper tasks
    Former-commit-id: d66eda4a11d67c477f3ef3ccaa25e78c04f438d0 [formerly 146318ef78033a3756dbdea64792ee552b982971] [formerly 120d4fc60b234092ecbbba118ba66d3d768e8ab2 [formerly 18260cf03dca6537e6798a2812cbe3ae019370c3]] [formerly ca9c82969c3fec9c7b88db1b8f5014104700623e [formerly 12f9d5e0063f184f9e15ebc372c0e6b0a915dc1d] [formerly 71a87b4faa92db17039d52ec5ac3833b928ebb70 [formerly 71a87b4faa92db17039d52ec5ac3833b928ebb70 [formerly cdce838ab43532791204aa91842173163a616989]]]]
    Former-commit-id: a66b46eb96b6f5727ec815e7af5d7ea67d9bcd73 [formerly adeb1c0eecf2295a84ff7a4ce8a3d7662ae1767f] [formerly dcdf96127c47cebd25673b4062e2b88b928cdde7 [formerly 9d2d506d9350b92194728fcbbdfe74a5176f6cb2]]
    Former-commit-id: 4c2c6e23357091584ffe928ab8b89398e2df4064 [formerly 816146c43d3393a7e94eb0c73c7f214c2cb0c84c]
    Former-commit-id: e2d1b344a9ff4a3d1fa4a2f9a29fd16563ed8621
    Former-commit-id: 3ac07468ec252664f10ce4b244cd924c5b0f63f2

--- config/application.rb
+++ config/application.rb
@@ -71,6 +71,8 @@ module ConsiderIt
     config.paths["app/controllers/concerns"] << "@server/controllers/concerns"
     config.paths["app/models/concerns"] << "@server/models/concerns"
 
+    config.paths["lib/tasks"] << "test"
+
     config.paths["config/initializers"] << "@server/notifications"
     asset_paths = ["@client", "assets", "assets/stylesheets/vendor/admin/rails_admin", "assets/fonts"]
     for asset_path in asset_paths

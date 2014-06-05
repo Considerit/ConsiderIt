b77312a79186630efded28779c94f5f8f5c68819
Travis Kriplean <tkriplean@gmail.com>
2014-06-04 21:23:05 -0700

    simplify to single file for client
    - move to backbone router
    - put router in bottom of react prototype file
    - eliminate backbone.marionette
    
    
    Former-commit-id: 5fd22c89fe408c2b5fabb08413ffb9195dad6d19 [formerly 3a135677c7673c424e8a245808dd7b7203a25060] [formerly cd0bd3cfeb1cfbbc041f947d2acc168e621af4e7 [formerly e00d28c3e40430c9b2759dafe16ca3cf56fc2a31]] [formerly 93eb95f7abe2729a935b1cd1588b74fcd2b8c3c8 [formerly 68724b3d52d197c9ce94589c718e783a06ffa348] [formerly ad98573036a89233f4fb526298f3f8fe71fc3d59 [formerly ad98573036a89233f4fb526298f3f8fe71fc3d59 [formerly eed4b3e8484f05ebb29daa865bbe3cc264fc4417]]]]
    Former-commit-id: ba615c933f1c8aeb37ade1f57dfecda518ae4969 [formerly 52b04c6d773375a9ec11f1568268b6dce3e5005c] [formerly 736935bf80b022f4ee34aeec863f5b742211897c [formerly 2a19a1ece22e474d52f1862098bc2a30f3dcf318]]
    Former-commit-id: 7a82f30265536cec4396988ce791734ea204f97e [formerly 6bb1af47a05f1f56602438787c61ed1dfdd3a6fb]
    Former-commit-id: 01c0ca3031eb5a3779c4bc6b75d7ec5be3e70c48
    Former-commit-id: 1d28d3d3a815ffbf3f105163eba14e0fd8a85cab

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -5,14 +5,11 @@
 
 //= require vendor/underscore
 //= require vendor/backbone
-//= require vendor/marionette
 
 //= require js-routes
 
 //= require vendor/main/jquery.ui
 //= require_tree ./vendor/main
 
-//= require client
-//= require franklin_app
 //= require_tree ../../@client/vendor_extensions
 //= require_tree ../../@client/components

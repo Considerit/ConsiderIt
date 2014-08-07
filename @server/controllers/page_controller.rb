33a89f07399a45e96abc23e90d44741413346b62
Michael Toomim <toomim@gmail.com>
2014-08-07 10:55:04 -0700

    Controller edits for activerest. New page controller.
    Former-commit-id: a74545b97fe9d960ac5e6680b18aca5445b29441 [formerly 51fc61b5ed1cc4e903597a4b5c17e1544c08607a] [formerly e52e3c6a5df5b6675687a0c6eb4c36ddc22ccecb [formerly e62b3b256d26c6ee1c2e62aee377753f535e8979]] [formerly c149de88b2c757cf7b4aea286adac77134faa2cb [formerly 924d0ea90bca661b4439bfc779a54717195918aa] [formerly 01955fad7dadbf5fd8d77560d06fbcdf5bbf26dc [formerly 01955fad7dadbf5fd8d77560d06fbcdf5bbf26dc [formerly cd52dfdba32a547790578b3289e44a4832fb4e04]]]]
    Former-commit-id: e83112fa27edec160f03668832c3c2aafce49c7f [formerly 7afb067dcb6b130c60fcfa31c7ba18c8b304963a] [formerly 88f0c64bcb866fec810a6f680a50c6c6896b31dd [formerly fac3fbf382194a182113d965e4ff300961c1d4fd]]
    Former-commit-id: c2bf0a5259ed678be292c1c9e544222a2e14bb7b [formerly 08093c52f3f4c1f2f3bcb8d53e70828d2f2d1bff]
    Former-commit-id: 5680836ab2de20620566e34a743968c11752679b
    Former-commit-id: be7846bc9a3e93c209a9ba334941dfdccbd11e2d

---
+++ @server/controllers/page_controller.rb
@@ -0,0 +1,18 @@
+class PageController < ApplicationController
+  respond_to :json
+  def show
+    proposal = Proposal.find_by_long_id(params[:id])
+    return if !proposal or cannot?(:read, proposal)
+
+    if !session.has_key?(proposal.id)
+      ApplicationController.reset_user_activities(session, proposal)
+    end
+
+    result = proposal.full_data(current_tenant,
+                                current_user,
+                                session[proposal.id],
+                                can?(:manage, proposal))
+    result['key'] = "/page/#{params[:id]}"
+    render :json => result
+  end
+end

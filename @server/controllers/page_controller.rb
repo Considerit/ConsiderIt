ea1a889e6010730350dddcb3ba940a18510cfe43
Michael Toomim <toomim@gmail.com>
2014-08-14 22:01:06 -0700

    .
    Former-commit-id: ba781904eb2c1529bf44c9df5c9dc0da4d84e436 [formerly 01ab0aaf914c5476ee247298653a84d4e9cab8a9] [formerly bbfed10178dcfe5c165cb41de8261f8d3753798d [formerly f31eccbb4193dce0f5c59beedb5aeb3c1e94ee2d]] [formerly 1469d05342607c37b6e29fa0fa9964a930dbff29 [formerly 8eb8e0d855cb1b902ec87b20e47a2151d26682d7] [formerly 18f2562bb7c2e9824716b4ad457bbc08034e2386 [formerly 18f2562bb7c2e9824716b4ad457bbc08034e2386 [formerly e12d8bd3466dbf92cc0f614d1dbe3b58dce10afe]]]]
    Former-commit-id: 18927a6b7a7fa31aa30894e553be8b4229bbd6e7 [formerly 9a84d5d59ea1fd3809b01feb9dceb337457b4d64] [formerly d72869e96c0a1381c4d5ef8b83f4d005fe2b61d4 [formerly a8ded9a3b8ec56505a6a584854216c2f86d0c1c6]]
    Former-commit-id: 9c29276651a2e495794121fd17a045df14aa92bd [formerly ce31c1516ac1c2f72ec488acebc1a4a2f01f2308]
    Former-commit-id: af2f33340a929e9063453ad19fa9ce6c8cdde1e6
    Former-commit-id: 804556f2e40bc6576f9aa37a1db93a3a285b21bf

--- @server/controllers/page_controller.rb
+++ @server/controllers/page_controller.rb
@@ -4,9 +4,9 @@ class PageController < ApplicationController
     proposal = Proposal.find_by_long_id(params[:id])
     return if !proposal or cannot?(:read, proposal)
 
-    if !session.has_key?(proposal.id)
-      ApplicationController.reset_user_activities(session, proposal)
-    end
+    # if !session.has_key?(proposal.id)
+    #   ApplicationController.reset_user_activities(session, proposal)
+    # end
 
     result = proposal.full_data(current_tenant,
                                 current_user,

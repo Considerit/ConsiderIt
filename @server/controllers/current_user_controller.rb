4fc1b0e6ad9d427137555fa8b83227910efa1702
Michael Toomim <toomim@gmail.com>
2014-08-20 14:10:47 -0700

    decuss
    Former-commit-id: 0b547716c8c40be4f152e12ef3c32f749180d774 [formerly 9b57ae483188977d481628011757a028397ac1cf] [formerly 8c6e4236b278a36d3641550df889d655a5dc5a46 [formerly f4efcd63539e05a3fee9f3c9244375e28d352a1d]] [formerly 7ab666a30f3accf6490df65f322fc3c9bfb52ed2 [formerly f902300fbf4e19378be0e96b6ebdbfbb3bed33b8] [formerly 407734158b89d5c6299aaffe847323d63293e635 [formerly 407734158b89d5c6299aaffe847323d63293e635 [formerly 2f0beac55fd90dfe9e813eaf501547af279eae78]]]]
    Former-commit-id: 2eb974e4f590f98d1bfde4c6b260e1f8f898dc4b [formerly 30681da610e06974b894d057098594b8353c5d3d] [formerly bd1a52c8242fdad3acc35196c52e09f953d4a401 [formerly d1144299d0c86065242b041166a7c8df643b4e4a]]
    Former-commit-id: b3288c1e6490f0f91359e544b1f2092facd94466 [formerly 051bd793f5c7cf7b7faa237244516da8d4757247]
    Former-commit-id: 7c0e29061f50d8ee9f4a781c6d478f4532c55247
    Former-commit-id: d27e21d1c3b10ac90199b2ad2b1383fd621ec078

--- @server/controllers/current_user_controller.rb
+++ @server/controllers/current_user_controller.rb
@@ -173,7 +173,7 @@ class CurrentUserController < DeviseController
         if User.find_by_email(params[:email])
           errors[:register].append 'That email is not available.'
         # And that it's valid
-        elsif false # I don't know how to check an email address in rails, so fuck it!
+        elsif false # I don't know how to check an email address in rails, so punt!
           errors[:register].append 'Bad email address'
         else
           puts('XXX Need to figure out how to validate email address in here')
@@ -198,7 +198,7 @@ class CurrentUserController < DeviseController
             raise "Error saving this user's password"
           end
           puts("Current user is now #{current_user.id}")
-          puts("Ok, logging back in... fucking asshole!")
+          puts("Ok, logging back in...")
           sign_in :user, current_user, :bypass => true
           puts("Current user is now #{current_user.id}")
         end

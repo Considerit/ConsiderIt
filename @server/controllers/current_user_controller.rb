719eefbd7dcddcc5b246e5e8e31c18c9a7089329
Travis Kriplean <tkriplean@gmail.com>
2014-08-22 19:38:27 -0400

    cleanup
    Former-commit-id: 91c07f42a7780de652e2724519361962ef5e5691 [formerly 093fc8a8a66133b0bc3f485e8b387cd2b76ac308] [formerly 61720827782870b2ed67d0326b362ba7abdef702 [formerly 10b90639dd2bda8d576de0b67ec4ab965d5e4412]] [formerly 2462357887e4cfb294ef64a2660a252645c0050d [formerly 2c9e0bb52ede8d546a777245ba6ad232c44b102d] [formerly d450c74ab4087fd889ef4b4511ec09faf204fe15 [formerly d450c74ab4087fd889ef4b4511ec09faf204fe15 [formerly 25d40d123867d2113188ed0368444399b42d56c4]]]]
    Former-commit-id: b5d79a784318bbbeb79dd7762178a9d5922a8e74 [formerly 28278e87a3eb7b5197ea841f50eda7dc824f4047] [formerly 6bdaa4da4ee7f7a716192dd8dc9460d37fa14371 [formerly ec2b0f44c19fbea82aa66f7d8c47cfff0cce2f24]]
    Former-commit-id: e09a5a4bde43e8d9b50677e516e87b3c49e85d53 [formerly b6bfa36c3bc907d1c348ad887ce02dcb6e6b5dce]
    Former-commit-id: c3c663a814c6513554dcc9f857ce7be700597320
    Former-commit-id: 15aae1ebe2a0885648e639300e595a4b95e72258

--- @server/controllers/current_user_controller.rb
+++ @server/controllers/current_user_controller.rb
@@ -184,9 +184,6 @@ class CurrentUserController < DeviseController
           puts("Current user is now #{current_user.id}")
         end
       end
-
-      # render :json => [form_authenticity_token]
-      # return
     end
 
     # Register the account
@@ -198,8 +195,6 @@ class CurrentUserController < DeviseController
       signed_pledge = params[:signed_pledge]
 
       puts('XXX Need to check password or third_party login')
-      puts('XXX Need to check the pledge')
-
 
       if has_name and can_login and signed_pledge
         current_user.registration_complete = true
@@ -325,71 +320,11 @@ class CurrentUserController < DeviseController
     update_via_third_party
   end
 
-
-
   # when something goes wrong in an oauth transation, this method gets called
   def failure
     # TODO: handle this gracefully for the user
     raise "Something went wrong in authenticating your account"
   end
-
-  # /end oauth
-
-  # def content_for_user
-  #   # proposals that are written by this user; private proposals this user has access to
-  #   proposals = Proposal.content_for_user(current_user) || []
-
-  #   top = []
-
-  #   proposals.each do |prop|
-  #     top.push(prop.top_con) if prop.top_con
-  #     top.push(prop.top_pro) if prop.top_pro
-  #   end
-
-  #   points = {}
-  #   Point.where('id in (?)', top).public_fields.each do |pnt|
-  #     points[pnt.id] = pnt
-  #   end
-
-  #   current_user.points.published.where(:hide_name => true).public_fields.each do |pnt|
-  #     points[pnt.id] = pnt
-  #   end
-
-  #   respond_to do |format|
-  #     format.json {
-  #       render :json => {
-  #         :points => points.values,
-  #         :proposals => proposals,
-  #         :opinions => current_user.opinions.published
-  #       }
-  #     }
-  #   end
-  # end
-
-  # # right now this is only used by LVG for zip codes...
-  # # TODO: move this to a taggable controller, and specify the model type being tagged
-  # def set_tag
-
-  #   new_tags = params[:tags].split(';')
-
-  #   if current_user
-  #     current_user.addTags new_tags, params['overwrite_type']
-  #     tags = current_user.getTags()
-  #   else
-  #     tags = session.has_key?(:tags) ? session[:tags] : []
-  #     if params['overwrite_type']
-  #       types = new_tags.map{|t| t.split(':')[0]}
-  #       tags.delete_if {|t| types.include?(t.split(':')[0])}
-  #     end
-  #     tags |= new_tags
-  #     session[:tags] = tags
-  #   end
-
-  #   respond_to do |format|
-  #     format.json { render :json => { :success => true, :user_tags => tags} }
-  #   end
-  # end  
-
 
   private
 

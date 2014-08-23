a0e214bb484098754aa77b9805e22b98f982bb5a
Michael Toomim <toomim@gmail.com>
2014-08-22 18:03:38 -0700

    Detect duplicate inclusions
    Former-commit-id: 117a7de156bc13cb548df57e182ec7fdac1c7995 [formerly 086b8db05cd9b942e173ec94e5eaf40531e20712] [formerly d1d41b39f9950d30ef60a0e21e9410b3bda214df [formerly 82929c4eb02cdaaa0e06fb0db0371956c5c1f311]] [formerly b2c8151e9225b2ed8204b70d5e5f62c7df1add75 [formerly b313f682df6917fdbe520dec152af7a998f5fc0d] [formerly b77b82d00e4e1e4f1ade658c961b3bd7f8140244 [formerly b77b82d00e4e1e4f1ade658c961b3bd7f8140244 [formerly 30d2ac9f2877be75516afabb3946b849b5646d72]]]]
    Former-commit-id: b8fd0a06ef3e8a4be5d14738714926d6e1db34d3 [formerly 14e341d13a0cb512ce08909971ebe93af1a045a8] [formerly af45a3f12a838e2307b2664e4fbc39152ea19de8 [formerly e89192de992076cddc3f30b96850b2a56866999d]]
    Former-commit-id: 305afb2178e332175eeee5e9127da367374160b4 [formerly 75d69b6dcd67ac7fe56029d2bf10416acfec2210]
    Former-commit-id: 50c9112ddbc729f25e854ccfb7d57879cf2fc222
    Former-commit-id: 56b0db87f7f8046ec2c25fd483047e8f37601665

--- @server/models/inclusion.rb
+++ @server/models/inclusion.rb
@@ -7,7 +7,29 @@ class Inclusion < ActiveRecord::Base
 
   acts_as_tenant(:account)
     
+  after_save :check_dupes
+
   #scope :by_user_with_stance, proc {|stance_segment| joins(:opinion).where("opinions.stance_segment=" + stance_segment.to_s) }
 
+  def check_dupes
+    user_points    = Inclusion.where(:user_id => self.user_id, :point_id => self.point_id)
+    point_opinions = Inclusion.where(:point_id => self.point_id, :opinion_id => self.opinion_id)
+    if user_points.length > 1
+      raise "This is a duplicate user_point user #{self.user_id} point #{self.point_id} of n=#{user_points.length} #{user_points.map{|i| i.id}}"
+    end
+    if point_opinions.length > 1
+      raise "This is a duplicate point_opinion point #{point_id} user #{opinion_id} of n=#{point_opinions.length}"
+    end
+  end
   
+  def self.find_all_dupes
+    for i in Inclusion.all.order(:user_id)
+      begin
+        i.check_dupes
+      rescue
+        puts"#{$!}"
+      end
+    end
+    nil
+  end
 end

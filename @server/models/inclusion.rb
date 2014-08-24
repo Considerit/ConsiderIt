e7c33de144eb124f69804a13c3038da56faadf05
Travis Kriplean <tkriplean@gmail.com>
2014-08-23 23:42:04 -0400

    ability to destroy duplicate inclusions
    Former-commit-id: 50279ea66b2b6eff26c6d62c2b5a92bf9e1abae2 [formerly 230a307e8080ec49fbc9725f90910c3d215b1033] [formerly e66118f566986d5ae620f5c5103be1a9e19fda4a [formerly b7225f65f7927b6896550ab29b99853c307cc300]] [formerly c0f7697175d683a25ac275c2e32638c63c80c867 [formerly 95765feac8ff6844359a5712e41d4a4651202eaa] [formerly f2b1579f31329bd98645255e17d49a395668d57a [formerly f2b1579f31329bd98645255e17d49a395668d57a [formerly fb3449d3efd32c0c1587c9bb6838c9209881d9d8]]]]
    Former-commit-id: 78a4dac5f0d8bfd4479c6bc5d05a67c13bd1ddb9 [formerly d0957df117ed3675a73fde16d368aef6d06eb9b8] [formerly a665a3bb954abfedd0f3094dfc1f178a829351fd [formerly aae307a833e4a8c7323f80d5fff9870d6f0cb214]]
    Former-commit-id: 2a0bb7b0b77056a52911dc59c6ed826d1a3f8f5f [formerly c79a5a5d52ebc67e62c1ce5d8aa609ee349820c1]
    Former-commit-id: 56d9415bc36fd9dbf38e3f33d32ac4855fff7f46
    Former-commit-id: 12fab80321f1ad5e59be5715c9eabecd1e0764c8

--- @server/models/inclusion.rb
+++ @server/models/inclusion.rb
@@ -8,17 +8,24 @@ class Inclusion < ActiveRecord::Base
     
   after_save :check_dupes
 
-  def check_dupes
+  def check_dupes(purge = false)
     user_points    = Inclusion.where(:user_id => self.user_id, :point_id => self.point_id)
     if user_points.length > 1
-      raise "This is a duplicate user_point user #{self.user_id} point #{self.point_id} of n=#{user_points.length} #{user_points.map{|i| i.id}}"
+      error_str = "#{created_at} This is a duplicate user_point user #{self.user_id} point #{self.point_id} of n=#{user_points.length} #{user_points.map{|i| i.id}}"
+      if purge
+        user_points.map {|i| i.id}[1..99999999].each do |dup|
+          Inclusion.find(dup).destroy
+        end
+
+      end
+      raise error_str
     end
   end
   
-  def self.find_all_dupes
-    for i in Inclusion.all.order(:user_id)
+  def self.find_all_dupes(purge = false)
+    for i in Inclusion.all.order(:created_at)
       begin
-        i.check_dupes
+        i.check_dupes purge
       rescue
         puts"#{$!}"
       end

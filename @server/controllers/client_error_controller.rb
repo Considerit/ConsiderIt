e656bb081e06e87f2358e2d9f1975248ca1f4f33
Michael Toomim <toomim@gmail.com>
2014-08-24 20:39:58 -0700

    Client errors now use database
    Former-commit-id: 94ea067bb252884644da112a842b8b02e5e87dc7 [formerly 6fa10952f85571840465cceb54ce4e9351a52c4e] [formerly 14f87282718cbbf979f68ffb651c1c1cbefc8ce0 [formerly 644f118a5214f95a74fa84b960ae3f9caddf63a3]] [formerly 688793b8ec6fed733ccb097896f13bb149a37d9b [formerly a69f0a18fff50508bad8db05f4790f73ef1ac828] [formerly fa2e63b2bef10ddaa2f56bb635f8a58fdd9a0092 [formerly fa2e63b2bef10ddaa2f56bb635f8a58fdd9a0092 [formerly 287467a91360f5bc1199d177a67cac5804653226]]]]
    Former-commit-id: 751d26d5b0aa305b4904e1198e4b282c5bdf1b8c [formerly 22e3838127b9e4421c8e3725f37ed80a9ea4e64b] [formerly a1f7ab70c8c10433e89df46f4ba352bb5022a14d [formerly 3e71c153d46587b549447391a7437be42d47aa5d]]
    Former-commit-id: c490175194fac10d073eadc46db97400eab352c5 [formerly 3c5d80517c30defb7eff7e423c9e17f4a95eae2a]
    Former-commit-id: 4b35c35381879d47401686c4c96e814f593e4d64
    Former-commit-id: 4017fc5f2e24760ccc687b413b349f09769e8b90

--- @server/controllers/client_error_controller.rb
+++ @server/controllers/client_error_controller.rb
@@ -1,7 +1,12 @@
-class ErrorController < ApplicationController
+class ClientErrorController < ApplicationController
   def create
     ## Store this somewhere instead of a puts
-    puts("Client error: #{params.to_json}")
+    ClientError.create trace: params[:stack],
+                       ip: request.env['REMOTE_ADDR'],
+                       user_agent: request.env["HTTP_USER_AGENT"],
+                       user_id: current_user && current_user.id
     render :json => ["Thanks for the error report!"]
   end
 end
+
+

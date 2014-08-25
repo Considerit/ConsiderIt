e5c6262fe976052e82051e19e1f1319a1fc6097a
Travis Kriplean <tkriplean@gmail.com>
2014-08-25 02:49:57 -0400

    cleanup

--- @server/controllers/application_controller.rb
+++ @server/controllers/application_controller.rb
@@ -7,7 +7,6 @@ class ApplicationController < ActionController::Base
   before_action :theme_resolver
   before_action :init_thread_globals
   after_action  :pageview
-  #include CacheableCSRFTokenRails
 
   def render(*args)
     if Rails.cache.read("avatar-digest-#{current_tenant.id}").nil?
@@ -39,106 +38,11 @@ class ApplicationController < ActionController::Base
       @limited_user_email = @limited_user.email
     end
 
-    #TODO: what does this do?
-    if args && args.first.respond_to?('has_key?')
-      args.first[:layout] = false if request.xhr? && args.first[:layout].nil?
-    elsif args && args.last.respond_to?('has_key?')
-      args.last[:layout] = false if request.xhr? && args.last[:layout].nil?
-    else
-      args.append({:layout => false}) if request.xhr?
-    end
 
     if current_tenant.host.nil?
       current_tenant.host = request.host
       current_tenant.host_with_port = request.host_with_port
       current_tenant.save
-    end
-
-    #TODO: now that we have a global redirect to home#index for non-ajax requests, can we move this to home controller?
-    if !request.xhr?
-      response.headers["Strict Transport Security"] = 'max-age=0'
-      
-      @page = request.path
-
-
-      #### Setting meta tag info ####
-      if APP_CONFIG[:meta].has_key? current_tenant.identifier.intern
-        meta = APP_CONFIG[:meta][current_tenant.identifier.intern]
-        using_default_meta = false
-      else 
-        meta = APP_CONFIG[:meta][:default]
-        using_default_meta = true
-      end
-
-      if using_default_meta
-        @title = current_tenant.app_title || meta[:title]
-        if current_tenant.header_text
-          description = ActionView::Base.full_sanitizer.sanitize(current_tenant.header_text, :tags=>[])  
-          if current_tenant.header_details_text && current_tenant.header_details_text != ''
-            description = "#{description} - #{ActionView::Base.full_sanitizer.sanitize(current_tenant.header_details_text, :tags=>[])}"
-          end
-        else
-          description = meta[:description]
-        end
-      else
-        @title = meta[:title] || current_tenant.app_title
-        description = meta[:description]
-      end
-
-      @title = @title.strip
-      @keywords = meta[:keywords].strip
-      @description = description.strip
-      ######
-
-      # @users = ActiveSupport::JSON.encode(ActiveRecord::Base.connection.select( "SELECT id,name,avatar_file_name,created_at, metric_influence, metric_points, metric_conversations,metric_opinions,metric_comments FROM users WHERE account_id=#{current_tenant.id}"))
-      
-      # active_proposals = Proposal.open_to_public.active.browsable
-      # inactive_proposals = Proposal.open_to_public.inactive.browsable
-
-      # proposals_active_count = active_proposals.count
-      # proposals_inactive_count = inactive_proposals.count
-
-
-      # proposals = current_tenant.enable_hibernation ? inactive_proposals : active_proposals
-
-
-      # top = []
-      # top_con_qry = proposals.where 'top_con IS NOT NULL'
-      # if top_con_qry.count > 0
-      #   top += top_con_qry.select(:top_con).map {|x| x.top_con}.compact
-      # end
-
-      # top_pro_qry = proposals.where 'top_pro IS NOT NULL' 
-      # if top_pro_qry.count > 0
-      #   top += top_pro_qry.select(:top_pro).map {|x| x.top_pro}.compact
-      # end
-      
-      # top_points = {}
-      # Point.where('id in (?)', top).public_fields.each do |pnt|
-      #   top_points[pnt.id] = pnt
-      # end
-
-      # @opinions = {}
-      # if current_user
-      #   hidden_proposals = Proposal.content_for_user(current_user)
-      #   hidden_proposals.each do |hidden|          
-      #     top_points[hidden.top_pro] = Point.find(hidden.top_pro) if hidden.top_pro
-      #     top_points[hidden.top_con] = Point.find(hidden.top_con) if hidden.top_con
-      #   end
-      #   proposals += hidden_proposals
-      #   @opinions = current_user.opinions.published
-      # end
-
-
-      # @proposals = {
-      #   :proposals => proposals,
-      #   :points => top_points.values,
-      #   :proposals_active_count => proposals_active_count,
-      #   :proposals_inactive_count => proposals_inactive_count,
-      # }
-
-      # @public_root = Rails.application.config.action_controller.asset_host.nil? ? "" : Rails.application.config.action_controller.asset_host
-
     end
 
     super

5011d763622761467cfb492ecf6adc33de715651
Travis Kriplean <tkriplean@gmail.com>
2014-08-25 23:28:14 -0400

    making it clear which emails have been migrated to new system
    Former-commit-id: 354595ad1ed6101029983e7954c404f90c5abdc1 [formerly fb7b087e6f11a79566fd3f45827fca0d33b63c1f] [formerly d0f279fdeea8fcbb96b9e4a4ea5313021e9e7bf2 [formerly 31c093529403d5137902023719638c9d2d05c37d]] [formerly 6d98449d0641197a06ca430393c9f7d93aeb4f7a [formerly 1d24f31f59741c9e4b2a1b65bd97756ba81c7c7f] [formerly 0d0e385d9ac324a3fd4a4fd0f7f898447fd17722 [formerly 0d0e385d9ac324a3fd4a4fd0f7f898447fd17722 [formerly 80520e62660d13d92c0a504f4d9839ca74bc2232]]]]
    Former-commit-id: 6437d91e3fd19d1e32976c08c528389462c04749 [formerly 3a323180199466e8e85d05626a22c5a8ae1b9b1a] [formerly 54022c85ff8a6268f96205419c85c6d91915d2e7 [formerly 616f124c2152afc73621ae278ef7b6b636011d0e]]
    Former-commit-id: f709ca8ef97c8ce882b97389d860673b15cdd861 [formerly c69f8d415edcc42813aa15db9e1761e80804cfa7]
    Former-commit-id: 63e6f37c0cb6c57dfbc23f6a40631926263d8333
    Former-commit-id: b37e632f6e8a53af164cd5b196fffd24912a6077

--- @server/mailers/event_mailer.rb
+++ @server/mailers/event_mailer.rb
@@ -23,39 +23,39 @@ class EventMailer < Mailer
   end
 
   #### DISCUSSION LEVEL ####
-  def discussion_new_proposal(user, proposal, options, notification_type = '')
-    @notification_type = notification_type
-    @user = user
-    @proposal = proposal
-    @host = options[:host]
-    @options = options
-    @url = "#{@host}/#{@proposal.long_id}"
+  # def discussion_new_proposal(user, proposal, options, notification_type = '')
+  #   @notification_type = notification_type
+  #   @user = user
+  #   @proposal = proposal
+  #   @host = options[:host]
+  #   @options = options
+  #   @url = "#{@host}/#{@proposal.long_id}"
 
-    subject = "new proposal \"#{@proposal.title}\""
+  #   subject = "new proposal \"#{@proposal.title}\""
 
-    from = format_email(options[:from], options[:app_title])
-    to = format_email user.email, user.name
+  #   from = format_email(options[:from], options[:app_title])
+  #   to = format_email user.email, user.name
 
-    mail(:from => from, :to => to, :subject => "[#{options[:app_title]}] #{subject}")
+  #   mail(:from => from, :to => to, :subject => "[#{options[:app_title]}] #{subject}")
 
-  end
+  # end
 
   #### PROPOSAL LEVEL ####
 
-  def proposal_milestone_reached(user, proposal, next_aggregate, options )
-    @user = user
-    @proposal = proposal
-    @next_aggregate = next_aggregate
-    @host = options[:host]
-    @options = options
+  # def proposal_milestone_reached(user, proposal, next_aggregate, options )
+  #   @user = user
+  #   @proposal = proposal
+  #   @next_aggregate = next_aggregate
+  #   @host = options[:host]
+  #   @options = options
 
-    from = format_email(options[:from], options[:app_title])
-    to = format_email user.email, user.name
+  #   from = format_email(options[:from], options[:app_title])
+  #   to = format_email user.email, user.name
 
-    subject = "update on \"#{@proposal.title}\""
-    mail(from => from, :to => to, :subject => "[#{options[:app_title]}] #{subject}")
+  #   subject = "update on \"#{@proposal.title}\""
+  #   mail(from => from, :to => to, :subject => "[#{options[:app_title]}] #{subject}")
 
-  end
+  # end
 
   def proposal_new_point(user, pnt, options, notification_type)
     @notification_type = notification_type
@@ -80,52 +80,52 @@ class EventMailer < Mailer
 
   #### POINT LEVEL ####
 
-  def point_new_comment(user, pnt, comment, options, notification_type)
-    @notification_type = notification_type
-    @user = user
-    @point = pnt
-    @comment = comment
-    @proposal = @point.proposal
-    @host = options[:host]
-    @options = options
+  # def point_new_comment(user, pnt, comment, options, notification_type)
+  #   @notification_type = notification_type
+  #   @user = user
+  #   @point = pnt
+  #   @comment = comment
+  #   @proposal = @point.proposal
+  #   @host = options[:host]
+  #   @options = options
 
 
-    to = format_email user.email, user.name
-    from = format_email(options[:from], options[:app_title])
+  #   to = format_email user.email, user.name
+  #   from = format_email(options[:from], options[:app_title])
 
-    if notification_type == 'your point'
-      subject = "new comment on a #{@point.is_pro ? 'pro' : 'con'} point you wrote"
-    elsif notification_type == 'participant'
-      subject = "#{@comment.user.username} commented on a discussion in which you participated"
-    elsif notification_type == 'included point'
-      subject = "new comment on a #{@point.is_pro ? 'pro' : 'con'} point you follow"
-    else
-      subject = "new comment on a #{@point.is_pro ? 'pro' : 'con'} point you follow"
-    end
+  #   if notification_type == 'your point'
+  #     subject = "new comment on a #{@point.is_pro ? 'pro' : 'con'} point you wrote"
+  #   elsif notification_type == 'participant'
+  #     subject = "#{@comment.user.username} commented on a discussion in which you participated"
+  #   elsif notification_type == 'included point'
+  #     subject = "new comment on a #{@point.is_pro ? 'pro' : 'con'} point you follow"
+  #   else
+  #     subject = "new comment on a #{@point.is_pro ? 'pro' : 'con'} point you follow"
+  #   end
 
-    mail(:from => from, :to => to, :subject => "[#{options[:app_title]}] #{subject}")
-  end
+  #   mail(:from => from, :to => to, :subject => "[#{options[:app_title]}] #{subject}")
+  # end
 
-  def point_new_assessment(user, pnt, assessment, options, notification_type)
-    @notification_type = notification_type
-    @user = user
-    @point = pnt
-    @assessment = assessment
-    @proposal = @point.proposal
-    @host = options[:host]
-    @options = options
-    from = format_email(options[:from], options[:app_title])
+  # def point_new_assessment(user, pnt, assessment, options, notification_type)
+  #   @notification_type = notification_type
+  #   @user = user
+  #   @point = pnt
+  #   @assessment = assessment
+  #   @proposal = @point.proposal
+  #   @host = options[:host]
+  #   @options = options
+  #   from = format_email(options[:from], options[:app_title])
 
-    if notification_type == 'your point'
-      subject = "a point you wrote has been fact checked"
-    else
-      subject = "a point you follow has been fact checked"
-    end
+  #   if notification_type == 'your point'
+  #     subject = "a point you wrote has been fact checked"
+  #   else
+  #     subject = "a point you follow has been fact checked"
+  #   end
 
-    to = format_email user.email, user.name
+  #   to = format_email user.email, user.name
 
-    mail(:from => from, :to => to, :subject => "[#{options[:app_title]}] #{subject}")
-  end
+  #   mail(:from => from, :to => to, :subject => "[#{options[:app_title]}] #{subject}")
+  # end
 
 
 end

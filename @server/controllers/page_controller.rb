8691857474ee1d48364fc05f377494e3583fa351
Travis Kriplean <tkriplean@gmail.com>
2014-09-10 12:38:49 -0700

    homepage implementation
    This code is not adequately refactored and commented. I will do so
    soon. But I need to deploy this to our test server for demo purposes.
    
    
    Former-commit-id: 4153de75adb4dd4533450ab7f01d4ccde032c021 [formerly 3763badcdd4c436035b3b3b97e72c14a40d53c26] [formerly fa9289f8c97347f59795bbbf0e668bdef9b8b73c [formerly 43cbb805d79fbd7b10124232cf2efb3c5635c98c]] [formerly e972061a0a61e3e60a95dadde6a2189da41ac461 [formerly 8e2e5ee9b19c6668655e59cfa965c00053b40e8a] [formerly 9be2484f1f3099ef53ecf1ee6e089c1bb2692a4f [formerly 9be2484f1f3099ef53ecf1ee6e089c1bb2692a4f [formerly a871bc0f7b928a24ce48a792292926ca1b6d0f78]]]]
    Former-commit-id: 20679669b4df27bd0894a1990da42a0ae16b61bd [formerly 69d9503b5ab1b9b24d3b8cc2aa0d850d1e18c6a8] [formerly 079790856b6524dc3ba2e8ca0d863d41adaff01e [formerly b76f76772fed1c972f2393bdf2b21a029ab4ee15]]
    Former-commit-id: eaef92e0094b913935f66935aa45f062d30192db [formerly c58b00e5b30c6a4ad45d550c8ef946eb8a31cdd5]
    Former-commit-id: c8418a3a70596164726b665f453bd01e7ba640a8
    Former-commit-id: f93f53efc128fd7fedcaef55a3024a87da97df7f

--- @server/controllers/page_controller.rb
+++ @server/controllers/page_controller.rb
@@ -5,26 +5,67 @@ class PageController < ApplicationController
     case params[:id]
 
     when 'homepage'
+      result = get_proposal_summaries()
+      result['users'] = get_all_user_data()
 
-      result = {
-        :proposal_summaries => current_tenant.proposals.open_to_public.browsable.map {|proposal| proposal.proposal_summary()}
-      }
-
-    else
+    else # if proposal
 
       proposal = Proposal.find_by_long_id(params[:id])
       if !proposal or cannot?(:read, proposal)
-        # TODO: return with a good HTML code for this case
+        # TODO: return an appropriate HTML code for this case
         render :json => {:result => 'Permission denied'}
         return
       end
 
       result = proposal.full_data(can?(:manage, proposal))
+      result.update(get_proposal_summaries())
+      result['users'] = get_all_user_data()
 
     end
 
     result['customer'] = current_tenant
     result['key'] = "/page/#{params[:id]}"
     render :json => result
+  end
+
+  private
+
+  # TODO: Not satisfied with this code. How should these requests be filled? 
+  #       proposal.full_data returns proposal data that is also 
+  #       included in get_proposal_summaries. It also returns your_opinion, which is returned in
+  #       your_opinions _if_ it is already published.
+  def get_all_user_data
+    users = ActiveRecord::Base.connection.select( "SELECT id,name,avatar_file_name FROM users WHERE account_id=#{current_tenant.id} AND (registration_complete=true OR id=#{current_user.id})")
+    users = users.as_json
+    jsonify_objects(users, 'user')
+  end
+
+  def get_proposal_summaries
+    clustered_proposals = {}
+
+    current_tenant.proposals.open_to_public.browsable.each do |proposal|        
+      clustered_proposals[proposal.cluster] = [] if !clustered_proposals.has_key? proposal.cluster
+      clustered_proposals[proposal.cluster].append proposal.proposal_summary()
+    end
+
+    if current_tenant.identifier == 'livingvotersguide'
+      # manual ordering for LVG
+      cluster_order = ['Statewide measures', 'Advisory votes'] 
+    else
+      #TODO: order the group for the general case. Probably sort groups by the most recent Opinion. 
+      cluster_order = clustered_proposals.keys()
+    end
+    proposals = {
+      key: 'viewable_proposals',
+      clusters: cluster_order.map {|cluster| {:name => cluster, :proposals => clustered_proposals[cluster] }}
+    }
+
+    result = {
+      :viewable_proposals => proposals,
+      :your_opinions => current_user.opinions.published.map {|o| o.as_json}
+    }
+
+    result
+
   end
 end

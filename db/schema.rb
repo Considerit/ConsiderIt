95ef1277d87a4b6cc915738af3e39b8e153ab8ad
Travis Kriplean <tkriplean@gmail.com>
2014-09-15 10:11:10 -0700

    about page that iframes to public html doc

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140909041312) do
+ActiveRecord::Schema.define(version: 20140915155758) do
 
   create_table "accounts", force: true do |t|
     t.string   "identifier"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20140909041312) do
     t.boolean  "enable_hibernation",                       default: false
     t.boolean  "enable_sharing",                           default: false
     t.string   "hibernation_message"
+    t.string   "about_page_url"
   end
 
   add_index "accounts", ["identifier"], name: "by_identifier", length: {"identifier"=>10}, using: :btree

3cb4c9b4af4a6edcc4215403ab408293c6564a93
Michael Toomim <toomim@gmail.com>
2014-09-17 23:01:41 -0700

    Added recent contributors column
    Former-commit-id: 83c334689903f864ea9abac59bb8bf7b4918b746 [formerly 78a8816663e967e6aaf259bfae0e7e034cea98ce] [formerly 4d667113d21e35453650a58d2973f03290245cf7 [formerly 6309b2fd688c0b540e3b976b3a90457f746707c1]] [formerly 8fbe7284eb8d0dfd7e027748ad4bde5b3815b72a [formerly 771bea4380f1f3c23c0e2a642e0942bf4061767a] [formerly c35778fc013d189ffd59c0a5804b99089ca7b048 [formerly c35778fc013d189ffd59c0a5804b99089ca7b048 [formerly a7c63124b270c4e626657c3b1acdffe292830f1c]]]]
    Former-commit-id: 666a3fea69dad6a87d3a964fd7643731746397e7 [formerly ce267aabf96f9270a9f536e713e879e559721236] [formerly e9685e12cf02e275781d0ce33ac809d1d48c195e [formerly 237961c91246b8a61beacdac25c5b67f1be8cf65]]
    Former-commit-id: 8183fa2c73bf5b71905afe812a3ae3f3798fd588 [formerly 41787fde8796914aa4e9941bea83970b41a49fd2]
    Former-commit-id: 3445e1c4e22ff22f349ba714bb514f667a22cf31
    Former-commit-id: f2e7cab9c8d3a0314099a3ee8637fcc6d1dcab75

--- @server/controllers/page_controller.rb
+++ @server/controllers/page_controller.rb
@@ -7,7 +7,8 @@ class PageController < ApplicationController
     when 'homepage'
       result = {
         # proposals: Proposal.summaries(), 
-        users: get_all_user_data()
+        users: get_all_user_data(),
+        contributors: recent_contributors()
       } 
       result['your_opinions'] = current_user.opinions.map {|o| o.as_json}
 
@@ -41,6 +42,11 @@ class PageController < ApplicationController
     users = ActiveRecord::Base.connection.select( "SELECT id,name,avatar_file_name FROM users WHERE account_id=#{current_tenant.id} AND (registration_complete=true OR id=#{current_user.id})")
     users = users.as_json
     jsonify_objects(users, 'user')
+  end
+
+  def recent_contributors
+    users = ActiveRecord::Base.connection.select( "SELECT u.id FROM users as u, opinions WHERE u.account_id=#{current_tenant.id} AND u.registration_complete=true AND opinions.user_id = u.id AND opinions.created_at > '#{9.months.ago.to_date}'")
+    return users.map {|u| "/user/#{u['id']}"}
   end
 
 end

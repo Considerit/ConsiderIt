2f76a76e80eb478c1fb57fc3ebfacd0b44bbb699
Travis Kriplean <tkriplean@gmail.com>
2014-09-21 15:18:37 -0700

    don't log every request as a page_view
    one step toward new site instrumentation
    
    
    Former-commit-id: 12205bd9808a827dd8824f0fb8990d9a2bbf835c [formerly a50a1c914f3b917693723e5e089df066073d623f] [formerly e4b165b4cbc3edfccead1902b9697962f9083f55 [formerly bdf165fe81f7f62bc662b8bda8e4296a0e714dbd]] [formerly a595d285f47409f2ba63886f235d81e440d6cb87 [formerly 14551a5ae43eaee2799043428a8aa4e85af5dd87] [formerly 1ce3a937a6b61065016a483792b557ec7cb4d341 [formerly 1ce3a937a6b61065016a483792b557ec7cb4d341 [formerly dfe7ebd6f4c45d9ada497e108c82dc1faabf4e3e]]]]
    Former-commit-id: 5a9b090dd1dc584b6231ff7579ffee83af62eea5 [formerly 58cc425b505bb1ce5f234070aeb1321568c4cc4d] [formerly 80d8edd26eda49940e216d9e89f4961a563021b0 [formerly 926ca3467e230c60a1acc0aa6ff8c8ed0af5c778]]
    Former-commit-id: 1887dea6b80df88fef527503db73fca025d9c53d [formerly 3d91f6f591690f0578cb40090a6cfb5bf47bb01f]
    Former-commit-id: dc0de5be7786ce9c19b8f61ad1686938af3272f6
    Former-commit-id: a2b8a428eb4d625523358a340f2b524cc8549911

--- @server/controllers/page_controller.rb
+++ @server/controllers/page_controller.rb
@@ -1,15 +1,17 @@
 class PageController < ApplicationController
   respond_to :json
-  
+
   def show
     case params[:id]
 
     when 'homepage'
-      dirty_key '/page/homepage'
+      key = '/page/homepage'
+      dirty_key key
 
     when 'about'
       # don't need anything special
-    
+      key = '/page/about'
+
     else # if proposal
 
       proposal = Proposal.find_by_long_id(params[:id])
@@ -21,12 +23,31 @@ class PageController < ApplicationController
         return
       end
 
-      dirty_key "/page/#{proposal.long_id}"
+      key = "/page/#{proposal.long_id}"
+      dirty_key key
 
     end
 
+    pageview key
+
     render :json => []
 
+  end
+
+  private 
+
+  def pageview(page)
+    begin
+      params = {
+        :account_id => current_tenant.id,
+        :user_id => current_user.id,
+        :url => request.fullpath,
+        :created_at => Time.current
+      }
+      PageView.create! ActionController::Parameters.new(params).permit!
+    rescue 
+      logger.info 'Could not create PageView'
+    end
   end
 
 end

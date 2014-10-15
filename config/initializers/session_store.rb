93b7302e9dfdef556531dd4ff9ef5bba27c49648
Travis Kriplean <tkriplean@gmail.com>
2014-10-15 15:26:59 -0700

    redid for session store for deploys that don't overwrite user sessions
    Former-commit-id: a37c7665adcd218dc11d857e64a1e5bfa3170b1c [formerly b66a7a03ecd77767932d46d084ac257862ffae92] [formerly eabcc48f1555db2c74a38285bb606240afbbe729 [formerly 88aa254ef0366a621b3f9867a4fc0818332a21d4]] [formerly d9054406715af519449207303f7915abf737a4f4 [formerly 95997e5b97c3df5006c9f444f61bf19cdea5ff8d] [formerly 14df9cee5acf1c0aefccceab0c621b3f8f87afc8 [formerly 14df9cee5acf1c0aefccceab0c621b3f8f87afc8 [formerly a21eef44105cb1d1bd8f688d01ddc14ec7c1fe35]]]]
    Former-commit-id: 9f132b329f4c450744ea3f53eb77daf6c25849d3 [formerly 37bc2a7daa5e326e897352e9c1d9b3d0f5b34748] [formerly 7ac5b9f15413977313f3ae883b7241fad1ff40e6 [formerly 9b5f9be55419395e0599925ce7741200e791d5d2]]
    Former-commit-id: 78e769ddc336849ab8ee3674f70cdc536f6aaa85 [formerly c30adbd04c19459fbe963c83d68377466b6d2bff]
    Former-commit-id: 2235963240dade4411287b794a8ec38888171bea
    Former-commit-id: 4e3a4d9bbf274ddbd4b74b614475445b7a992eb8

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,15 @@
 # Be sure to restart your server when you modify this file.
 
+
+# Use a redis cache store for sessions, a different store than our other memcached data.
+# This allows us to maintain user sessions across code deployments.
+ConsiderIt::Application.config.session_store :redis_store, :expire_after => 1.month
+
+
 #ConsiderIt::Application.config.session_store :cookie_store, :key => '_ConsiderIt_session'
 #ConsiderIt::Application.config.session_store :mem_cache_store, 'localhost', '127.0.0.1:11211', {:namespace => 'considerit'}
 
-ConsiderIt::Application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 1.day
-
+#ConsiderIt::Application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 1.day
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information

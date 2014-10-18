d9ef1ebc5e7f7ad5c72f73cd0f889ca3934d85e2
Travis Kriplean <tkriplean@gmail.com>
2014-10-17 18:52:51 -0700

    .
    Former-commit-id: fe1fb0810e0b05db4f71f7b9dd00c149494021f2 [formerly 05e7ec109a7207b6d13938ea35772b9655505814] [formerly eb158f516e7bc18b10dbcfda0306e31712401592 [formerly bf4e8c6f28c91aeb7247ca1f8a4aa5332d4c168f]] [formerly 79341d77fa00cb1c2331eeb664f4c368d9dda63a [formerly bb4d84048affb9e5369cf9666cda8c39fe1f429d] [formerly 05fcfa81fd57b5c3183073fdf8285808608549af [formerly 05fcfa81fd57b5c3183073fdf8285808608549af [formerly 66e0d8b8b0653754ad361de7533a8df5f2d57420]]]]
    Former-commit-id: 48de80f735d4a7e412f2e9949ace3485a443b9c0 [formerly 72b5008d41ed7c0ef5cbf7ef6f814ec9040f502e] [formerly 5fd2576040c10b3698cc3aa83ef027623f34539e [formerly c74ba5502e31cd3e7c171085bc5f8a22e11a2709]]
    Former-commit-id: d261696eef70e3739741c16e709b50e8864e8e33 [formerly eb871afa2b755cd99ba9c9bef540bf6d5ba47f82]
    Former-commit-id: 9cd77cc24e16407e4b8df48e2771bac7d1fe0a2f
    Former-commit-id: 2c471fcdbc9f384b8aab6d02ec1e063d9efbefc4

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -55,21 +55,21 @@ OAUTH_SETUP_PROC = lambda do |env|
   #   - https://github.com/intridea/omniauth-oauth2/pull/18/files; https://github.com/zquestz/omniauth-google-oauth2/issues/31#issuecomment-8922362
   #   - https://github.com/intridea/omniauth-oauth2/issues/32
 
-  if env['omniauth.strategy'].name() == 'google_oauth2' && Rails.env.production? && subdomain
-    env['omniauth.strategy'].options[:state] = subdomain
-    env['omniauth.strategy'].options[:redirect_uri] = "#{request.scheme}://googleoauth.#{host}/auth/google_oauth2/callback"
-    env['omniauth.strategy'].options['state'] = subdomain
-    env['omniauth.strategy'].options['redirect_uri'] = "#{request.scheme}://googleoauth.#{host}/auth/google_oauth2/callback"
-    env['omniauth.strategy'].options['client_options'][:state] = subdomain
-    env['omniauth.strategy'].options['client_options'][:redirect_uri] = "#{request.scheme}://googleoauth.#{host}/auth/google_oauth2/callback"
-    env['omniauth.strategy'].options['client_options']['state'] = subdomain
-    env['omniauth.strategy'].options['client_options']['redirect_uri'] = "#{request.scheme}://googleoauth.#{host}/auth/google_oauth2/callback"
-    env['omniauth.strategy'].options[:client_options][:state] = subdomain
-    env['omniauth.strategy'].options[:client_options][:redirect_uri] = "#{request.scheme}://googleoauth.#{host}/auth/google_oauth2/callback"
-    env['omniauth.strategy'].options[:client_options]['state'] = subdomain
-    env['omniauth.strategy'].options[:client_options]['redirect_uri'] = "#{request.scheme}://googleoauth.#{host}/auth/google_oauth2/callback"
+  # if env['omniauth.strategy'].name() == 'google_oauth2' && Rails.env.production? && subdomain
+  #   env['omniauth.strategy'].options[:state] = subdomain
+  #   env['omniauth.strategy'].options[:redirect_uri] = "#{request.scheme}://googleoauth.#{host}/auth/google_oauth2/callback"
+  #   env['omniauth.strategy'].options['state'] = subdomain
+  #   env['omniauth.strategy'].options['redirect_uri'] = "#{request.scheme}://googleoauth.#{host}/auth/google_oauth2/callback"
+  #   env['omniauth.strategy'].options['client_options'][:state] = subdomain
+  #   env['omniauth.strategy'].options['client_options'][:redirect_uri] = "#{request.scheme}://googleoauth.#{host}/auth/google_oauth2/callback"
+  #   env['omniauth.strategy'].options['client_options']['state'] = subdomain
+  #   env['omniauth.strategy'].options['client_options']['redirect_uri'] = "#{request.scheme}://googleoauth.#{host}/auth/google_oauth2/callback"
+  #   env['omniauth.strategy'].options[:client_options][:state] = subdomain
+  #   env['omniauth.strategy'].options[:client_options][:redirect_uri] = "#{request.scheme}://googleoauth.#{host}/auth/google_oauth2/callback"
+  #   env['omniauth.strategy'].options[:client_options]['state'] = subdomain
+  #   env['omniauth.strategy'].options[:client_options]['redirect_uri'] = "#{request.scheme}://googleoauth.#{host}/auth/google_oauth2/callback"
 
-  end
+  # end
 end
 
 

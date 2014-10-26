4b7846458e32c8872a0ac2ed3ad4275d5798bd58
Travis Kriplean <tkriplean@gmail.com>
2014-10-25 21:45:17 -0700

    csrf cleanup
    Former-commit-id: 36b29a25443ab0f17c566e7310b0e025d80122ec [formerly 6c350f6b1c9892aaa34cbcc753b124821f4fdeae] [formerly 0ca3c65f3ef4a5c3cb6260033ecc2897ebc2c450 [formerly e82e05b416aaad7f25f41f84a579379f8b11fd51]] [formerly 268d9155ada2436aa8188965378ef085f231d670 [formerly 061586064c38a502f7581423d6658cb87b8d552a] [formerly 8b5b8a6775607f6fe0b01253e825ee8a12bd7664 [formerly 8b5b8a6775607f6fe0b01253e825ee8a12bd7664 [formerly c78054cc4df234c9ce4698bcd0de422448234965]]]]
    Former-commit-id: 29be9f964465bc4b44a63f17be42197616a4b12c [formerly 67fe3ea51cd17b938e84470d0929d5f228f0ea85] [formerly 24d57f37b2c7b7d3c196bc7f20326c6c2eb2fbab [formerly 24c38dff6155e9e5c37d306cc37df6b7a99b4f27]]
    Former-commit-id: 4cf2428e66c28e1e639a58559d75f6207d5243a6 [formerly 5e1c897d3132cd30776b881896941749b95ce43e]
    Former-commit-id: c84e8963b17ef613b3f56a1b0462f8e07ebf9956
    Former-commit-id: ad61adbd3346d6e0253d0ee2acc5c0bc8106a376

--- @server/controllers/opinion_controller.rb
+++ @server/controllers/opinion_controller.rb
@@ -1,7 +1,5 @@
 class OpinionController < ApplicationController
 
-  protect_from_forgery with: :exception
-
   respond_to :json
 
   def show
--- @server/controllers/point_controller.rb
+++ @server/controllers/point_controller.rb
@@ -1,5 +1,4 @@
 class PointController < ApplicationController
-  protect_from_forgery with: :exception
   respond_to :json
 
   def show

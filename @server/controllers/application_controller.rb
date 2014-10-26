8b5ebe62a67bfc97a381ffe7a7b37da73f3a2fe8
Travis Kriplean <tkriplean@gmail.com>
2014-10-25 21:45:17 -0700

    csrf cleanup

--- @server/controllers/application_controller.rb
+++ @server/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 require 'digest/md5'
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token, if: :csrf_skippable?
 
   set_current_tenant_through_filter
--- @server/controllers/proposal_controller.rb
+++ @server/controllers/proposal_controller.rb
@@ -1,7 +1,5 @@
 class ProposalController < ApplicationController
 
-  protect_from_forgery with: :exception
-
   respond_to :json
 
   def index

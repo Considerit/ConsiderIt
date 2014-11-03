7509f1aac6294016585f14bdfe0ac79049b3c308
Travis Kriplean <tkriplean@gmail.com>
2014-11-02 22:14:14 -0800

    more cleanup; handle avatars in more activeresty way
    Former-commit-id: b51dadbf5bb016be23f807182b1e30016c688533 [formerly 7b024b2413184698a52fb6da66ecbf3d0a1b623b] [formerly 4cb93c141ea477a92464dbf86ea5e481a99d1fe5 [formerly 55c26e64d50c43afdd96eb5b3f0c562b8d036a8a]] [formerly 07d3b53d61415220f1ae7e2b1badb1e19c99606a [formerly 77f9740615b9844acac9d98241e96684a48fbbdc] [formerly c18b0a2b31436c46b4552bd934f740129b2b693a [formerly c18b0a2b31436c46b4552bd934f740129b2b693a [formerly 7827c71892bdcc2c18e46a8ecc3703a723084e41]]]]
    Former-commit-id: ba700367949b1c0fd8e0ad4f206a5edf3edba304 [formerly 3ce16ec951bb22e31f0ba2ca45298d4647b86219] [formerly c288569555daab0cacf083c25d8bb5c1c1421bd3 [formerly 9f540d1f6b46087c26061a3c65498c64840f1567]]
    Former-commit-id: ef4ba397f8a4d2f5e08bda6bb60f9d43cb047a30 [formerly b37f06421f7d059ae861fd99a81a395dfd70f406]
    Former-commit-id: 53ae871220aac42a221d1dba5b8fbabb4b1b88bf
    Former-commit-id: 95c6e297259c5910ea839950c9ba0b8ab580fafe

--- config/routes.rb
+++ config/routes.rb
@@ -59,7 +59,7 @@ ConsiderIt::Application.routes.draw do
   get 'current_user' => 'current_user#show'
   match 'current_user' => 'current_user#update', :via => [:put]
 
-  get '/avatars' => "home#avatars", :as => :get_avatars
+  get '/avatars' => "home#avatars"
 
   # This is for the special /opinion/current_user/234:
   match 'opinion/:id/:proposal_id' => 'opinion#show', :via => [:get, :put]

2cefeab4565b715d888425d01e4d81fa2ec3793a
Travis Kriplean <tkriplean@gmail.com>
2014-11-14 15:55:50 -0800

    .
    Former-commit-id: ebcdf2eb08acb420871e76c2770067d582e31adc [formerly d96cd5d6dbbb6fede99e01c6bf3c9288fafb0ec9] [formerly 417f3a1d24d1e48dd8a128ff70cb1dcee133e450 [formerly 294cf8df944f8fd27fe85a257413c19c92e1e6f9]] [formerly 7e37d51df0047dfa22af0185684f730430b8b13c [formerly 760e803f68710bad1499448a7e8cfcf438862381] [formerly 79897a5533098acee33c0850a717d2baafa4c6e0 [formerly 79897a5533098acee33c0850a717d2baafa4c6e0 [formerly 7ca7ccde6ced65b7873f5aeca0354a6d8f6148a4]]]]
    Former-commit-id: d7f1ec464d63e8e60e3daadb7b8a1ab7b4c81f60 [formerly e24581a6ae6142a4205ca7e3ac50e31722808814] [formerly 8594ddaa41b628f602ef6d9e673fc3c953620f7c [formerly 8a1fb6444e6949e77f9af9b0d4ad332c9ff459a6]]
    Former-commit-id: 3067ac418a24842e1a8a5e7913a484378c123041 [formerly 35dcecef23aa2d888a97497a01b0a7bd48802353]
    Former-commit-id: ba396e71f4a0d05f140f36a44365fca2d648ffa2
    Former-commit-id: 9b467a687e92fc18165986e021c69978e4937aca

--- @server/models/proposal.rb
+++ @server/models/proposal.rb
@@ -47,7 +47,7 @@ class Proposal < ActiveRecord::Base
         # If the user has a zipcode, we'll want to include all the jurisdictions 
         # associated with that zipcode. We'll also want to insert them between the statewide
         # measures and the advisory votes, since we hate the advisory votes. 
-        local_jurisdictions = ActiveRecord::Base.connection.select( "SELECT distinct(cluster) FROM proposals WHERE account_id=#{current_tenant.id} AND active=1 AND hide_on_homepage=1 AND zips like '%#{user_tags['zip']}%' ").map {|r| r['cluster']}
+        local_jurisdictions = ActiveRecord::Base.connection.select( "SELECT distinct(cluster) FROM proposals WHERE account_id=#{current_tenant.id} AND hide_on_homepage=1 AND zips like '%#{user_tags['zip']}%' ").map {|r| r['cluster']}
       end
       manual_clusters = ['Statewide measures', local_jurisdictions, 'Advisory votes'].flatten
       proposals = current_tenant.proposals.open_to_public.where("YEAR(created_at)=#{year}").where('cluster IN (?)', manual_clusters)

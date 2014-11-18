6ee04acaef7592ecbb5e0ad5dc20db5ae5a6fb32
Travis Kriplean <tkriplean@gmail.com>
2014-11-17 22:40:56 -0800

    ability to configure subdomain branding
    Former-commit-id: e43dea923fd2ef48accd00c7d041579b0eb82c2d [formerly 4666d72b125946197575e513b40c08d328c77614] [formerly 13192ea86f2758b0b7283f61f3e457ee408365d4 [formerly 3bb0100b9a5a863a348f99145b94e20da1f268a6]] [formerly 5d96dd537e0aff89c1f7f113823ce608f88216ee [formerly bf6122728b0d0be4fb7e6e382692ef5bb1f61538] [formerly 29711c25cd601ae6a64fdb8ca122fc186df45a2d [formerly 29711c25cd601ae6a64fdb8ca122fc186df45a2d [formerly d631d5c727cbf093fa6950c92b71bdc956d4f4a7]]]]
    Former-commit-id: a230a6c7c20b391742ecf24cfab8fb1876392480 [formerly 72cf0fa74f27f3e511d73512ed9e6e9d85bed44e] [formerly ff1c31b33be33fc4d01032048616ea44166f09b3 [formerly 315c24ea204acc544adb243f7814f2f31a7a286e]]
    Former-commit-id: 573eb7a239b894d63e7dc4a78d3cad22e3760dc4 [formerly 3376fc79d8114704d02c6b5083f789133055bae7]
    Former-commit-id: 27826eb998bf1bc08033f927e13b04ad38458698
    Former-commit-id: 984d33afeedc48bfe46ca81e18109577294730c7

--- config/application.rb
+++ config/application.rb
@@ -89,8 +89,8 @@ module ConsiderIt
     # Enable FS storage for Paperclip
     Paperclip::Attachment.default_options.merge!({
       :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
-      :url => "/system/:attachment/:id/:style/:filename",   
-      :default_url => "#{ENV['RAILS_RELATIVE_URL_ROOT'] || ''}/system/default_avatar/:style_default-profile-pic.png",
+      :url => "/system/:attachment/:id/:style/:filename"   
+      #:default_url => "#{ENV['RAILS_RELATIVE_URL_ROOT'] || ''}/system/default_avatar/:style_default-profile-pic.png",
     })    
   end
 end

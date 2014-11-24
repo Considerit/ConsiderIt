fac7cf8dd0d3825ffda02f44ba0befaec320fc74
Travis Kriplean <tkriplean@gmail.com>
2014-11-24 12:03:56 -0800

    renaming home_controller to html_controller; moving avatars url to user_controller
    Former-commit-id: e56bf92a3870e4c92a1590176f1871957c39b487 [formerly bcc57a5a1a3cbf0d9e63d484671605cf8764f0e0] [formerly 7da1fe717b7a2450b16df2c6dea9ab22859de4b8 [formerly e1c09737d8f1238f296d8c3dc0046b429dfb50d2]] [formerly eecb7c621f70633fe980bd4d14001fc36eca01a8 [formerly 8f2460c070c46b8fb99138c0ea402dd17c6ba7fe] [formerly f9d2ad7657ea4b0d4ef2f78024698a2f7309a33e [formerly f9d2ad7657ea4b0d4ef2f78024698a2f7309a33e [formerly 5251cb1eb0b2892f3727c4a558e87bcac9088eb5]]]]
    Former-commit-id: 118e0b20cabfbb5cb9bb93548d2f49f0373a97f1 [formerly 2125b304896e65768c92a8aa892db64fbfd7e4c3] [formerly 46764ccf838c54efc5b2ad20b67776d1205f9997 [formerly 4ddcf6c87bf0dce8eedbb229c155362d25049ffa]]
    Former-commit-id: 9b269e86df6953eeed6420f8357111556ad26c7a [formerly fbc51de4a179960740f55cd8173e09a0124b5620]
    Former-commit-id: 98bdb6d467c85d518bd3a93ee774d6bd68783854
    Former-commit-id: abe931df7dfdf9219db4b158e67a1ab28737fde8

--- @server/controllers/user_controller.rb
+++ @server/controllers/user_controller.rb
@@ -1,6 +1,10 @@
 class UserController < ApplicationController
   respond_to :json
 
+  caches_action :avatars, :cache_path => proc {|c|
+    {:tag => "avatars-#{current_subdomain.id}-#{Rails.cache.read("avatar-digest-#{current_subdomain.id}")}-#{session[:search_bot]}"}
+  }
+
   def index
     dirty_key "/users"
     render :json => []
@@ -20,4 +24,19 @@ class UserController < ApplicationController
     dirty_key "/user/#{params[:id]}"
     render :json => []
   end
+
+  def avatars
+    # don't fetch avatars for search bots
+    respond_to do |format|
+      @user = User
+      avatars = session[:search_bot] ? '' : render_to_string(:partial => 'user/avatars') 
+      format.json { 
+        render :json => {
+          key: '/avatars',
+          avatars: avatars
+        }
+      }
+    end
+  end
+
 end

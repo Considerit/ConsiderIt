e8a6ffb9e8ee44b7f6a2fcafbe59cabaa86892f4
Travis Kriplean <tkriplean@gmail.com>
2014-12-02 13:50:00 -0800

    start to invitation email, committing to wrong branch...
    Former-commit-id: adf75a44ccf8c980a8bd5b5d8228054406be5bb7 [formerly d48feefd6854a13433fa83ee077d4f3cb6da7677] [formerly 71ba1dfdf43e255705bda4ff13fc850fcd8fd571 [formerly ab7664ee64220c56b8f6811487d119454eb7c44a]] [formerly 94b653b98eb3a329c502641414457ff78f279a2d [formerly 8d9b14972462d46654a2d12c863ccb358c2b1abb] [formerly d677eaae92425fc4ef40087088a88595321befbf [formerly d677eaae92425fc4ef40087088a88595321befbf [formerly b0116c2817f714a942bd0a07dbb82b3d6946cea9]]]]
    Former-commit-id: 3307fad84fbe4c902a3406db1930a3a2fb5b5f47 [formerly a548efd1472448419fc221e79aff1e0a4dda2355] [formerly f6e0e5f7abada0fa26132b797bfb691045412c93 [formerly 00c31e58ae23a3fe0976fc6d61dd80236a295f9e]]
    Former-commit-id: fc72f0e1fd15bcb9ee0f48a0c56975605b19d1cf [formerly a97f9a5bfb609934441c459bdaa5445602b1c694]
    Former-commit-id: b78abe1b87003afccee0cb69943109d801d56d69
    Former-commit-id: aa471d2cd8a082362f02c8289a003446347ed99a

--- @server/mailers/user_mailer.rb
+++ @server/mailers/user_mailer.rb
@@ -16,5 +16,26 @@ class UserMailer < Mailer
     mail(:from => from, :to => to, :subject => "[#{app_title}] #{subject}")
   end
 
+  def invitation(inviter, invitee, invitation_obj, action, current_subdomain)
+    @user = invitee
+    @subdomain = current_subdomain
+    @invitation_obj = invitation_obj
+    @action = action
+
+    to = format_email user.email, user.name    
+    from = format_email(inviter, inviter.name)
+
+    case invitation_obj.class
+
+    when Subdomain
+      subject = "#{inviter.name} invites you to #{action} at #{invitation_obj.app_title}"
+    when Proposal
+      subject = "#{inviter.name} invites you to #{action} at '#{invitation_obj.name}'"
+    end
+
+    mail(:from => from, :to => to, :subject => "#{subject}")
+
+  end
+
 end
 

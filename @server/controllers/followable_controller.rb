bf3e03be37d91fb638675c387fb7a419ee712c56
Travis Kriplean <tkriplean@gmail.com>
2014-12-03 10:49:57 -0800

    renaming followable controller to notifications

--- @server/controllers/followable_controller.rb
+++
@@ -1,6 +0,0 @@
-class FollowableController < ApplicationController
-  def index
-    follows = current_user.notifications
-    render :json => [{:key => '/dashboard/email_notifications', :follows => follows}]
-  end  
-end

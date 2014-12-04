1a80ea36f593153bb8bdb229b54c87851ade0c4d
Travis Kriplean <tkriplean@gmail.com>
2014-12-03 17:51:10 -0800

    moderation option for new proposals
    Former-commit-id: b67da191545681ab13539439e51e19546d1c5c8f [formerly d8ea9f4d4247c43fe183e34f5c74087968df5a56] [formerly 69b5d5d6b4e8816cd2fdb2ecb3f7b24f812de93b [formerly d2ceac67a3f57285d6a2b5c3c776ccdb983d5db3]] [formerly 36cba9c956b0a51430f9b849437e8f7e3d2c1a5b [formerly 715f0fc02837124fb6070e804fe77d446d282ee9] [formerly 18d3cb11dd19f7ebd6d3eb309ea7276f64a5385e [formerly 18d3cb11dd19f7ebd6d3eb309ea7276f64a5385e [formerly bd1060374bf52849f5e5df02cf4adc226ae541e3]]]]
    Former-commit-id: 6f2bb6dfac5011708d927d794df3ce94b5df28cd [formerly 95fad04603a01d7ed09a7faa347041aebb109191] [formerly 0896426d1bf59d9892e32b333321495cc72b3af0 [formerly 704c83242fdf09d1e6e98357ff3442b290bef483]]
    Former-commit-id: e032443fe12b34aeb2d8b2960becf0f19df0fbec [formerly 85fa8d3f34aeddbf471174e8a62313d622712ec1]
    Former-commit-id: 124211338ff653b3f38b35e2795e5d9e43e5d615
    Former-commit-id: 5f4497d9c0facd7611ac1d4d719c81442d8625b0

--- @server/models/concerns/moderatable.rb
+++ @server/models/concerns/moderatable.rb
@@ -2,14 +2,7 @@ module Moderatable
   extend ActiveSupport::Concern
 
   included do 
-    has_one :moderation, :as => :moderatable, :class_name => 'Moderation', :dependent => :destroy
-    
-    class_attribute :moderatable_fields
-    class_attribute :moderatable_objects
-  end
-
-  def moderatable?
-    true
+    has_one :moderation, :as => :moderatable, :class_name => 'Moderation', :dependent => :destroy    
   end
 
 end

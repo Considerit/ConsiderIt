bc8f8c5d423946389c7e117c48667c2404a6d7e9
Travis Kriplean <tkriplean@gmail.com>
2014-12-09 19:16:40 -0800

    misc bug fixes and refactoring

--- @server/controllers/notification_controller.rb
+++
@@ -1,5 +0,0 @@
-class NotificationController < ApplicationController
-  def index
-    render :json => [{:key => '/dashboard/email_notifications', :follows => current_user.notifications}]
-  end  
-end

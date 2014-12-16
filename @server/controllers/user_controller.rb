df3528c7b19a6c9a1ce576569d9059c43d0caf7e
Travis Kriplean <tkriplean@gmail.com>
2014-12-15 18:30:51 -0800

    removing action caching
    Former-commit-id: 0f7d5a5d06e55400ac721fd98e6fecd6aaf4e7aa [formerly 80d6d14433304ae9927b8a4b94fe93cba00031d5] [formerly adff522c8f8dc3c6a441053cd587ae0c1719fb92 [formerly 39a8b4bc16717ccedda87d4302703a019ad84c13]] [formerly ff20ad09b2d52741fa3d82809be5de39f4c7c1f5 [formerly 8b0fb198700c13ef8d22f8ba0a9493a3ce7cdc97] [formerly c96abb04199c388fef5c86a867fc817aeb67e2e2 [formerly c96abb04199c388fef5c86a867fc817aeb67e2e2 [formerly 9c0b92afa1d58ae9acf26c08797f7dea5641f95d]]]]
    Former-commit-id: 13db18d86d4d393bcdbba509fc86f1734dd3e7b5 [formerly 562c107d22ec2e189e664da85db72da1430f3043] [formerly 85f5f0cec8987d2272ed90202872ad9569d1cddc [formerly fc6d4f623155ba57a3bf39a58e1887a8bb0524f6]]
    Former-commit-id: e513609a9853d912da1978068813ac3e2997b296 [formerly 886d5e58804504f2a7167b80e90b7967383aafd2]
    Former-commit-id: c901d18844e1f3b84071d20252dc33dfe4a88ebf
    Former-commit-id: 335732d9c21aadf77cc23fc2ed213587ba82e06b

--- @server/controllers/user_controller.rb
+++ @server/controllers/user_controller.rb
@@ -1,11 +1,6 @@
 class UserController < ApplicationController
   respond_to :json
 
-  caches_action :avatars, :cache_path => proc {|c|
-    current_cache_key = Rails.cache.read("avatar-digest-#{current_subdomain.id}") || 0
-    {:tag => "avatars-#{current_subdomain.id}-#{current_cache_key}-#{session[:search_bot]}"}
-  }
-
   def index
     dirty_key "/users"
     render :json => []
@@ -27,19 +22,22 @@ class UserController < ApplicationController
   end
 
   def avatars
-    if Rails.cache.read("avatar-digest-#{current_subdomain.id}").nil?
-      Rails.cache.write("avatar-digest-#{current_subdomain.id}", 0)
-    end
     
-    # don't fetch avatars for search bots
     respond_to do |format|
       @user = User
 
-      avatars = ''
-      if !session[:search_bot]
-        users = @user.where("registered=1 AND b64_thumbnail IS NOT NULL AND INSTR(active_in, '\"#{current_subdomain.id}\"')")
-        avatars = users.select([:id,:b64_thumbnail]).map {|user| "#avatar-#{user.id} { background-image: url('#{user.b64_thumbnail}');}"}.join(' ')
+      if !session[:search_bot]        
+        cache_key = "avatar-digest-#{current_subdomain.id}"
+        avatars = Rails.cache.read(cache_key)
+        if avatars.nil?
+          users = @user.where("registered=1 AND b64_thumbnail IS NOT NULL AND INSTR(active_in, '\"#{current_subdomain.id}\"')")
+          avatars = users.select([:id,:b64_thumbnail]).map {|user| "#avatar-#{user.id} { background-image: url('#{user.b64_thumbnail}');}"}.join(' ')
+          Rails.cache.write(cache_key, avatars)
+        end
+      else
+        avatars = ''
       end
+
       format.json { 
         render :json => {
           key: '/avatars',

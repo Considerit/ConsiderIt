a9c57d588652169217d8da3f6e9523430e6a00a1
Travis Kriplean <tkriplean@gmail.com>
2015-01-04 10:30:20 -0800

    fixing subject lines for invitation emails
    Former-commit-id: add51570da0bc2f6730ed3600848deefb77887f4 [formerly 46c82973ae56e1a3f77e04ca7a4bd7f77c616857] [formerly ff2ba702c9ad8530d2b6344429efaeaf0d426a83 [formerly 697024ac03e65c0660ec9c386b2070d18a3ebf84]] [formerly 0a691c9c739be570eb84ce5818124abcf1a995c8 [formerly e05ff2b3f20d926a503f83b5faf0cda7bb481d78] [formerly a501f8903c94889170429f2832fa31323f0b0863 [formerly a501f8903c94889170429f2832fa31323f0b0863 [formerly 33b7e125fa0d0b3a2d5c1d68ccd6fa2bf65ede97]]]]
    Former-commit-id: 51d34a4cb6f228ac04b4e20313e9461205646991 [formerly 2e6a40ac22c98e49862aba0478e8e77f1b696dcb] [formerly dab94829b3ba8e7242a30d655583b4b7000c0f96 [formerly ad444d3268452766737e1796b37bf3a36a575fb5]]
    Former-commit-id: 6720ee543ab1ae923f7e5edefc24f81c425ab301 [formerly 7f3500d0dd59fd61fcfff027c2b2f9d4c165f579]
    Former-commit-id: 007a454fd404799a58be85bc64264c53c3e5140b
    Former-commit-id: f95091715b4d3652da8ccdb5b6f2b0bead976807

--- @server/emails/mailers/user_mailer.rb
+++ @server/emails/mailers/user_mailer.rb
@@ -25,13 +25,37 @@ class UserMailer < Mailer
     mail(:from => from, :to => to, :subject => "[#{current_subdomain.app_title}] #{subject}")
   end
 
-  def invitation(inviter, invitee, invitation_obj, action, current_subdomain, message = nil)
+  def invitation(inviter, invitee, invitation_obj, role, current_subdomain, message = nil)
     @user = invitee
     @inviter = inviter
     @subdomain = current_subdomain
     @invitation_obj = invitation_obj
-    @action = action
     @message = message
+
+    case role
+    when 'writer'
+      @action = 'write new points at'
+    when 'commenter'
+      @action = 'comment on points at'
+    when 'opiner'
+      @action = 'give your opinion at'
+    when 'proposer'
+      @action = 'add new proposals at'
+    when 'admin'
+      @action = 'administer'
+    when 'evaluator'
+      @action = 'fact check'
+    when 'editor'
+      @action = 'edit'
+    when 'visitor'
+      @action = 'visit'
+    else 
+      if role[-1] == 'r'
+        @action = "#{role[0..-3]}e"
+      else
+        @action = role
+      end
+    end
 
     if invitee.first_name
       to = format_email invitee.email, invitee.name    
@@ -42,9 +66,9 @@ class UserMailer < Mailer
     case invitation_obj.class.to_s
 
     when 'Subdomain'
-      subject = "#{inviter.name} invites you to #{action} at #{invitation_obj.app_title}"
+      subject = "#{inviter.name} invites you to #{@action} #{invitation_obj.app_title}"
     when 'Proposal'
-      subject = "#{inviter.name} invites you to #{action} at '#{invitation_obj.name}'"
+      subject = "#{inviter.name} invites you to #{@action} '#{invitation_obj.name}'"
     else
       raise "Why are you trying to send an invitation to a #{invitation_obj.class.to_s}?"
     end

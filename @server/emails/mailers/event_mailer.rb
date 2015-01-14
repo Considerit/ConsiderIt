437bc0babde1230f2c22b3c54652f0550d8876b3
Travis Kriplean <tkriplean@gmail.com>
2015-01-14 12:29:10 -0800

    default app_title to subdomain name
    Former-commit-id: 1a43206932a8db2f608ecc3441ee36f807397a9f [formerly 4445f3225493124031c106ba7f411bac4d61d6ea] [formerly d3af0babb626a18dfa6cad0c832728bd9cecb0d9 [formerly 0597594b344069ae39c92234b79464f4cce86ae1]] [formerly f94d3666247734a6c0935ba80d606699bcdde623 [formerly 63f719a93805ee0025081dc3fdd942d31427096e] [formerly 179674031eeddf603521b6799f4c5ed2239ce424 [formerly 179674031eeddf603521b6799f4c5ed2239ce424 [formerly 167e6e1131873c492f6096ebfc5a1e7d455a03ad]]]]
    Former-commit-id: 7cbd794ccc37d83ec9d9111b3d580048cdcb5f56 [formerly 0d114f02ce53e423fe5b41011cc43c56817c5bd2] [formerly 62b5866425f19a7c3dad1323b12f740f976fbbfd [formerly fdc0118b2e6f47cb3370713e13311d3c49f08dd0]]
    Former-commit-id: 3eea499655a5468eb0d9c27ccb1d4b014d8a2740 [formerly ecac36ffe9b62cfe4c8277a6b212fee64dfbed81]
    Former-commit-id: 55643223e51e22b3a48b9d2bb16ecfc3e76d39a9
    Former-commit-id: 86aa026ab7cfc95cbebfb5c6510034c12878327e

--- @server/emails/mailers/event_mailer.rb
+++ @server/emails/mailers/event_mailer.rb
@@ -3,33 +3,30 @@ require 'mail'
 class EventMailer < Mailer
 
 
-  def send_message(message, current_user, current_subdomain)
+  def send_message(message, current_user, subdomain)
     @message = message
-    @subdomain = current_subdomain
+    @subdomain = subdomain
+
+    recipient = message.addressedTo()
+
+    to = format_email recipient.email, recipient.name
 
     # from e.g. Moderator <hank@cityclub.org>
     from = format_email current_user.email, message.sender
 
-    recipient = message.addressedTo()
-
-    # reply_to = format_email @message.sender, @message.senderName()
-    to = format_email recipient.email, recipient.name
-
-    subject = "[#{current_subdomain.app_title}] #{@message.subject}"
-
-    mail(:from => from, :to => to, :subject => subject, :bcc => from)
+    mail(:from => from, :to => to, :subject => subject_line(@message.subject, subdomain), :bcc => from)
 
   end
 
-  def new_point(user, pnt, current_subdomain, notification_type)
+  def new_point(user, pnt, subdomain, notification_type)
     @notification_type = notification_type
     @user = user
     @point = pnt
     @proposal = @point.proposal
-    @subdomain = current_subdomain
+    @subdomain = subdomain
 
     to = format_email user.email, user.name    
-    from = format_email(default_sender(current_subdomain), current_subdomain.app_title)
+    from = format_email(default_sender(subdomain), (subdomain.app_title or subdomain.name))
 
     if notification_type == 'your proposal'
       subject = "new #{@point.is_pro ? 'pro' : 'con'} point for your proposal \"#{@point.proposal.title}\""
@@ -37,20 +34,19 @@ class EventMailer < Mailer
       subject = "new #{@point.is_pro ? 'pro' : 'con'} point for \"#{@point.proposal.title}\""
     end
 
-    mail(:from => from, :to => to, :subject => "[#{current_subdomain.app_title}] #{subject}")
+    mail(:from => from, :to => to, :subject => subject_line(subject, subdomain))
   end
 
-  def new_comment(user, pnt, comment, current_subdomain, notification_type)
+  def new_comment(user, pnt, comment, subdomain, notification_type)
     @notification_type = notification_type
     @user = user
     @point = pnt
     @comment = comment
     @proposal = @point.proposal
-    @subdomain = current_subdomain
-
+    @subdomain = subdomain
 
     to = format_email user.email, user.name
-    from = format_email(default_sender(current_subdomain), current_subdomain.app_title)
+    from = format_email(default_sender(subdomain), (subdomain.app_title or subdomain.name))
 
     if notification_type == 'your point'
       subject = "new comment on a #{@point.is_pro ? 'pro' : 'con'} point you wrote"
@@ -62,18 +58,19 @@ class EventMailer < Mailer
       subject = "new comment on a #{@point.is_pro ? 'pro' : 'con'} point you follow"
     end
 
-    mail(:from => from, :to => to, :subject => "[#{current_subdomain.app_title}] #{subject}")
+    mail(:from => from, :to => to, :subject => subject_line(subject, subdomain))
   end
 
-  def new_assessment(user, pnt, assessment, current_subdomain, notification_type)
+  def new_assessment(user, pnt, assessment, subdomain, notification_type)
     @notification_type = notification_type
     @user = user
     @point = pnt
     @assessment = assessment
     @proposal = @point.proposal
-    @subdomain = current_subdomain
+    @subdomain = subdomain
 
-    from = format_email(default_sender(current_subdomain), current_subdomain.app_title)
+    to = format_email user.email, user.name
+    from = format_email(default_sender(subdomain), (subdomain.app_title or subdomain.name))
 
     if notification_type == 'your point'
       subject = "a point you wrote has been fact checked"
@@ -81,9 +78,7 @@ class EventMailer < Mailer
       subject = "a point you follow has been fact checked"
     end
 
-    to = format_email user.email, user.name
-
-    mail(:from => from, :to => to, :subject => "[#{current_subdomain.app_title}] #{subject}")
+    mail(:from => from, :to => to, :subject => subject_line(subject, subdomain))
   end
 
 

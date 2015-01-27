414d37e552eb55adcdd2389536f875f362522d19
Travis Kriplean <tkriplean@gmail.com>
2015-01-26 16:57:53 -0800

    .
    Former-commit-id: 3be0f876bc42216f74e85c8e541dc08a6068535a [formerly 6d3bb403b4052c791cc9673638c474074c46a8aa] [formerly b051430f9b32d95a9b605ba1d9f158c2c2a34594 [formerly 0206753a4d6414d7f09d7aff6d3fcfffa63eb1cb]] [formerly 3690b0f60d11ca397958c030eec1a86762bf8fc4 [formerly a85e5e581909c7785117a00eab97dccd0fcfe95c] [formerly 481a4bf7850a7a14708b58903c67888cd2776357 [formerly 481a4bf7850a7a14708b58903c67888cd2776357 [formerly 4f5fbe0bd56ce5dd4c8622465171788022885bd5]]]]
    Former-commit-id: eb00b397d57bb7bf06e473d5ab7c071b9046122a [formerly ee1367f95cfd0fda15c8eb62a031be036715ca18] [formerly 9d5054286a692a5d74324e4f2146afac2f9beb61 [formerly f49a50906cd1afbcc94fa122127f8718b34d5713]]
    Former-commit-id: 774b59981073df816dc50be99af68a16734a474d [formerly 5e35e13a8c181fad3180bdaa1c414767a8623bf1]
    Former-commit-id: 5c9fd93a61fd5389b038401b44ced2c61cf79db2
    Former-commit-id: c945b57ef7bfa1b14bc36bcd134c6bf14f838e13

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -1,3 +1,11 @@
+
+
+# Rails.backtrace_cleaner.add_silencer do |line|
+#   pp (line !~ line.gsub(Rails.root, '')), line, Rails.root
+#   (line !~ Rails::BacktraceCleaner::APP_DIRS_PATTERN)
+# end
+
+Rails.backtrace_cleaner.remove_silencers!
 Rails.backtrace_cleaner.add_filter { |line| line.gsub(Rails.root.to_s, '<root>') }
 Rails.backtrace_cleaner.add_silencer { |line| line.index('<root>').nil? and line.index('/') == 0 }
 Rails.backtrace_cleaner.add_silencer { |line| line.index('<root>/vendor/') == 0 }

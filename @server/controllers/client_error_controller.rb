96081a70c6b279468a489e75f8dff2acf3dbbd48
Michael Toomim <toomim@gmail.com>
2015-01-28 19:05:31 -0800

    Email us when there are client errors!
    Former-commit-id: a43c2e676e0804ce4db0b9e93ef7497653e98950 [formerly c73d72499eb867720399dd2b21b37170d0c5db03] [formerly 37e4e62a21fe4c681443e5c5a4a92f88a5a98ce5 [formerly aec3d916affffcbdbc79dd25c717356549125724]] [formerly d89777c8d97b8feb63dd86940dac15bea4ebdd9c [formerly 05d516e18cd3f80178a9ec40c792e923d29a4463] [formerly ba100ec3574ee9ed024e1172fe695d1348af4e84 [formerly ba100ec3574ee9ed024e1172fe695d1348af4e84 [formerly 18b1e0d58f9d43258beaa3abe449ecc5086dbefd]]]]
    Former-commit-id: 9e32a3f41321a3fe7bdbec555fe4dd15cbd6275d [formerly f9557bcb2cf0a7ec39371c2bc33f4ab2ddb10118] [formerly edce81d9a8920397e916e26d01f9e4fc281ffa71 [formerly 50855f768652781319a4ba27e333fc26afd47c67]]
    Former-commit-id: b8ae9f30c8e8189d69efbe127573c8ad5db54f54 [formerly bcc8ab14f1fd59d682c81cf46520679488db6e70]
    Former-commit-id: ee414e946e34a8424e88f23f2fe4956062015e40
    Former-commit-id: 48ac78300e07c63d304ea6c17e115b78c8df5b9a

--- @server/controllers/client_error_controller.rb
+++ @server/controllers/client_error_controller.rb
@@ -3,12 +3,19 @@ class ClientErrorController < ApplicationController
   
   def create
     ## Store this somewhere instead of a puts
-    ClientError.create trace: params[:stack],
-                       message: params[:message],
-                       line: params[:line_number],
-                       ip: request.env['REMOTE_ADDR'],
-                       user_agent: request.env["HTTP_USER_AGENT"],
-                       user_id: current_user && current_user.id
+    e = ClientError.create trace: params[:stack],
+                           message: params[:message],
+                           line: params[:line_number],
+                           ip: request.env['REMOTE_ADDR'],
+                           user_agent: request.env["HTTP_USER_AGENT"],
+                           user_id: current_user && current_user.id
+
+    begin
+      raise "ClientError: \"#{e.message}\" from #{e.ip} user #{e.user_id}"
+    rescue => err
+      ExceptionNotifier.notify_exception err, :env => request.env
+    end
+    
     render :json => ["Thanks for the error report!"]
   end
 end

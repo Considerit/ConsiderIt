d0a2dac750046809fe6b8b54952de724e1e4e3d8
Travis Kriplean <tkriplean@gmail.com>
2015-03-06 18:14:31 -0800

    adding support for SVG clip path

--- @client/assets/javascripts/vendor/react.js
+++ @client/assets/javascripts/vendor/react.js
@@ -7096,6 +7096,7 @@ var ReactDOM = mapObject({
   g: false,
   line: false,
   linearGradient: false,
+  clipPath: false,
   mask: false,
   path: false,
   pattern: false,
@@ -13734,6 +13735,7 @@ var MUST_USE_ATTRIBUTE = DOMProperty.injection.MUST_USE_ATTRIBUTE;
 
 var SVGDOMPropertyConfig = {
   Properties: {
+    clipPath: MUST_USE_ATTRIBUTE,
     cx: MUST_USE_ATTRIBUTE,
     cy: MUST_USE_ATTRIBUTE,
     d: MUST_USE_ATTRIBUTE,
@@ -13779,6 +13781,7 @@ var SVGDOMPropertyConfig = {
     y: MUST_USE_ATTRIBUTE
   },
   DOMAttributeNames: {
+    clipPath: 'clip-path',
     fillOpacity: 'fill-opacity',
     fontFamily: 'font-family',
     fontSize: 'font-size',

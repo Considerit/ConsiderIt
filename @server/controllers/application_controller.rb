9679c8649f111d10828a151b2973910fc86ddd32
Travis Kriplean <tkriplean@gmail.com>
2015-03-31 12:29:37 -0700

    homepage video prototype
    Former-commit-id: da78035bb19bc3f2df50f825a265eeb82d024ff6 [formerly 3a7fd1baa0ca9e644557023fcc0881e3002916bc] [formerly 250ad8cbbfb227f279d8753eb70749294685af3f [formerly 8f2cf8ad9ea33fb2506f86468590c11ac5f9ec2f]] [formerly b2dbc68f7353202e44c9b3b797edf0afcc28268e [formerly d357aa781a6e8a3e98686782209691659079984e] [formerly 3f84d93ed22bbf38865d03f991e4d75b800c3e25 [formerly 3f84d93ed22bbf38865d03f991e4d75b800c3e25 [formerly eba2d5ab350f74357370616387334c3d33c02b6c]]]]
    Former-commit-id: 09a8bf3c1ef6c8c5dc11e6dcf5ad24de3cff3879 [formerly ed4f13df575e2b06d224e9e13a1a2a2bcddd242f] [formerly f16acf1a1ebfeb629595a4f3d41abf570df45ac7 [formerly 46f4c68c85d4388596fba664fd1a94d35fbfe6ba]]
    Former-commit-id: ed82065bc378edc569af203cc05b3d0f59424930 [formerly 6f623771bee12d6b26da035a5b6f2ef29b5d7fc1]
    Former-commit-id: 712969c2b7e5deae0bb8dbbc268ef0189008955e
    Former-commit-id: 0e2905e108b6ee1180e521d132f05ce278e09241

--- @server/controllers/application_controller.rb
+++ @server/controllers/application_controller.rb
@@ -5,7 +5,7 @@ require Rails.root.join('@server', 'permissions')
 
 
 # Set to true if you're working on the http://consider.it homepage. 
-ENABLE_HOMEPAGE_IN_DEV = false
+ENABLE_HOMEPAGE_IN_DEV = true
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token, if: :csrf_skippable?

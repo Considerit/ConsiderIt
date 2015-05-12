bdfb731918858ca40b7c60779028bedc4b5ac9ad
Travis Kriplean <tkriplean@gmail.com>
2015-05-11 23:27:07 -0700

    some digest formatting
    Former-commit-id: 8f53462fa5f5552d0ef3d5608eb8719c66ebe33b [formerly 162e761ce962e18a10634303ae8c25eccfb603c9] [formerly 45267b8e93e1f40034c2bf2b16c44ecc123a2f31 [formerly 724b3fbecbbb0f6cc6ba641c6be29df0db14b491]] [formerly 2ec262671009f584c40e03e7df7b01e05fba6011 [formerly 67703918a43b4fe2cf34ddce01017cfb89b61a6d] [formerly c66477d85e39ad5ef689cd21d80fe13a6c7eaa5f [formerly c66477d85e39ad5ef689cd21d80fe13a6c7eaa5f [formerly e7914bae667970ab3fe901ec79cc4c804fa9ec14]]]]
    Former-commit-id: a56945bee26894424de5917feeb566006b736470 [formerly b4939077fd198b109028ce50efefa36470ef99b3] [formerly 0c085de158d1e05d0649164409cfd2935d42ebe3 [formerly 9874997deee59923ed4e79dcd9c66d0b6b49cfc9]]
    Former-commit-id: 5d7af4c22c6072089765922a0f6fe4fee6991b0e [formerly 2b65a1da4e3f2f71f1de8a2e91f7d45b0dbf17bd]
    Former-commit-id: 4ffd791fa632c4bd25d802df6bb993f61ceaad32
    Former-commit-id: 202951c17f979919d5dfa75f1bfac8950729a295

--- @email/email_templates/helpers/mailer_helper.rb
+++ @email/email_templates/helpers/mailer_helper.rb
@@ -14,4 +14,64 @@ module MailerHelper
     
     "#{Rails.env.development? ? 'http://' : 'https://'}#{subdomain.host_with_port}/#{relative_path}?#{query_params}"    
   end
+
+
+  ### digests
+
+  def group_by_object(notifications)
+    notifications.each_with_object({}) { |n,h| 
+      case n.event_object_type
+      when 'Comment'
+        obj_id = n.event_object.point.id        
+      end
+      h[obj_id] ||= []
+      h[obj_id].push(n) 
+    }
+  end
+
+  def group_by_user(notifications)
+    notifications.each_with_object({}) { |n,h| 
+      case n.event_object_type
+      when 'Point'
+        obj_id = n.event_object.user_id        
+      end
+      h[obj_id] ||= []
+      h[obj_id].push(n) 
+    }
+  end
+
+  #
+  # Milly Unger, Phil Simms, Jorge R., and 45 others...
+  def get_user_str(notifications, max_users = 3)
+    users = notifications.map {|n| n.event_object.user}.uniq
+    over = users.length - max_users
+
+    str = ""
+    named = [max_users, users.length].min
+    users[0..named-1].each_with_index do |user, idx|
+      pp 'HOLA', "#{idx}, #{named - 1}"
+      if idx == named - 1 && over <= 0 && named > 1
+        str += ' and '
+      end
+      str += user.name
+      if idx < named - 1 && named != 2
+        str += ', '
+      end
+
+
+    end
+
+    if over > 0
+      str += " and #{over} other#{over > 1 ? 's' : ''}"
+    end
+
+    pp str
+    str.gsub('  ', ' ')
+  end
+
+  def logo_red
+    "#B03A44"
+  end
+
+
 end

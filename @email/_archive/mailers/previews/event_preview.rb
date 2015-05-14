ae31e28a00b1819b21b1c9c23d7784889f655e01
Travis Kriplean <tkriplean@gmail.com>
2015-05-13 17:49:20 -0700

    remove daily moderation checks; separate out unused parts of email system
    Former-commit-id: 52010ba0926420dc3126dae7627b0aedbb8d44d6 [formerly 26cf74a79c283192762c69b4b8a53af4c0d9eecc] [formerly 57c003de965993cbbd2706c54068e2049270e060 [formerly 5994ddf64af463bd6928ecf16465e87b74d3a382]] [formerly 931a3820f53ea78d0e00c267baf04432cfbf26d6 [formerly 3edb53760c3da67de79adafd260c64c7d6a84c13] [formerly bde0d9e08360cec25f043931088bbc90fcd94960 [formerly bde0d9e08360cec25f043931088bbc90fcd94960 [formerly de0c0266940002b3d0288c1e97743b4f9c24d91c]]]]
    Former-commit-id: c51f89b76ce006330adb3e9b2130d7fb7a60a70f [formerly ffddcfe3bf3f795d766ac3167cefd59a34584226] [formerly db2455ce58e20a980204a3ba3141a1c90bc16f4a [formerly 0ee8afe8dd75518c6160d0a20c1402c710cdeabe]]
    Former-commit-id: c457d17ca4a585df6c06fa2430c27e0f4073aecc [formerly a00e9ef2695d3b801175fb6564932f1df245ecf3]
    Former-commit-id: 5f32ef96e9528c81533fcc643f529bcb2f97d976
    Former-commit-id: 9f9e86c47f56ffe410185f5cc27b4a9c6208f631

---
+++ @email/_archive/mailers/previews/event_preview.rb
@@ -0,0 +1,33 @@
+class EventPreview < ActionMailer::Preview
+  def new_point
+    subdomain = Subdomain.first
+    point = subdomain.points.published.last
+    EventMailer.new_point(User.where('registered').last, point, subdomain, 'opinion submitter')
+  end
+
+  def new_assessment
+    assessment = Assessment.completed.last
+
+    EventMailer.new_assessment(assessment.point.user, assessment.point, assessment, assessment.subdomain, 'your point')
+  end
+
+  def new_comment
+    subdomain = Subdomain.first
+    comment = subdomain.comments.last
+    EventMailer.new_comment(comment.point.user, comment.point, comment, subdomain, 'your point')
+  end
+
+  def send_message
+    subdomain = Subdomain.first
+
+    attrs = {
+      'recipient' => "/user/1701",
+      'body' => "This is a test message",
+      'subject' => "You should see this",
+      'sender_mask' => 'moderator'
+    }
+
+    EventMailer.send_message(attrs, User.where('registered').last, subdomain)
+  end
+
+end

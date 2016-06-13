db8c686da3ccf0deae170419aef623e7e19744ea
Travis Kriplean <tkriplean@gmail.com>
2016-06-13 15:32:55 -0700

    new email digests

--- @email/send_digest.rb
+++ @email/send_digest.rb
@@ -1,7 +1,7 @@
-# wait at least 2 min before sending any notification
-BUFFER = 2 * 60 
+# wait at least 5 min before sending any notification
+BUFFER = 5 * 60 
 
-def send_digest(subdomain, user, notifications, subscription_settings, deliver = true)
+def send_digest(subdomain, user, notifications, subscription_settings, deliver = true, since = nil)
   
   send_emails = subscription_settings['send_emails']
   return if !send_emails
@@ -62,7 +62,19 @@ def send_digest(subdomain, user, notifications, subscription_settings, deliver =
 
   if do_send
 
-    mail = DigestMailer.digest(subdomain, user, notifications)
+    # Hack!! this notification system is terrible, so I'm going to add even more entropy.
+    # I'm going to get all the data across the subdomain since the last time a digest 
+    # was sent, and not rely on the notification objects. Sorry future Travis!
+
+    if !since 
+      since = last_digest_sent_at
+    end 
+
+    if !since
+      since = user.created_at
+    end
+
+    mail = DigestMailer.digest(subdomain, user, notifications, get_new_activity(subdomain, user, since), last_digest_sent_at, send_emails)
     if deliver
       mail.deliver_now
 
@@ -85,6 +97,166 @@ def send_digest(subdomain, user, notifications, subscription_settings, deliver =
   end
 
   mail 
+
+end
+
+def get_new_activity(subdomain, user, since)
+
+  new_proposals = {}
+  subdomain.proposals.where("created_at > '#{since}'").each do |proposal|
+    if proposal.opinions.published.where(:user_id => user.id).count == 0 
+      new_proposals[proposal.id] = proposal 
+    end 
+  end 
+
+  new_points = subdomain.points.published.named.where("created_at > '#{since}' AND user_id != #{user.id} AND last_inclusion != -1")
+  new_opinions = subdomain.opinions.published.where("created_at > '#{since}' AND user_id != #{user.id}")
+  new_comments = subdomain.comments.where("created_at > '#{since}' AND user_id != #{user.id}")
+
+
+
+  your_proposals = {}
+  active_proposals = {}
+  new_points.each do |pnt|
+    proposal = pnt.proposal
+    next if new_proposals.key?(proposal.id)
+
+    proposal_dict = proposal.user_id == user.id ? your_proposals : active_proposals
+    if !proposal_dict.has_key? proposal.id 
+      proposal_dict[proposal.id] = {
+        :obj => proposal,
+        :events => {},
+        :relationship => user.opinions.published.where(:proposal_id => proposal.id).count > 0 ? 'You expressed an opinion' : false 
+      }
+    end
+
+    key = "new_point_#{pnt.id}"
+    proposal_dict[proposal.id][:events][key] = {
+      :obj => pnt,
+      :type => 'new_point',
+      :users => [pnt.user]
+    }
+  end 
+
+  new_comments.each do |comment|
+    pnt = comment.point
+    proposal = pnt.proposal
+    next if new_proposals.key?(proposal.id)
+    proposal_dict = proposal.user_id == user.id ? your_proposals : active_proposals
+    if !proposal_dict.has_key? proposal.id 
+      proposal_dict[proposal.id] = {
+        :obj => proposal,
+        :events => {},
+        :relationship => user.opinions.published.where(:proposal_id => proposal.id).count > 0 ? 'You expressed an opinion' : false 
+      }
+    end
+
+    key = "new_comment_#{pnt.id}"
+    if !proposal_dict[proposal.id][:events].has_key? key 
+      if pnt.user_id == user.id
+        relationship = 'Your point'
+      elsif user.comments.where(:point_id => pnt.id).count > 0 
+        relationship = 'You commented on this point'
+      else 
+        relationship = false
+      end
+      proposal_dict[proposal.id][:events][key] = {
+        :obj => pnt,
+        :type => 'new_comment',
+        :users => [comment.user],
+        :relationship => relationship
+      }
+    else 
+      proposal_dict[proposal.id][:events][key][:users].append comment.user
+    end 
+
+  end 
+
+  new_opinions.each do |opinion|
+    proposal = opinion.proposal
+    next if new_proposals.key?(proposal.id)
+
+    proposal_dict = proposal.user_id == user.id ? your_proposals : active_proposals
+    if !proposal_dict.has_key? proposal.id 
+      proposal_dict[proposal.id] = {
+        :obj => proposal,
+        :events => {},
+        :relationship => user.opinions.published.where(:proposal_id => proposal.id).count > 0 ? 'You expressed an opinion' : false 
+      }
+    end
+
+    if !proposal_dict[proposal.id][:events].has_key? 'new_opinion' 
+      proposal_dict[proposal.id][:events]['new_opinion'] = {
+        :type => 'new_opinion',
+        :users => [opinion.user]
+      }
+    else 
+      proposal_dict[proposal.id][:events]['new_opinion'][:users].append opinion.user
+    end 
+  end 
+
+  user.points.published.where(:subdomain_id => subdomain.id).each do |pnt|
+    pnt.inclusions.where("created_at > '#{since}' AND user_id != #{user.id}").each do |inclusion|
+      pnt = inclusion.point
+      proposal = pnt.proposal
+      next if new_proposals.key?(proposal.id) || proposal.opinions.published.where(:user_id => inclusion.user_id).count == 0
+
+      proposal_dict = proposal.user_id == user.id ? your_proposals : active_proposals
+      if !proposal_dict.has_key? proposal.id 
+        proposal_dict[proposal.id] = {
+          :obj => proposal,
+          :events => {},
+          :relationship => user.opinions.published.where(:proposal_id => proposal.id).count > 0 ? 'You expressed an opinion' : false 
+        }
+      end
+
+      key = "new_inclusion_#{pnt.id}"
+      if !proposal_dict[proposal.id][:events].has_key? key 
+        if pnt.user_id == user.id
+          relationship = 'Your point'
+        elsif user.comments.where(:point_id => pnt.id).count > 0 
+          relationship = 'You commented on this point'
+        elsif user.inclusions.where(:point_id => pnt.id).count > 0 
+          relationship = 'You agreed with this point'
+        else 
+          relationship = false 
+        end
+        proposal_dict[proposal.id][:events][key] = {
+          :obj => pnt,
+          :type => 'new_inclusion',
+          :users => [inclusion.user],
+          :relationship => relationship
+        }
+      else 
+        proposal_dict[proposal.id][:events][key][:users].append inclusion.user
+      end 
+
+    end 
+  end
+
+  active = active_proposals.values()
+  your = your_proposals.values()
+
+  [active,your].each do |arr|
+    arr.sort_by! do |p|
+      tot = 0
+      p[:events].values.each do |e|
+        tot += e[:users].length 
+      end 
+      tot *= 10 if p.key?(:relationship) && p[:relationship]
+      -tot
+    end
+  end 
+
+  new_proposals = new_proposals.values()
+  new_proposals.sort_by! {|p| -p.opinions.published.count }
+
+  return {
+    :new_proposals => new_proposals,
+    :active_proposals => active,
+    :your_proposals => your
+  }
+
 
 end
 

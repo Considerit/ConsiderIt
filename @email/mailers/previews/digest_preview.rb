398c0cf202c64c174463c550d8d2db8890499ab8
Travis Kriplean <tkriplean@gmail.com>
2016-09-10 21:09:17 -0700

    refactor emails to work better (hopefully) with many email clients

--- @email/mailers/previews/digest_preview.rb
+++ @email/mailers/previews/digest_preview.rb
@@ -5,32 +5,33 @@ require Rails.root.join("@email", "send_digest")
 class DigestPreview < ActionMailer::Preview
 
   def digest 
+
     notifications = Notifier.aggregate(
         skip_moderation_filter: true,
         #filter: {'sent_email' => false},
-        #filter: {'subdomain_id' => 2354, 'user_id' => 1701}
+        # filter: {'subdomain_id' => Subdomain.find_by_name('consider').id, 'user_id' => 1701}
         # filter: {'sent_email' => false, 'subdomain_id' => 2354, 'user_id' => 1701}
-        filter: {'sent_email' => false, 'subdomain_id' => 2354}
+        filter: {'sent_email' => false, 'subdomain_id' => 2604}
         )
-    
+
     subdomain_id = notifications.keys.sample
     subdomain = Subdomain.find(subdomain_id)
 
 
-    while true 
-        user_id = notifications[subdomain_id].keys.sample
+    users = notifications[subdomain_id].keys
+    
+    mail = nil
+
+    users.each do |user_id|
         user = User.find(user_id)
 
         mail = send_digest(subdomain, user, notifications[subdomain_id][user_id], 
-            user.subscription_settings(subdomain), false) #, user.created_at) #proposals.where(:subdomain=>subdomain_id).last.created_at)
+            user.subscription_settings(subdomain), false, user.created_at) #proposals.where(:subdomain=>subdomain_id).last.created_at)
 
-        pp mail, user_id
-        
-        if mail 
+        if mail
             break
         end 
     end 
-
 
     mail
   end

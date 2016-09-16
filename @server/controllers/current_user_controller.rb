94804cf80ee58fce2a2a3221849b298fad53f532
Nathan Morris <nathan.ms@gmail.com>
2016-09-16 04:22:19 +0000

    new user account creation with SAML. works but needs cleanup

--- @server/controllers/current_user_controller.rb
+++ @server/controllers/current_user_controller.rb
@@ -452,6 +452,7 @@ class CurrentUserController < ApplicationController
   end
 
   def acs
+    errors = []
     settings = User.get_saml_settings(get_url_base)
     response = OneLogin::RubySaml::Response.new(params[:SAMLResponse], :settings => settings)
     puts 'SAML WORKING'
@@ -467,11 +468,52 @@ class CurrentUserController < ApplicationController
       logger.info "NAMEID: #{response.nameid}"
 
       # log user. in TODO allow for incorrect login and new user with name field
-      user = User.find_by_email(response.nameid.downcase)
-      replace_user(current_user, user)
-      set_current_user(user)
-      current_user.add_to_active_in
-      update_roles_and_permissions
+      email = response.nameid.downcase
+      user = User.find_by_email(email)
+
+      if !user || !user.registered
+        # create a new user from SAML assertion if not already registered
+        # TODO clean this up
+        attrs = {'email' => email, 'password' => 'TODO_UPDATE_RANDOM'} 
+        try_saml_update_password 'create account', errors, attrs 
+        update_saml_user_attrs 'create account', errors, attrs
+
+        current_user.name = 'Place Holder'
+        current_user.email = email
+        has_name = current_user.name && current_user.name.length > 0
+        ok_email = current_user.email && current_user.email.length > 0
+        signed_pledge = true 
+        ok_password = true
+
+        if has_name && ok_email && signed_pledge && ok_password
+          puts 'SAML register'
+
+          current_user.registered = true
+          if !current_user.save
+            raise "Error registering this uesr"
+          end
+
+          current_user.add_to_active_in
+          dirty_if_any_private_proposals current_user
+
+          update_roles_and_permissions
+
+          # if this user was created via the invitation process, note that
+          # they've gone through the registration process
+          if current_user.complete_profile
+            current_user.complete_profile = false
+            current_user.save
+          end
+          log('registered account')
+          puts current_user
+
+        end
+      else
+        replace_user(current_user, user)
+        set_current_user(user)
+        current_user.add_to_active_in
+        update_roles_and_permissions
+      end
 
       redirect_to '/' 
     else
@@ -490,6 +532,106 @@ class CurrentUserController < ApplicationController
   def get_url_base
     "#{request.protocol}#{request.host_with_port}"
   end
+
+  def update_saml_user_attrs(trying_to, errors, attrs = {})
+    # TODO if possible merge with update_user_attrs
+    types = { 
+      :avatar => ActionDispatch::Http::UploadedFile, 
+      :bio => String, 
+      :name => String,
+      :hide_name => 'boolean',
+      :email => String
+    }
+
+
+    types.each do |field, type| 
+      value = attrs[field]
+      error = "Field #{field} is wrong type #{value.class}"
+      if type == 'boolean'
+        raise error if value && !(!!value == value)
+      else
+        raise error if value && value.class != type
+      end
+    end
+
+    fields = ['avatar', 'bio', 'name', 'hide_name', 'tags', 'subscriptions']
+    new_params = attrs.select{|k,v| fields.include? k}
+    new_params[:name] = '' if !new_params[:name] #TODO: Do we really want to allow blank names?...
+
+    if new_params.has_key? :tags
+      # strip out non-editable tags...
+      new_tags = new_params[:tags].reject {|k,v| !k.include?('.editable') } 
+
+      # make sure non-editable tags weren't removed entirely...
+      non_editable_old_tags = JSON.parse(current_user.tags || '{}').reject {|k,v| k.include?('.editable') } 
+      new_tags.update non_editable_old_tags
+
+      new_params[:tags] = JSON.dump new_tags
+
+      dirty_key '/proposals' # might have access to more proposals if user tags have been changed (LVG, zipcodes)
+    end
+
+    if new_params.has_key? :subscriptions
+      new_params[:subscriptions] = current_user.update_subscriptions(new_params[:subscriptions])
+    end
+
+    if current_user.update_attributes(new_params)
+      # puts("Updating params. #{new_params}")
+      if !current_user.save
+        raise 'Error saving basic current_user parameters!'
+      end
+      
+
+    else
+      raise 'Had trouble manipulating this user!'
+    end
+
+    # Update their email address.  First, check if they gave us a new address
+    email = attrs[:email]
+    user = User.find_by_email(email)
+    if !email || email.length == 0
+      if trying_to == 'create account'
+        errors.append 'No email address specified'
+      end
+    # And if it's not taken
+    elsif user && (user != current_user)
+      errors.append 'There is already an account with that email'
+    # And that it's valid
+    elsif !/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i.match(email)
+      errors.append 'Email address is not properly formatted'
+    elsif current_user.email != email
+      # puts("Updating email from #{current_user.email} to #{params[:email]}")
+      # Okay, here comes a new email address!
+      current_user.update_attributes({:email => email, :verified => false})
+      if !current_user.save
+        raise "Error saving this user's email"
+      end
+    end
+  end
+
+  def try_saml_update_password(trying_to, errors, attrs={})
+    # Update their password
+    # TODO merge with try_update_password if possible
+    if !attrs[:password] || attrs[:password].length == 0
+      if trying_to == 'create account' || trying_to == 'reset password'
+        errors.append 'No password specified'
+      end
+    elsif attrs[:password].length < @min_pass
+      errors.append 'Password is too short'
+    else
+      # puts("Changing user's password.")
+      current_user.password = attrs[:password]
+      if !current_user.save
+        raise "Error saving this user's password"
+      end
+    end
+
+  end
+
+
+
+
+
 
 end
 

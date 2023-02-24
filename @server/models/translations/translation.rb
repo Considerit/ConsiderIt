50d2553ebe80f65dec447ec0ebffc02b7be68aca
Travis Kriplean <travis@consider.it>
2023-02-24 14:27:03 -0800

    .

--- @server/models/translations/translation.rb
+++ @server/models/translations/translation.rb
@@ -152,6 +152,7 @@ class Translations::Translation < ApplicationRecord
       $translation_uses[string_id] = 1
     end
 
+    pp "logging translation count", DateTime.now, $translation_uses_last_written_at, $translation_uses_write_after, DateTime.now - $translation_uses_last_written_at >= $translation_uses_write_after
     if DateTime.now - $translation_uses_last_written_at >= $translation_uses_write_after
       keys = $translation_uses.keys()
       if keys.length > 0 

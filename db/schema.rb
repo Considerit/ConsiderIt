a441058d2a63f778a0db1520207c3c1908a0b0ff
Travis Kriplean <travis@consider.it>
2023-07-23 18:18:26 -0700

    .

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_21_210145) do
+ActiveRecord::Schema.define(version: 2023_07_22_175101) do
 
   create_table "ahoy_events", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "subdomain_id"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2023_06_21_210145) do
     t.integer "subdomain_id"
     t.integer "moderation_status"
     t.integer "point_id"
+    t.boolean "hide_name", default: false
     t.index ["commentable_id"], name: "index_comments_on_commentable_id"
     t.index ["subdomain_id", "commentable_id", "commentable_type", "moderation_status"], name: "select_comments"
     t.index ["subdomain_id", "commentable_id", "commentable_type"], name: "select_comments_on_commentable"
@@ -218,6 +219,7 @@ ActiveRecord::Schema.define(version: 2023_06_21_210145) do
     t.string "banner_content_type"
     t.integer "banner_file_size"
     t.datetime "banner_updated_at"
+    t.boolean "hide_name", default: false
     t.index ["subdomain_id", "active"], name: "select_proposal_by_active"
     t.index ["subdomain_id", "id"], name: "select_proposal"
     t.index ["subdomain_id", "slug"], name: "select_proposal_by_long_id"

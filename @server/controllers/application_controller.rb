55b45857d761f07b952fcfe0982e192b913c2e46
Travis Kriplean <travis@consider.it>
2024-05-01 12:05:59 -0700

    really need to make a configuration option for this...

--- @server/controllers/application_controller.rb
+++ @server/controllers/application_controller.rb
@@ -471,7 +471,7 @@ protected
 
   
   def allow_iframe_requests
-    enable_iframing = ['allsides', 'enviroissues', 'debate', 'galacticfederation']
+    enable_iframing = ['allsides', 'enviroissues', 'debate', 'galacticfederation', 'monroe2044']
 
     if current_tenant && (enable_iframing.include?(current_tenant.name) || @oembed_request)
       response.headers.delete('X-Frame-Options')

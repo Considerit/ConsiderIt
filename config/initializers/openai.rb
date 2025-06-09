245dec4b4930501f838373821b501f2df30b5c9a
Travis Kriplean <travis@consider.it>
2025-06-08 19:05:22 -0700

    misc

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,5 +1,5 @@
-OpenAI.configure do |config|
-    config.access_token = APP_CONFIG[:openai][:access]
-    config.request_timeout = 480
-    # config.organization_id = ENV.fetch('OPENAI_ORGANIZATION_ID') # Optional.
-end
+# OpenAI.configure do |config|
+#     config.access_token = APP_CONFIG[:openai][:access]
+#     config.request_timeout = 480
+#     # config.organization_id = ENV.fetch('OPENAI_ORGANIZATION_ID') # Optional.
+# end
